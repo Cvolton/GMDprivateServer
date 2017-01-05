3f683e2e7b9591054aa2df076fc5f8653dd1acbf
Cvolton <clubpenguinmisa@gmail.com>
2017-01-05 15:44:35 +0100

    global rank

--- getGJUserInfo20.php
+++ getGJUserInfo20.php
@@ -12,6 +12,20 @@ include "connection.php";
 	//placeholders
 	$creatorpoints = $user["creatorPoints"];
 	$youtubeurl = "";
+	// GET POSITION
+	$e = "SET @rownum := 0;";
+	$query = $db->prepare($e);
+	$query->execute();
+	$f = "SELECT rank, stars FROM (
+                    SELECT @rownum := @rownum + 1 AS rank, stars, extID
+                    FROM users ORDER BY stars DESC
+                    ) as result WHERE extID='$extid'";
+	$query = $db->prepare($f);
+	$query->execute();
+	$leaderboard = $query->fetchAll();
+	$leaderboard = $leaderboard[0];
+	$rank = $leaderboard["rank"];
+	//var_dump($leaderboard);
 	if($me==$extid){
 		/* notifications */
 			//friends
@@ -25,7 +39,7 @@ include "connection.php";
 			//19 = enabled (0) disabled (1) friend requests
 			//31 = isnt (0) or is (1) friend or (3) incoming request or (4) outgoing request
 			//:32:9558256:35:XiB0cnU=:37:3 months
-			echo "1:".$user["userName"].":2:".$user["userID"].":13:".$user["coins"].":17:".$user["userCoins"].":10:".$user["color1"].":11:".$user["color2"].":3:".$user["stars"].":46:0:4:".$user["demons"].":8:".$creatorpoints.":18:0:19:0:20:".$youtubeurl.":21:".$user["accIcon"].":22:".$user["accShip"].":23:".$user["accBall"].":24:".$user["accBird"].":25:".$user["accDart"].":26:".$user["accRobot"].":28:".$user["accGlow"].":43:1:47:1:30:69:16:".$user["extID"].":31:0:44::45::38:3:39:".$requests.":40:9:29:1";
+			echo "1:".$user["userName"].":2:".$user["userID"].":13:".$user["coins"].":17:".$user["userCoins"].":10:".$user["color1"].":11:".$user["color2"].":3:".$user["stars"].":46:0:4:".$user["demons"].":8:".$creatorpoints.":18:0:19:0:20:".$youtubeurl.":21:".$user["accIcon"].":22:".$user["accShip"].":23:".$user["accBall"].":24:".$user["accBird"].":25:".$user["accDart"].":26:".$user["accRobot"].":28:".$user["accGlow"].":43:1:47:1:30:".$rank.":16:".$user["extID"].":31:0:44::45::38:3:39:".$requests.":40:9:29:1";
 	}else{
 		/* friend state */
 			$friendstate=0;
@@ -62,7 +76,7 @@ include "connection.php";
 			}
 		/* sending the data */
 		//$friendstate is :31:
-		echo "1:".$user["userName"].":2:".$user["userID"].":13:".$user["coins"].":17:".$user["userCoins"].":10:".$user["color1"].":11:".$user["color2"].":3:".$user["stars"].":46:0:4:".$user["demons"].":8:".$creatorpoints.":18:0:19:0:20:".$youtubeurl.":21:".$user["accIcon"].":22:".$user["accShip"].":23:".$user["accBall"].":24:".$user["accBird"].":25:".$user["accDart"].":26:".$user["accRobot"].":28:".$user["accGlow"].":43:1:47:1:30:69:16:".$user["extID"].":31:".$friendstate.":44::45::29:1";
+		echo "1:".$user["userName"].":2:".$user["userID"].":13:".$user["coins"].":17:".$user["userCoins"].":10:".$user["color1"].":11:".$user["color2"].":3:".$user["stars"].":46:0:4:".$user["demons"].":8:".$creatorpoints.":18:0:19:0:20:".$youtubeurl.":21:".$user["accIcon"].":22:".$user["accShip"].":23:".$user["accBall"].":24:".$user["accBird"].":25:".$user["accDart"].":26:".$user["accRobot"].":28:".$user["accGlow"].":43:1:47:1:30:".$rank.":16:".$user["extID"].":31:".$friendstate.":44::45::29:1";
 		if ($INCrequests > 0){
 			$request = $INCrequestinfo[0];
 			echo ":32:".$request["ID"].":35:".$request["comment"].":37:".$uploaddate;
