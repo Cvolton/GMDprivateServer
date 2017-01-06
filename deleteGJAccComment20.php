a351615346d570b1e66f4b9af7b80174330cd503
Cvolton <clubpenguinmisa@gmail.com>
2017-01-06 01:51:02 +0100

    deleting stuff

---
+++ deleteGJAccComment20.php
@@ -0,0 +1,23 @@
+<?php
+include "connection.php";
+require "incl/GJPCheck.php";
+$commentID = htmlspecialchars($_POST["commentID"],ENT_QUOTES);
+$accountID = htmlspecialchars($_POST["accountID"],ENT_QUOTES);
+$gjp = htmlspecialchars($_POST["gjp"],ENT_QUOTES);
+$GJPCheck = new GJPCheck();
+$gjpresult = $GJPCheck->check($gjp,$accountID);
+if($gjpresult == 1){
+	$query2 = $db->prepare("SELECT * FROM users WHERE extID = '$accountID'");
+	$query2->execute();
+	$result = $query2->fetchAll();
+	if ($query2->rowCount() > 0) {
+		$userIDalmost = $result[0];
+		$userID = $userIDalmost[1];
+	}
+	$query = $db->prepare("DELETE from acccomments WHERE commentID='$commentID' AND userID='$userID' LIMIT 1");
+	$query->execute();
+	echo "1";
+}else{
+	echo "-1";
+}
+?>
--- deleteGJFriendRequests20.php
+++ deleteGJFriendRequests20.php
@@ -1,10 +1,19 @@
 <?php
 include "connection.php";
+require "incl/GJPCheck.php";
+$gjp = htmlspecialchars($_POST["gjp"],ENT_QUOTES);
 $accountID = htmlspecialchars($_POST["accountID"],ENT_QUOTES);
 $targetAccountID = htmlspecialchars($_POST["targetAccountID"],ENT_QUOTES);
 //REMOVING THE REQUEST
 $query = $db->prepare("DELETE from friendreqs WHERE toAccountID='$accountID' AND accountID='$targetAccountID' LIMIT 1");
-$query->execute();
-//RESPONSE SO IT DOESNT SAY "FAILED"
-echo "1";
+$GJPCheck = new GJPCheck();
+$gjpresult = $GJPCheck->check($gjp,$accountID);
+if($gjpresult == 1){
+	$query->execute();
+	//RESPONSE SO IT DOESNT SAY "FAILED"
+	echo "1";
+}else{
+	//OR YOU KNOW WHAT LETS MAKE IT SAY "FAILED"
+	echo "-1";
+}
 ?>
