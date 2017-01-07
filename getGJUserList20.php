f4e0e191059a5944969debec7bd7687425a64971
Cvolton <clubpenguinmisa@gmail.com>
2017-01-07 18:27:39 +0100

    bugfix: something goes wrong when you're lonely

--- getGJUserList20.php
+++ getGJUserList20.php
@@ -10,18 +10,24 @@ $gjp = htmlspecialchars($_POST["gjp"],ENT_QUOTES);
 	$result = $query->fetchAll();
 	$account = $result[0];
 	$friendlist = $account["friends"];
-	$friendsarray = explode(',',$friendlist);
-	$friends = count($friendsarray);
-	for ($x = 0; $x < $friends; $x++) {
-		$currentfriend = $friendsarray[$x];
-		$query = "SELECT * FROM users WHERE extID = '$currentfriend'";
-		$query = $db->prepare($query);
-		$query->execute();
-		$result = $query->fetchAll();
-		$user = $result[0];
-		$gjpresult = $GJPCheck->check($gjp,$accountID);
-		if($gjpresult == 1){
-			echo "1:".$user["userName"].":2:".$user["userID"].":9:".$user["icon"].":10:".$user["color1"].":11:".$user["color2"].":14:".$user["iconType"].":15:".$user["special"].":16:".$currentfriend.":18:0:41:|";
-		}else{echo "-1";}
+	if($friendlist == ""){
+		echo "-2";
+	}
+	else
+	{
+		$friendsarray = explode(',',$friendlist);
+		$friends = count($friendsarray);
+		for ($x = 0; $x < $friends; $x++) {
+			$currentfriend = $friendsarray[$x];
+			$query = "SELECT * FROM users WHERE extID = '$currentfriend'";
+			$query = $db->prepare($query);
+			$query->execute();
+			$result = $query->fetchAll();
+			$user = $result[0];
+			$gjpresult = $GJPCheck->check($gjp,$accountID);
+			if($gjpresult == 1){
+				echo "1:".$user["userName"].":2:".$user["userID"].":9:".$user["icon"].":10:".$user["color1"].":11:".$user["color2"].":14:".$user["iconType"].":15:".$user["special"].":16:".$currentfriend.":18:0:41:|";
+			}else{echo "-1";}
+		}
 	}
 ?>
