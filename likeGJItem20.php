04853334b8d4c746345bc844d03361c9fb665efa
Cvolton <clubpenguinmisa@gmail.com>
2017-01-08 06:32:04 +0100

    (dis)liking comments

--- likeGJItem20.php
+++ likeGJItem20.php
@@ -1,7 +1,7 @@
 <?php
 include "connection.php";
 if($_POST["type"]=1){
-	if($_POST["like"]=1){
+	if($_POST["like"]==1){
 	$itemID = htmlspecialchars($_POST["itemID"],ENT_QUOTES);
 	$query=$db->prepare("select * from levels where levelID = '$itemID'");
 	$query->execute();
@@ -19,6 +19,52 @@ if($_POST["type"]=1){
 	$result = $result2[0];
 	$likes = $result["likes"] - 1;
 	$query2=$db->prepare("UPDATE levels SET likes = '$likes' WHERE levelID = '$itemID';");
+	$query2->execute();
+	echo "1";
+	}
+}
+if($_POST["type"]=2){
+	if($_POST["like"]==1){
+	$itemID = htmlspecialchars($_POST["itemID"],ENT_QUOTES);
+	$query=$db->prepare("select * from comments where commentID = '$itemID'");
+	$query->execute();
+	$result2 = $query->fetchAll();
+	$result = $result2[0];
+	$likes = $result["likes"] + 1;
+	$query2=$db->prepare("UPDATE comments SET likes = '$likes' WHERE commentID = '$itemID';");
+	$query2->execute();
+	echo "1";
+	}else if($_POST["like"]==0){
+	$itemID = $_POST["itemID"];
+	$query=$db->prepare("select * from comments where commentID = '$itemID'");
+	$query->execute();
+	$result2 = $query->fetchAll();
+	$result = $result2[0];
+	$likes = $result["likes"] - 1;
+	$query2=$db->prepare("UPDATE comments SET likes = '$likes' WHERE commentID = '$itemID';");
+	$query2->execute();
+	echo "1";
+	}
+}
+if($_POST["type"]=3){
+	if($_POST["like"]==1){
+	$itemID = htmlspecialchars($_POST["itemID"],ENT_QUOTES);
+	$query=$db->prepare("select * from acccomments where commentID = '$itemID'");
+	$query->execute();
+	$result2 = $query->fetchAll();
+	$result = $result2[0];
+	$likes = $result["likes"] + 1;
+	$query2=$db->prepare("UPDATE acccomments SET likes = '$likes' WHERE commentID = '$itemID';");
+	$query2->execute();
+	echo "1";
+	}else if($_POST["like"]==0){
+	$itemID = $_POST["itemID"];
+	$query=$db->prepare("select * from acccomments where commentID = '$itemID'");
+	$query->execute();
+	$result2 = $query->fetchAll();
+	$result = $result2[0];
+	$likes = $result["likes"] - 1;
+	$query2=$db->prepare("UPDATE acccomments SET likes = '$likes' WHERE commentID = '$itemID';");
 	$query2->execute();
 	echo "1";
 	}
