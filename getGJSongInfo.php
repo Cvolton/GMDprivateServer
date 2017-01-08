6ac9f075166641a0d496df6bdf188c08a06f779a
Pizzaroot <therealpizzaroot@gmail.com>
2017-01-08 17:12:48 +0400

    songinfo available
    yes it is

--- getGJSongInfo.php
+++ getGJSongInfo.php
@@ -1,24 +1,32 @@
 <?php
-include "connection.php";
-$songid = htmlspecialchars($_POST["songID"],ENT_QUOTES);
-$query3=$db->prepare("select * from songs where ID = '".$songid."'");
-$query3->execute();
-if($query3->rowCount() == 0) {
+$songid = htmlspecialchars($_POST["songID"], ENT_QUOTES);
+
 $url = 'http://www.boomlings.com/database/getGJSongInfo.php';
 $data = array('songID' => $songid, 'secret' => 'Wmfd2893gb7');
 $options = array(
     'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
         'method'  => 'POST',
-        'content' => http_build_query($data),
-    ),
+        'content' => http_build_query($data)
+    )
 );
 $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
-echo $result;
-}else{
-$result3 = $query3->fetchAll();
-$result4 = $result3[0];
-echo "1~|~".$result4["ID"]."~|~2~|~".$result4["name"]."~|~3~|~".$result4["authorID"]."~|~4~|~".$result4["authorName"]."~|~5~|~".$result4["size"]."~|~6~|~~|~10~|~".$result4["download"]."~|~7~|~~|~8~|~0";
+if ($result == "-1") {
+    $songinfo = file_get_contents("http://www.newgrounds.com/audio/listen/".$songid);
+    $songurl = explode('","', explode('"url":"', $songinfo)[1])[0];
+    $songname = substr($songurl, 43);
+    $realsongurl = "http://audio.ngfiles.com/".round($songid, -3)."/".$songid."_".$songname;
+    if (url_exists($realsongurl)) {
+        echo "1~|~".$songid."~|~2~|~".substr($songname, 0, -4)."~|~3~|~undefined~|~4~|~undefined~|~5~|~69.69~|~6~|~~|~10~|~".urlencode($realsongurl)."~|~7~|~~|~8~|~0";
+    } else {
+        die('-1');   
+    }
+} else {
+    echo $result;
+}      
+function url_exists($url) {
+    if (!$fp = curl_init($url)) return false;
+    return true;
 }
 ?>
