74fb6da831d3a24164fe94c6d41983c886b61647
Cvolton <clubpenguinmisa@gmail.com>
2017-01-08 02:13:03 +0100

    bugfix: removing friends would remove only from one persons friendlist

--- removeGJFriend20.php
+++ removeGJFriend20.php
@@ -18,7 +18,7 @@ if(($key = array_search($targetAccountID, $friendsarray)) !== false) {
     unset($friendsarray[$key]);
 }
 $newfriends = implode(",",$friendsarray);
-$query = $db->prepare("UPDATE accounts SET friends='$newfriends' WHERE accountID='$accountID'");
+$query3 = $db->prepare("UPDATE accounts SET friends='$newfriends' WHERE accountID='$accountID'");
 // REMOVING FOR USER 2
 $query = "SELECT * FROM accounts WHERE accountID = '$targetAccountID'";
 $query = $db->prepare($query);
@@ -38,7 +38,7 @@ $query2 = $db->prepare("UPDATE accounts SET friends='$newfriends' WHERE accountI
 $GJPCheck = new GJPCheck();
 $gjpresult = $GJPCheck->check($gjp,$accountID);
 if($gjpresult == 1){
-	$query->execute();
+	$query3->execute();
 	$query2->execute();
 	echo "1";
 }else{
