06b9de77195f88a6040776424805dfb98870e285
Cvolton <clubpenguinmisa@gmail.com>
2017-01-08 19:13:46 +0100

    account management

---
+++ accounts/accountManagement.php
@@ -0,0 +1,4 @@
+<h1>Account management tools:</h1>
+<a href="changePassword.php">Change Password</a>
+<h1>Server tools:</h1>
+<a href="../tools/songReupload.php">Song Reupload</a>
---
+++ accounts/changePassword.php
@@ -0,0 +1,18 @@
+<?php
+include "../connection.php";
+//here im getting all the data
+$userName = explode(";", htmlspecialchars($_POST["userName"],ENT_QUOTES))[0];
+$oldpassword = md5($_POST["oldpassword"] . "epithewoihewh577667675765768rhtre67hre687cvolton5gw6547h6we7h6wh");
+$newpassword = md5($_POST["newpassword"] . "epithewoihewh577667675765768rhtre67hre687cvolton5gw6547h6we7h6wh");
+if($userName != "" AND $newpassword != "" AND $oldpassword != ""){
+$query = $db->prepare("UPDATE accounts SET password='$newpassword' WHERE userName='$userName' AND password='$oldpassword'");	
+$query->execute();
+if($query->rowCount()==0){
+	echo "Invalid old password or nonexistent account. <a href='changePassword.php'>Try again</a>";
+}else{
+	echo "Password changed. <a href='accountManagement.php'>Go back to account management</a>";
+}
+}else{
+	echo '<form action="changePassword.php" method="post">Username: <input type="text" name="userName"><br>Old password: <input type="text" name="oldpassword"><br>New password: <input type="text" name="newpassword"><br><input type="submit" value="Reupload"></form>';
+}
+?>
