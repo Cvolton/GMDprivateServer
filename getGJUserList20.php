6c54d84171a379c08f95ef9fab70c41e1d930726
Cvolton <clubpenguinmisa@gmail.com>
2017-01-08 02:11:39 +0100

    unblocking users

--- getGJUserList20.php
+++ getGJUserList20.php
@@ -4,30 +4,35 @@ require_once "incl/GJPCheck.php";
 $GJPCheck = new GJPCheck();
 $accountID = htmlspecialchars($_POST["accountID"],ENT_QUOTES);
 $gjp = htmlspecialchars($_POST["gjp"],ENT_QUOTES);
+$type = htmlspecialchars($_POST["type"],ENT_QUOTES);
 	$query = "SELECT * FROM accounts WHERE accountID = '$accountID'";
 	$query = $db->prepare($query);
 	$query->execute();
 	$result = $query->fetchAll();
 	$account = $result[0];
-	$friendlist = $account["friends"];
-	if($friendlist == ""){
+	if($type == 0){
+		$usrlist = $account["friends"];
+	}else if($type==1){
+		$usrlist = $account["blocked"];
+	}
+	if($usrlist == ""){
 		echo "-2";
 	}
 	else
 	{
-		$friendsarray = explode(',',$friendlist);
-		$friends = count($friendsarray);
-		for ($x = 0; $x < $friends; $x++) {
-			$currentfriend = $friendsarray[$x];
-			$query = "SELECT * FROM users WHERE extID = '$currentfriend'";
-			$query = $db->prepare($query);
-			$query->execute();
-			$result = $query->fetchAll();
-			$user = $result[0];
-			$gjpresult = $GJPCheck->check($gjp,$accountID);
-			if($gjpresult == 1){
-				echo "1:".$user["userName"].":2:".$user["userID"].":9:".$user["icon"].":10:".$user["color1"].":11:".$user["color2"].":14:".$user["iconType"].":15:".$user["special"].":16:".$currentfriend.":18:0:41:|";
-			}else{echo "-1";}
-		}
+		$usrsarray = explode(',',$usrlist);
+		$usrs = count($usrsarray);
 	}
+	for ($x = 0; $x < $usrs; $x++) {
+				$currentfriend = $usrsarray[$x];
+				$query = "SELECT * FROM users WHERE extID = '$currentfriend'";
+				$query = $db->prepare($query);
+				$query->execute();
+				$result = $query->fetchAll();
+				$user = $result[0];
+				$gjpresult = $GJPCheck->check($gjp,$accountID);
+				if($gjpresult == 1){
+					echo "1:".$user["userName"].":2:".$user["userID"].":9:".$user["icon"].":10:".$user["color1"].":11:".$user["color2"].":14:".$user["iconType"].":15:".$user["special"].":16:".$currentfriend.":18:0:41:|";
+				}else{echo "-1";}
+			}
 ?>
---
+++ unblockGJUser20.php
@@ -0,0 +1,43 @@
+<?php
+include "connection.php";
+require_once "incl/GJPCheck.php";
+$accountID = htmlspecialchars($_POST["accountID"],ENT_QUOTES); //249 - maincra
+$gjp = htmlspecialchars($_POST["gjp"],ENT_QUOTES);
+$targetAccountID = htmlspecialchars($_POST["targetAccountID"],ENT_QUOTES); //250 - tomasek
+// REMOVING FOR USER 1
+$query = "SELECT * FROM accounts WHERE accountID = '$accountID'"; //selecting maincra
+$query = $db->prepare($query);
+$query->execute();
+$result = $query->fetchAll();
+$accinfo = $result[0];
+$blockeds = $accinfo["blocked"]; //getting who was blocked by maincra
+$blockedsarray = explode(',',$blockeds);
+if(($key = array_search($targetAccountID, $blockedsarray)) !== false) {
+    unset($blockedsarray[$key]); //removing the guy who was blocked by maincra
+}
+$newblockeds = implode(",",$blockedsarray);
+$query3 = $db->prepare("UPDATE accounts SET blocked='$newblockeds' WHERE accountID='$accountID'"); //mysql query
+// REMOVING FOR USER 2
+$query = "SELECT * FROM accounts WHERE accountID = '$targetAccountID'"; //selecting tomasek
+$query = $db->prepare($query);
+$query->execute();
+$result = $query->fetchAll();
+$accinfo = $result[0];
+$blockedlist = $accinfo["blockedBy"]; //getting who blocked tomasek
+$blockedsarray = explode(',',$blockedlist);
+if(($key = array_search($accountID, $blockedsarray)) !== false) {
+    unset($blockedsarray[$key]); //removing the guy who blocked tomasek from the array
+}
+$newblockeds = implode(",",$blockedsarray); 
+$query2 = $db->prepare("UPDATE accounts SET blockedBy='$newblockeds' WHERE accountID='$targetAccountID'"); //mysql query
+//EXECUTING THE QUERIES
+$GJPCheck = new GJPCheck();
+$gjpresult = $GJPCheck->check($gjp,$accountID);
+if($gjpresult == 1){
+	$query3->execute();
+	$query2->execute();
+	echo "1";
+}else{
+	echo "-1";
+}
+?>
