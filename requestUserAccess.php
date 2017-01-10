192170cd76838371969cedb2ae0ea72ee426e750
Cvolton <clubpenguinmisa@gmail.com>
2017-01-10 22:44:42 +0100

    bugfix: mod status doesn't load

--- requestUserAccess.php
+++ requestUserAccess.php
@@ -2,8 +2,8 @@
 //error_reporting(0);
 include "connection.php";
 require_once "incl/GJPCheck.php";
-$gjp = explode("|", explode("~", htmlspecialchars($_POST["gjp"],ENT_QUOTES);)[0])[0])[0])[0];
-$id = explode("|", explode("~", htmlspecialchars($_POST["accountID"],ENT_QUOTES);)[0])[0])[0])[0];
+$gjp = explode("|", explode("~", htmlspecialchars($_POST["gjp"],ENT_QUOTES))[0])[0];
+$id = explode("|", explode("~", htmlspecialchars($_POST["accountID"],ENT_QUOTES))[0])[0];
 $query2 = $db->prepare("SELECT * FROM users WHERE extID = :id");
 $query2->execute([':id' => $id]);
 $result = $query2->fetchAll();
