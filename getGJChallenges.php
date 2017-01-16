092cc68f0b283514b3f6d9b7e7c4b0b731b59e91
Cvolton <clubpenguinmisa@gmail.com>
2017-01-16 01:32:46 +0100

    A slight change to the quests system

--- getGJChallenges.php
+++ getGJChallenges.php
@@ -21,7 +21,7 @@ $chk = $XORCipher->cipher(base64_decode(substr($chk, 5)),19847);
 $quest1 = "0,".$quest1Type.",".$quest1Amount.",".$quest1Reward.",".$quest1Name."";
 $quest2 = "0,".$quest2Type.",".$quest2Amount.",".$quest2Reward.",".$quest2Name."";
 $quest3 = "0,".$quest3Type.",".$quest3Amount.",".$quest3Reward.",".$quest3Name."";
-$string = base64_encode($XORCipher->cipher("SaKuJ:".$userid.":".$chk.":".$udid.":".$accountID.":133700:".$quest1.":".$quest2.":".$quest3."",19847));
+$string = base64_encode($XORCipher->cipher("SaKuJ:".$userid.":".$chk.":".$udid.":".$accountID.":1337:".$quest1.":".$quest2.":".$quest3."",19847));
 $hash = $generateHash->genSolo3($string);
 echo "SaKuJ".$string . "|".$hash;
 ?>
--- questInfo.php
+++ questInfo.php
@@ -5,13 +5,13 @@ $quest1Type=1;
 $quest1Amount=100;
 $quest1Reward=69;
 //   QUEST 2
-$quest2Name="Negr Spek";
+$quest2Name="Big Mek Spek";
 $quest2Type=2;
 $quest2Amount=100;
 $quest2Reward=69;
 //   QUEST 3
-$quest3Name="Negr Spek";
-$quest3Type=0;
+$quest3Name="Smakovka";
+$quest3Type=3;
 $quest3Amount=100;
 $quest3Reward=69;
 ?>
