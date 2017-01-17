f66acfb8b55353d2bf44e8e4e6283233aa29253a
Cvolton <clubpenguinmisa@gmail.com>
2017-01-17 18:40:14 +0100

    made quests reset every day on midnight

--- getGJChallenges.php
+++ getGJChallenges.php
@@ -18,10 +18,24 @@ $result = $query->fetchAll();
 $result = $result[0];
 $userid = $result["userID"];
 $chk = $XORCipher->cipher(base64_decode(substr($chk, 5)),19847);
-$quest1 = "0,".$quest1Type.",".$quest1Amount.",".$quest1Reward.",".$quest1Name."";
-$quest2 = "0,".$quest2Type.",".$quest2Amount.",".$quest2Reward.",".$quest2Name."";
-$quest3 = "0,".$quest3Type.",".$quest3Amount.",".$quest3Reward.",".$quest3Name."";
-$string = base64_encode($XORCipher->cipher("SaKuJ:".$userid.":".$chk.":".$udid.":".$accountID.":1337:".$quest1.":".$quest2.":".$quest3."",19847));
+//Generating quest IDs
+$from = strtotime('2017-01-17');
+$today = time();
+$difference = $today - $from;
+$questID = floor($difference / 86400);
+$questID = $questID * 3;
+$quest1ID = $questID;
+$quest2ID = $questID+1;
+$quest3ID = $questID+2;
+//Time left
+$midnight = strtotime("tomorrow 00:00:00");
+$current = time();
+$timeleft = $midnight - $current;
+//quests
+$quest1 = $quest1ID.",".$quest1Type.",".$quest1Amount.",".$quest1Reward.",".$quest1Name."";
+$quest2 = $quest2ID.",".$quest2Type.",".$quest2Amount.",".$quest2Reward.",".$quest2Name."";
+$quest3 = $quest3ID.",".$quest3Type.",".$quest3Amount.",".$quest3Reward.",".$quest3Name."";
+$string = base64_encode($XORCipher->cipher("SaKuJ:".$userid.":".$chk.":".$udid.":".$accountID.":".$timeleft.":".$quest1.":".$quest2.":".$quest3."",19847));
 $hash = $generateHash->genSolo3($string);
 echo "SaKuJ".$string . "|".$hash;
 ?>
