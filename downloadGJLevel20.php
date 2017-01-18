5ac13fa8b15ef7cf1c1b7fc1e5a03229b352ee82
Cvolton <clubpenguinmisa@gmail.com>
2017-01-18 23:17:20 +0100

    daily features

--- downloadGJLevel20.php
+++ downloadGJLevel20.php
@@ -3,9 +3,19 @@ include "connection.php";
 require "incl/XORCipher.php";
 //$levelID = 2632;
 $levelID = htmlspecialchars($_POST["levelID"],ENT_QUOTES);
+$feaID = 0;
 if(!is_numeric($levelID)){
 	echo -1;
 }else{
+	if($levelID == "-1"){
+				$query = $db->prepare("SELECT * FROM dailyfeatures ORDER BY timestamp DESC LIMIT 1");
+				$query->execute();
+				$result = $query->fetchAll();
+				$result = $result[0];
+				$levelID = $result["levelID"];
+				$feaID = $result["feaID"];
+				$daily = 1;
+	}
 $query=$db->prepare("select * from levels where levelID = ?");
 $query->execute(array($levelID));
 $lvls = $query->rowCount();
@@ -19,7 +29,10 @@ $timeago2 = date('Y-M-D', $timeago);
 $xor = new XORCipher();
 $xorPass = base64_encode($xor->cipher($result["password"],26364));
 //submitting data
-echo "1:".$result["levelID"].":2:".$result["levelName"].":3:".$result["levelDesc"].":4:".$result["levelString"].":5:".$result["levelVersion"].":6:".$result["userID"].":8:10:9:".$result["starDifficulty"].":10:".$result["downloads"].":11:1:12:".$result["audioTrack"].":13:".$result["gameVersion"].":14:".$result["likes"].":17:".$result["starDemon"].":43:".$result["starDemonDiff"].":25:".$result["starAuto"].":18:".$result["starStars"].":19:".$result["starFeatured"].":42:".$result["starEpic"].":45:0:15:".$result["levelLength"].":30:".$result["original"].":31:0:28:".$timeago2. ":29:".$timeago2. ":35:".$result["songID"].":36:".$result["extraString"].":37:".$result["coins"].":38:".$result["starCoins"].":39:".$result["requestedStars"].":27:$xorPass";
+echo "1:".$result["levelID"].":2:".$result["levelName"].":3:".$result["levelDesc"].":4:".$result["levelString"].":5:".$result["levelVersion"].":6:".$result["userID"].":8:10:9:".$result["starDifficulty"].":10:".$result["downloads"].":11:1:12:".$result["audioTrack"].":13:".$result["gameVersion"].":14:".$result["likes"].":17:".$result["starDemon"].":43:".$result["starDemonDiff"].":25:".$result["starAuto"].":18:".$result["starStars"].":19:".$result["starFeatured"].":42:".$result["starEpic"].":45:0:15:".$result["levelLength"].":30:".$result["original"].":31:0:28:".$timeago2. ":29:".$timeago2. ":35:".$result["songID"].":36:".$result["extraString"].":37:".$result["coins"].":38:".$result["starCoins"].":39:".$result["requestedStars"].":46:1:47:2:27:$xorPass";
+if($daily == 1){
+	echo ":41:".$feaID;
+}
 //2.02 stuff
 echo "#";
 require "incl/generateHash.php";
@@ -27,11 +40,26 @@ $hash = new generateHash();
 echo $hash->genSolo($result["levelID"]);
 //2.1 stuff
 echo "#";
-$somestring = $result["userID"].",".$result["starStars"].",".$result["starDemon"].",".$result["levelID"].",".$result["starCoins"].",".$result["starFeatured"].",".$result["password"].",0";
+$somestring = $result["userID"].",".$result["starStars"].",".$result["starDemon"].",".$result["levelID"].",".$result["starCoins"].",".$result["starFeatured"].",".$result["password"].",".$feaID;
 echo $hash->genSolo2($somestring);
 echo "#";
-echo $somestring;
-#userid,version,0,levelid,1,starfeatured,1,0
+if($daily == 1){
+	$userIDquery = $db->prepare("SELECT * FROM users WHERE userID = '".$result["userID"]."'");
+	$userIDquery->execute();
+	$userID = $userIDquery->fetchAll();
+	if ($userIDquery->rowCount() > 0) {
+		$userID = $userID[0];
+		$userID = $userID["extID"];
+		if(is_numeric($userID)){
+			$userID = $userID;
+		}else{
+			$userID = 0;
+		}
+	}
+	echo $result["userID"] . ":" . $result["userName"] . ":" . $userID;
+}else{
+	echo $somestring;
+}
 //adding the download
 $downloads = $result["downloads"] + 1;
 $query2=$db->prepare("UPDATE levels SET downloads = :downloads WHERE levelID = :levelID");
---
+++ getGJDailyLevel.php
@@ -0,0 +1,16 @@
+<?php
+include "connection.php";
+include "questInfo.php";
+//Getting DailyID
+$query=$db->prepare("SELECT * FROM dailyfeatures ORDER BY timestamp DESC LIMIT 1");
+$query->execute(array($levelID));
+$result = $query->fetchAll();
+$result = $result[0];
+$dailyID = $result["feaID"];
+//Time left
+$midnight = strtotime("tomorrow 00:00:00");
+$current = time();
+$timeleft = $midnight - $current;
+//quests
+echo $dailyID ."|". $timeleft;
+?>
