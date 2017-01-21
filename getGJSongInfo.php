2fd66fb349f9e9613e691619edf32c242ca523a9
Cvolton <clubpenguinmisa@gmail.com>
2017-01-21 18:12:12 +0100

    added the ability to use unscouted ng songs too

--- getGJSongInfo.php
+++ getGJSongInfo.php
@@ -7,15 +7,24 @@ if($query3->rowCount() == 0) {
 $url = 'http://www.boomlings.com/database/getGJSongInfo.php';
 $data = array('songID' => $songid, 'secret' => 'Wmfd2893gb7');
 $options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data),
-    ),
+	'http' => array(
+		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+		'method'  => 'POST',
+		'content' => http_build_query($data),
+	),
 );
 $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
-echo $result;
+if ($result == "-1") {
+	$songinfo = file_get_contents("http://www.newgrounds.com/audio/listen/".$songid);
+	$songurl = explode('","', explode('"url":"', $songinfo)[1])[0];
+	$songauthor = explode('","', explode('artist":"', $songinfo)[1])[0];
+	$songurl = str_replace("\/", "/", $songurl);
+	$songname = explode("<title>", explode("</title>", $songinfo)[0])[1];
+	echo "1~|~".$songid."~|~2~|~".$songname."~|~3~|~1234~|~4~|~".$songauthor."~|~5~|~6.69~|~6~|~~|~10~|~".$songurl."~|~7~|~~|~8~|~0";
+} else {
+	echo $result;
+}	  
 }else{
 $result3 = $query3->fetchAll();
 $result4 = $result3[0];
