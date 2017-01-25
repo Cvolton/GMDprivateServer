aec83b3130620a9a75d97991d953b5310be5a41a
Cvolton <clubpenguinmisa@gmail.com>
2017-01-25 18:19:30 +0100

    placeholder

---
+++ incl/generateHash.php
@@ -0,0 +1,23 @@
+<?php
+//credits to pavlukivan for decoding and to IAD for most of genSolo
+class generateHash {
+	public function genMulti($lvlsmultistring) {
+		
+	}
+	public function genSolo($lvlsmultistring) {
+		
+	}
+	public function genSolo2($lvlsmultistring) {
+		
+	}
+	public function genSolo3($lvlsmultistring) {
+		
+	}
+	public function genSolo4($lvlsmultistring){
+		
+	}
+	public function genPack($lvlsmultistring) {
+		
+	}
+}
+?>
