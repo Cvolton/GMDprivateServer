332eddf1a2ff21f508fd5517cee9550e73137823
Cvolton <clubpenguinmisa@gmail.com>
2017-01-28 18:08:55 +0100

    made downloadGJLevels show upload and update date

--- downloadGJLevel20.php
+++ downloadGJLevel20.php
@@ -23,13 +23,13 @@ if($lvls!=0){
 $result2 = $query->fetchAll();
 $result = $result2[0];
 //getting the days since uploaded... or outputting the date in Y-M-D format at least for now...
-$timeago = $result["uploadDate"];
-$timeago2 = date('Y-M-D', $timeago);
+$uploadDate = date("d-m-Y G-i", $result["uploadDate"]);
+$updateDate = date("d-m-Y G-i", $result["updateDate"]);
 //password xor
 $xor = new XORCipher();
 $xorPass = base64_encode($xor->cipher($result["password"],26364));
 //submitting data
-echo "1:".$result["levelID"].":2:".$result["levelName"].":3:".$result["levelDesc"].":4:".$result["levelString"].":5:".$result["levelVersion"].":6:".$result["userID"].":8:10:9:".$result["starDifficulty"].":10:".$result["downloads"].":11:1:12:".$result["audioTrack"].":13:".$result["gameVersion"].":14:".$result["likes"].":17:".$result["starDemon"].":43:".$result["starDemonDiff"].":25:".$result["starAuto"].":18:".$result["starStars"].":19:".$result["starFeatured"].":42:".$result["starEpic"].":45:0:15:".$result["levelLength"].":30:".$result["original"].":31:0:28:".$timeago2. ":29:".$timeago2. ":35:".$result["songID"].":36:".$result["extraString"].":37:".$result["coins"].":38:".$result["starCoins"].":39:".$result["requestedStars"].":46:1:47:2:27:$xorPass";
+echo "1:".$result["levelID"].":2:".$result["levelName"].":3:".$result["levelDesc"].":4:".$result["levelString"].":5:".$result["levelVersion"].":6:".$result["userID"].":8:10:9:".$result["starDifficulty"].":10:".$result["downloads"].":11:1:12:".$result["audioTrack"].":13:".$result["gameVersion"].":14:".$result["likes"].":17:".$result["starDemon"].":43:".$result["starDemonDiff"].":25:".$result["starAuto"].":18:".$result["starStars"].":19:".$result["starFeatured"].":42:".$result["starEpic"].":45:0:15:".$result["levelLength"].":30:".$result["original"].":31:0:28:".$uploadDate. ":29:".$updateDate. ":35:".$result["songID"].":36:".$result["extraString"].":37:".$result["coins"].":38:".$result["starCoins"].":39:".$result["requestedStars"].":46:1:47:2:27:$xorPass";
 if($daily == 1){
 	echo ":41:".$feaID;
 }
