99558c26f1b313e9aed5acd65d426ac66cf8b09e
Cvolton <clubpenguinmisa@gmail.com>
2017-01-28 18:09:14 +0100

    reworked the gauntlets system

--- getGJGauntlets21.php
+++ getGJGauntlets21.php
@@ -6,8 +6,11 @@ $query = $db->prepare("SELECT * FROM gauntlets ORDER BY ID");
 $query->execute();
 $result = $query->fetchAll();
 foreach($result as &$gauntlet){
-	echo "1:".$gauntlet["ID"].":3:".$gauntlet["levels"]."|";
-	$string = $string.$gauntlet["ID"].$gauntlet["levels"];
+	if($gauntlet["level5"] != 0){
+		$lvls = $gauntlet["level1"].",".$gauntlet["level2"].",".$gauntlet["level3"].",".$gauntlet["level4"].",".$gauntlet["level5"];
+		echo "1:".$gauntlet["ID"].":3:".$lvls."|";
+		$string = $string.$gauntlet["ID"].$lvls;
+	}
 }
 echo "#".$generateHash->genSolo2($string);
 ?>
