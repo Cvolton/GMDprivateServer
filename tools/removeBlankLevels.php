77da574c4fc5d595aade51000a816b7dc1fe1bcb
Cvolton <clubpenguinmisa@gmail.com>
2017-01-28 18:11:33 +0100

    removing blank glitched songs from the db

--- tools/removeBlankLevels.php
+++ tools/removeBlankLevels.php
@@ -4,5 +4,7 @@ $query = $db->prepare("DELETE FROM levels WHERE levelString = ''");
 $query->execute();
 $query = $db->prepare("DELETE FROM users WHERE extID = ''");
 $query->execute();
+$query = $db->prepare("DELETE FROM songs WHERE download = ''");
+$query->execute();
 echo "<hr>If you do not see any errors above, the deletion was most probably succesful"
 ?>
