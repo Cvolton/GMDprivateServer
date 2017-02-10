d6d3ef8aed70e5a87f014c6d7659f8380818dbdb
Cvolton <clubpenguinmisa@gmail.com>
2017-02-11 00:26:37 +0100

    added bans based on usercoins

--- tools/autoban.php
+++ tools/autoban.php
@@ -1,18 +1,25 @@
 <hr>
 <?php
 include "../connection.php";
-$query = $db->prepare("SELECT starStars FROM levels");
+//unbanning everyone
+$query = $db->prepare("UPDATE users SET isBanned = '0'");
 $query->execute();
-$result = $query->fetchAll();
+$query = $db->prepare("SELECT starStars, coins, starCoins FROM levels");
+$query->execute();
+$levelstuff = $query->fetchAll();
 //counting stars
 $stars = 0;
-foreach($result as $level){
+foreach($levelstuff as $level){
 	$stars = $stars + $level["starStars"];
+	if($level["starCoins"]){
+		$coins = $coins + $level["coins"];
+	}
 }
 $query = $db->prepare("SELECT stars FROM mappacks");
 $query->execute();
 $result = $query->fetchAll();
 //counting stars
+echo "<h3>Stars based bans</h3>";
 foreach($result as $pack){
 	$stars = $stars + $pack["stars"];
 }
@@ -27,6 +34,19 @@ foreach($result as $user){
 	$query->execute([':id' => $user["userID"]]);
 	echo "Banned ".$user["userName"]." - ".$user["userID"]."<br>";
 }
+//counting coins
+echo "<h3>User coins based bans</h3>";
+$quarter = floor($coins / 4);
+$coins = $coins + $quarter;
+$query = $db->prepare("SELECT userID, userName FROM users WHERE userCoins > :coins");
+$query->execute([':coins' => $coins]);
+$result = $query->fetchAll();
+//banning ppl
+foreach($result as $user){
+	$query = $db->prepare("UPDATE users SET isBanned = '1' WHERE userID = :id");
+	$query->execute([':id' => $user["userID"]]);
+	echo "Banned ".$user["userName"]." - ".$user["userID"]."<br>";
+}
 //done
-echo "<hr>Banned everyone with over ".$stars." stars<hr>done";
+echo "<hr>Banned everyone with over ".$stars." stars and over ".$coins." user coins<hr>done";
 ?>
