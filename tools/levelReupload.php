f660fb6724f9349870a774e2840e9b4e15e375a7
Cvolton <clubpenguinmisa@gmail.com>
2017-02-12 22:23:38 +0100

    bugfix: the entire level reupload system not working properly

--- tools/levelReupload.php
+++ tools/levelReupload.php
@@ -19,19 +19,24 @@ if($_POST["levelid"]!=0){
 	);
 	$context  = stream_context_create($options);
 	$result = file_get_contents($url, false, $context);
-	$resultarray = explode(':', $result);
-	//echo $result;
-	//var_dump($resultarray);
-	$uploadDate = time();
-	$query = $db->prepare("SELECT * FROM levels WHERE originalReup = :lvl");
-	$query->execute([':lvl' => $_POST["levelid"]]);
-	if($query->rowCount() == 0){
-		$query = $db->prepare("INSERT INTO levels (levelName, gameVersion, binaryVersion, userName, levelDesc, levelVersion, levelLength, audioTrack, auto, password, original, twoPlayer, songID, objects, coins, requestedStars, extraString, levelString, levelInfo, secret, uploadDate, starDifficulty,originalReup)
-		VALUES (:name ,'20', '27', 'ORS', :a, :b, :c, :d, '0', '1337666', '0', :e, :f, '0', :g, :h, :i, :j, '0', '0', '$uploadDate', :k, :l)");
-		$query->execute([':name' => $resultarray[3], ':a' => $resultarray[5], ':b' => $resultarray[9], ':c' => $resultarray[35], ':d' => $resultarray[21], ':e' => $resultarray[39], ':f' => $resultarray[45], ':g' => $resultarray[49], ':h' => $resultarray[53], ':i' => $resultarray[47], ':j' => $resultarray[7], ':k' => $resultarray[15], ':l' => $resultarray[1]]);
-		echo "Level reuploaded, ID: " . $db->lastInsertId() . "<br><hr><br>";
+	if($result == "" OR $result == "-1"){
+		echo "This level doesn't exist.";
 	}else{
-		echo "This level has been already reuploaded";
+		$resultarray = explode(':', $result);
+		//echo $result;
+		//var_dump($resultarray);
+		$uploadDate = time();
+		$query = $db->prepare("SELECT * FROM levels WHERE originalReup = :lvl");
+		$query->execute([':lvl' => $_POST["levelid"]]);
+		if($query->rowCount() == 0){
+			//var_dump($resultarray);
+			$query = $db->prepare("INSERT INTO levels (levelName, gameVersion, binaryVersion, userName, levelDesc, levelVersion, levelLength, audioTrack, auto, password, original, twoPlayer, songID, objects, coins, requestedStars, extraString, levelString, levelInfo, secret, uploadDate, updateDate, starDifficulty, originalReup, userID, extID, unlisted)
+																			VALUES (:name ,'20', '27', 'Reupload', :desc, :version, :length, :audiotrack, '0', '1337666', '0', :twoPlayer, :songID, '0', :coins, :reqstar, :extraString, :levelString, '0', '0', '$uploadDate', '$uploadDate', :starDifficulty, :originalReup, '388', '263', '0')");
+			$query->execute([':name' => $resultarray[3], ':desc' => $resultarray[5], ':version' => $resultarray[9], ':length' => $resultarray[41], ':audiotrack' => $resultarray[21], ':twoPlayer' => $resultarray[45], ':songID' => $resultarray[51], ':coins' => $resultarray[55], ':reqstar' => $resultarray[59], ':extraString' => $resultarray[53], ':levelString' => $resultarray[7], ':starDifficulty' => $resultarray[15], ':originalReup' => $resultarray[1]]);
+			echo "Level reuploaded, ID: " . $db->lastInsertId() . "<br><hr><br>";
+		}else{
+			echo "This level has been already reuploaded";
+		}
 	}
 }else{
 	echo '<form action="levelReupload.php" method="post">ID: <input type="text" name="levelid"><br>URL (dont change if you dont know what youre doing): <input type="text" name="server" value="http://www.boomlings.com/database/downloadGJLevel22.php"><br><input type="submit" value="Reupload"></form><br>Alternative servers to reupload from:<br>
