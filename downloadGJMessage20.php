2d8880c8401868260fb38b9488685fd267321353
Cvolton <clubpenguinmisa@gmail.com>
2017-02-16 19:46:32 +0100

    patched an exploit where you could read other people's PMs

--- downloadGJMessage20.php
+++ downloadGJMessage20.php
@@ -11,10 +11,14 @@ $result = $result2[0];
 $GJPCheck = new GJPCheck();
 $gjpresult = $GJPCheck->check($gjp,$accountID);
 if($gjpresult == 1){
-	$uploadDate = date("d/m/Y G.i", $result["timestamp"]);
-	echo "6:".$result["userName"].":3:".$result["userID"].":2:".$result["accID"].":1:".$result["messageID"].":4:".$result["subject"].":8:1:9:0:5:".$result["body"].":7:".$uploadDate."";
-	$query=$db->prepare("UPDATE messages SET isNew=1 WHERE messageID = :messageID");
-	$query->execute([':messageID' => $messageID]);
+	if($accountID == $result["accID"] OR $accountID == $result["toAccountID"]){
+		$uploadDate = date("d/m/Y G.i", $result["timestamp"]);
+		echo "6:".$result["userName"].":3:".$result["userID"].":2:".$result["accID"].":1:".$result["messageID"].":4:".$result["subject"].":8:1:9:0:5:".$result["body"].":7:".$uploadDate."";
+		$query=$db->prepare("UPDATE messages SET isNew=1 WHERE messageID = :messageID");
+		$query->execute([':messageID' => $messageID]);	
+	}else{
+		echo -1;
+	}
 }else{
 	echo -1;
 }
