f62ddd79af26e1785e07d64f7fe0632b31c62112
Cvolton <clubpenguinmisa@gmail.com>
2017-02-17 20:15:45 +0100

    made packTable.php count the packs

--- tools/packTable.php
+++ tools/packTable.php
@@ -1,14 +1,16 @@
 <h1>MAP PACKS</h1>
-<table border="1"><tr><th>#</th><th>Map Pack</th><th>Stars</th><th>Coins</th><th>Levels</th></tr>
+<table border="1"><tr><th>#</th><th>ID</th><th>Map Pack</th><th>Stars</th><th>Coins</th><th>Levels</th></tr>
 <?php
 //error_reporting(0);
 include "../connection.php";
+$x = 1;
 $query = $db->prepare("SELECT * FROM mappacks ORDER BY ID ASC");
 $query->execute();
 $result = $query->fetchAll();
 foreach($result as &$pack){
 	$lvlarray = explode(",", $pack["levels"]);
-	echo "<tr><td>".$pack["ID"]."</td><td>".$pack["name"]."</td><td>".$pack["stars"]."</td><td>".$pack["coins"]."</td><td>";
+	echo "<tr><td>$x</td><td>".$pack["ID"]."</td><td>".$pack["name"]."</td><td>".$pack["stars"]."</td><td>".$pack["coins"]."</td><td>";
+	$x++;
 	foreach($lvlarray as &$lvl){
 		echo $lvl . " - ";
 		$query = $db->prepare("SELECT * FROM levels WHERE levelID = :levelID");
