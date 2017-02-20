761bff70db94fcc6b4af5bbb26d73d64450e91c4
Cvolton <clubpenguinmisa@gmail.com>
2017-02-21 00:18:10 +0100

    fixed gdps not working with strict sql mode on

--- database.sql
+++ database.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 02, 2017 at 02:49 PM
--- Server version: 5.1.71-community-log
+-- Generation Time: Feb 21, 2017 at 12:17 AM
+-- Server version: 5.6.33-log
 -- PHP Version: 5.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -30,12 +30,12 @@ CREATE TABLE IF NOT EXISTS `acccomments` (
   `userID` int(11) NOT NULL,
   `userName` varchar(50) NOT NULL,
   `comment` longtext NOT NULL,
-  `secret` varchar(10) NOT NULL,
+  `secret` varchar(10) NOT NULL DEFAULT 'unused',
   `commentID` int(11) NOT NULL AUTO_INCREMENT,
   `timestamp` int(11) NOT NULL,
-  `likes` int(11) NOT NULL,
+  `likes` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`commentID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=665 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1033 ;
 
 -- --------------------------------------------------------
 
@@ -47,22 +47,34 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `userName` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `secret` varchar(255) NOT NULL,
+  `secret` varchar(255) NOT NULL DEFAULT 'unused',
   `accountID` int(11) NOT NULL AUTO_INCREMENT,
   `saveData` longtext NOT NULL,
   `isAdmin` int(11) NOT NULL DEFAULT '0',
-  `userID` int(11) NOT NULL,
-  `friends` longtext NOT NULL,
-  `blockedBy` longtext NOT NULL,
-  `blocked` longtext NOT NULL,
-  `mS` int(11) NOT NULL,
-  `frS` int(11) NOT NULL,
-  `youtubeurl` varchar(255) NOT NULL,
-  `twitter` varchar(255) NOT NULL,
-  `twitch` varchar(255) NOT NULL,
+  `userID` int(11) NOT NULL DEFAULT '0',
+  `friends` varchar(1024) NOT NULL DEFAULT 'unused',
+  `blockedBy` varchar(1024) NOT NULL DEFAULT 'unused',
+  `blocked` varchar(1024) NOT NULL DEFAULT 'unused',
+  `mS` int(11) NOT NULL DEFAULT '0',
+  `frS` int(11) NOT NULL DEFAULT '0',
+  `youtubeurl` varchar(255) NOT NULL DEFAULT '',
+  `twitter` varchar(255) NOT NULL DEFAULT '',
+  `twitch` varchar(255) NOT NULL DEFAULT '',
   `salt` varchar(255) NOT NULL,
   PRIMARY KEY (`accountID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=707 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=925 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bannedips`
+--
+
+CREATE TABLE IF NOT EXISTS `bannedips` (
+  `IP` varchar(255) NOT NULL DEFAULT '127.0.0.1',
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -71,12 +83,12 @@ CREATE TABLE IF NOT EXISTS `accounts` (
 --
 
 CREATE TABLE IF NOT EXISTS `blocks` (
-  `ID` int(11) NOT NULL DEFAULT '0',
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `person1` int(11) NOT NULL,
   `person2` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID` (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -88,14 +100,14 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `userID` int(11) NOT NULL,
   `userName` varchar(50) NOT NULL,
   `comment` longtext NOT NULL,
-  `secret` varchar(10) NOT NULL,
+  `secret` varchar(10) NOT NULL DEFAULT 'none',
   `levelID` int(11) NOT NULL,
   `commentID` int(11) NOT NULL AUTO_INCREMENT,
   `timestamp` int(11) NOT NULL,
-  `likes` int(11) NOT NULL,
-  `percent` int(11) NOT NULL,
+  `likes` int(11) NOT NULL DEFAULT '0',
+  `percent` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`commentID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3098 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4745 ;
 
 -- --------------------------------------------------------
 
@@ -108,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `dailyfeatures` (
   `levelID` int(11) NOT NULL,
   `timestamp` int(11) NOT NULL,
   PRIMARY KEY (`feaID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=85 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=107 ;
 
 -- --------------------------------------------------------
 
@@ -123,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `friendreqs` (
   `uploadDate` int(11) NOT NULL,
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=509 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=860 ;
 
 -- --------------------------------------------------------
 
@@ -138,7 +150,7 @@ CREATE TABLE IF NOT EXISTS `friendships` (
   `isNew1` int(11) NOT NULL,
   `isNew2` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=243 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=503 ;
 
 -- --------------------------------------------------------
 
@@ -154,7 +166,7 @@ CREATE TABLE IF NOT EXISTS `gauntlets` (
   `level4` int(11) NOT NULL,
   `level5` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 -- --------------------------------------------------------
 
@@ -164,13 +176,13 @@ CREATE TABLE IF NOT EXISTS `gauntlets` (
 
 CREATE TABLE IF NOT EXISTS `levels` (
   `gameVersion` int(11) NOT NULL,
-  `binaryVersion` int(11) NOT NULL,
+  `binaryVersion` int(11) NOT NULL DEFAULT '0',
   `userName` text NOT NULL,
   `levelID` int(11) NOT NULL AUTO_INCREMENT,
   `levelName` text NOT NULL,
   `levelDesc` text NOT NULL,
   `levelVersion` int(11) NOT NULL,
-  `levelLength` int(11) NOT NULL,
+  `levelLength` int(11) NOT NULL DEFAULT '0',
   `audioTrack` int(11) NOT NULL,
   `auto` int(11) NOT NULL,
   `password` int(11) NOT NULL,
@@ -187,22 +199,22 @@ CREATE TABLE IF NOT EXISTS `levels` (
   `starDifficulty` int(11) NOT NULL DEFAULT '0' COMMENT '0=N/A 10=EASY 20=NORMAL 30=HARD 40=HARDER 50=INSANE 50=AUTO 50=DEMON',
   `downloads` int(11) NOT NULL DEFAULT '5',
   `likes` int(11) NOT NULL DEFAULT '3',
-  `starDemon` int(1) NOT NULL,
-  `starAuto` varchar(1) NOT NULL,
-  `starStars` int(11) NOT NULL,
+  `starDemon` int(1) NOT NULL DEFAULT '0',
+  `starAuto` varchar(11) NOT NULL DEFAULT '0',
+  `starStars` int(11) NOT NULL DEFAULT '0',
   `uploadDate` varchar(1337) NOT NULL,
   `updateDate` int(11) NOT NULL,
-  `starCoins` int(11) NOT NULL,
+  `starCoins` int(11) NOT NULL DEFAULT '0',
   `starFeatured` int(11) NOT NULL DEFAULT '0',
-  `starHall` int(11) NOT NULL,
-  `starEpic` int(11) NOT NULL,
-  `starDemonDiff` int(11) NOT NULL,
+  `starHall` int(11) NOT NULL DEFAULT '0',
+  `starEpic` int(11) NOT NULL DEFAULT '0',
+  `starDemonDiff` int(11) NOT NULL DEFAULT '0',
   `userID` int(11) NOT NULL,
   `extID` varchar(255) NOT NULL,
   `unlisted` int(11) NOT NULL,
-  `originalReup` int(11) NOT NULL COMMENT 'used for levelReupload.php',
+  `originalReup` int(11) NOT NULL DEFAULT '0' COMMENT 'used for levelReupload.php',
   PRIMARY KEY (`levelID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4517 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5587 ;
 
 -- --------------------------------------------------------
 
@@ -217,7 +229,7 @@ CREATE TABLE IF NOT EXISTS `levelscores` (
   `percent` int(11) NOT NULL,
   `uploadDate` int(11) NOT NULL,
   PRIMARY KEY (`scoreID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1700 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3949 ;
 
 -- --------------------------------------------------------
 
@@ -228,13 +240,13 @@ CREATE TABLE IF NOT EXISTS `levelscores` (
 CREATE TABLE IF NOT EXISTS `mappacks` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `levels` varchar(100) NOT NULL COMMENT 'entered as "ID of level 1, ID of level 2, ID of level 3" for example "13,14,15" (without the "s)',
+  `levels` varchar(512) NOT NULL COMMENT 'entered as "ID of level 1, ID of level 2, ID of level 3" for example "13,14,15" (without the "s)',
   `stars` int(11) NOT NULL,
   `coins` int(11) NOT NULL,
   `difficulty` int(11) NOT NULL,
   `rgbcolors` varchar(11) NOT NULL COMMENT 'entered as R,G,B',
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=38 ;
 
 -- --------------------------------------------------------
 
@@ -251,10 +263,10 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `messageID` int(11) NOT NULL AUTO_INCREMENT,
   `toAccountID` int(11) NOT NULL,
   `timestamp` int(11) NOT NULL,
-  `secret` varchar(10) NOT NULL,
+  `secret` varchar(25) NOT NULL DEFAULT 'unused',
   `isNew` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`messageID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=247 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=373 ;
 
 -- --------------------------------------------------------
 
@@ -264,17 +276,17 @@ CREATE TABLE IF NOT EXISTS `messages` (
 
 CREATE TABLE IF NOT EXISTS `modactions` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `type` int(11) NOT NULL,
-  `value` varchar(255) NOT NULL,
-  `timestamp` int(11) NOT NULL,
-  `value2` varchar(255) NOT NULL,
-  `value3` int(11) NOT NULL,
-  `value4` int(11) NOT NULL,
-  `value5` int(11) NOT NULL,
-  `value6` int(11) NOT NULL,
-  `account` int(11) NOT NULL,
+  `type` int(11) NOT NULL DEFAULT '0',
+  `value` varchar(255) NOT NULL DEFAULT '0',
+  `timestamp` int(11) NOT NULL DEFAULT '0',
+  `value2` varchar(255) NOT NULL DEFAULT '0',
+  `value3` int(11) NOT NULL DEFAULT '0',
+  `value4` int(11) NOT NULL DEFAULT '0',
+  `value5` int(11) NOT NULL DEFAULT '0',
+  `value6` int(11) NOT NULL DEFAULT '0',
+  `account` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=390 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2191 ;
 
 -- --------------------------------------------------------
 
@@ -288,7 +300,7 @@ CREATE TABLE IF NOT EXISTS `modips` (
   `isMod` int(11) NOT NULL,
   `accountID` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
 
 -- --------------------------------------------------------
 
@@ -301,7 +313,7 @@ CREATE TABLE IF NOT EXISTS `reports` (
   `levelID` int(11) NOT NULL,
   `hostname` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=32 ;
 
 -- --------------------------------------------------------
 
@@ -317,7 +329,7 @@ CREATE TABLE IF NOT EXISTS `songs` (
   `size` varchar(100) NOT NULL,
   `download` varchar(1337) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5100474 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5100799 ;
 
 -- --------------------------------------------------------
 
@@ -330,37 +342,37 @@ CREATE TABLE IF NOT EXISTS `users` (
   `userID` int(11) NOT NULL AUTO_INCREMENT,
   `extID` varchar(100) NOT NULL,
   `userName` varchar(69) NOT NULL DEFAULT 'undefined',
-  `stars` int(11) NOT NULL,
-  `demons` int(11) NOT NULL,
-  `icon` int(11) NOT NULL,
-  `color1` int(11) NOT NULL,
-  `color2` int(11) NOT NULL,
-  `iconType` int(11) NOT NULL,
-  `coins` int(11) NOT NULL,
-  `userCoins` int(11) NOT NULL,
-  `special` int(11) NOT NULL,
-  `gameVersion` int(11) NOT NULL,
-  `secret` varchar(69) NOT NULL,
-  `accIcon` int(11) NOT NULL,
-  `accShip` int(11) NOT NULL,
-  `accBall` int(11) NOT NULL,
-  `accBird` int(11) NOT NULL,
-  `accDart` int(11) NOT NULL,
-  `accRobot` int(11) NOT NULL,
-  `accGlow` int(11) NOT NULL,
-  `creatorPoints` int(11) NOT NULL,
+  `stars` int(11) NOT NULL DEFAULT '-1',
+  `demons` int(11) NOT NULL DEFAULT '-1',
+  `icon` int(11) NOT NULL DEFAULT '0',
+  `color1` int(11) NOT NULL DEFAULT '0',
+  `color2` int(11) NOT NULL DEFAULT '0',
+  `iconType` int(11) NOT NULL DEFAULT '0',
+  `coins` int(11) NOT NULL DEFAULT '-1',
+  `userCoins` int(11) NOT NULL DEFAULT '-1',
+  `special` int(11) NOT NULL DEFAULT '0',
+  `gameVersion` int(11) NOT NULL DEFAULT '0',
+  `secret` varchar(69) NOT NULL DEFAULT 'none',
+  `accIcon` int(11) NOT NULL DEFAULT '0',
+  `accShip` int(11) NOT NULL DEFAULT '0',
+  `accBall` int(11) NOT NULL DEFAULT '0',
+  `accBird` int(11) NOT NULL DEFAULT '0',
+  `accDart` int(11) NOT NULL DEFAULT '0',
+  `accRobot` int(11) DEFAULT '0',
+  `accGlow` int(11) NOT NULL DEFAULT '0',
+  `creatorPoints` int(11) NOT NULL DEFAULT '0',
   `IP` varchar(69) NOT NULL DEFAULT '127.0.0.1',
-  `lastPlayed` int(11) NOT NULL,
-  `diamonds` int(11) NOT NULL,
-  `accSpider` int(11) NOT NULL,
-  `accExplosion` int(11) NOT NULL,
-  `chest1time` int(11) NOT NULL,
-  `chest2time` int(11) NOT NULL,
-  `chest1count` int(11) NOT NULL,
-  `chest2count` int(11) NOT NULL,
-  `isBanned` int(11) NOT NULL,
+  `lastPlayed` int(11) NOT NULL DEFAULT '0',
+  `diamonds` int(11) NOT NULL DEFAULT '-1',
+  `accSpider` int(11) NOT NULL DEFAULT '0',
+  `accExplosion` int(11) NOT NULL DEFAULT '0',
+  `chest1time` int(11) NOT NULL DEFAULT '0',
+  `chest2time` int(11) NOT NULL DEFAULT '0',
+  `chest1count` int(11) NOT NULL DEFAULT '0',
+  `chest2count` int(11) NOT NULL DEFAULT '0',
+  `isBanned` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`userID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=877 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1292 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
