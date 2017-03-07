b3169bbc59327d37552f3842ed0f0090ac2e5989
Cvolton <clubpenguinmisa@gmail.com>
2017-03-07 18:23:23 +0100

    double friend reqs prevention

--- uploadFriendRequest20.php
+++ uploadFriendRequest20.php
@@ -10,18 +10,24 @@ $gjp = $ep->remove($_POST["gjp"]);
 $toAccountID = $ep->remove($_POST["toAccountID"]);
 $comment = $ep->remove($_POST["comment"]);
 $uploadDate = time();
-$query = $db->prepare("INSERT INTO friendreqs (accountID, toAccountID, comment, uploadDate)
-VALUES (:accountID, :toAccountID, :comment, :uploadDate)");
-if($accountID != "" AND $toAccountID != ""){
-	//GJPCheck
-	$GJPCheck = new GJPCheck();
-	$gjpresult = $GJPCheck->check($gjp,$accountID);
-	if($gjpresult == 1){
-		$query->execute([':accountID' => $accountID, ':toAccountID' => $toAccountID, ':comment' => $comment, ':uploadDate' => $uploadDate]);
-		echo 1;
+$query = $db->prepare("SELECT * FROM friendreqs WHERE (accountID=:accountID AND toAccountID=:toAccountID) OR (toAccountID=:accountID AND accountID=:toAccountID)");
+$query->execute([':accountID' => $accountID, ':toAccountID' => $toAccountID]);
+if($query->rowCount() == 0){
+	$query = $db->prepare("INSERT INTO friendreqs (accountID, toAccountID, comment, uploadDate)
+	VALUES (:accountID, :toAccountID, :comment, :uploadDate)");
+	if($accountID != "" AND $toAccountID != ""){
+		//GJPCheck
+		$GJPCheck = new GJPCheck();
+		$gjpresult = $GJPCheck->check($gjp,$accountID);
+		if($gjpresult == 1){
+			$query->execute([':accountID' => $accountID, ':toAccountID' => $toAccountID, ':comment' => $comment, ':uploadDate' => $uploadDate]);
+			echo 1;
+		}else{
+			echo -1;
+		}
 	}else{
 		echo -1;
-	}
+	}	
 }else{
 	echo -1;
 }
