a095076a042f6a214f7926a0e39de8a60b5f1888
Cvolton <clubpenguinmisa@gmail.com>
2017-03-07 18:20:27 +0100

    like hacks prevention

--- likeGJItem20.php
+++ likeGJItem20.php
@@ -2,9 +2,25 @@
 include "connection.php";
 require_once "incl/exploitPatch.php";
 $ep = new exploitPatch();
-if($_POST["type"]=1){
+$type = $_POST["type"] + 2;
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+	$ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+	$ip = $_SERVER['REMOTE_ADDR'];
+}
+$itemID = $ep->remove($_POST["itemID"]);
+$query6 = $db->prepare("SELECT * FROM actions WHERE type=:type AND value=:itemID AND value2=:ip");
+$query6->execute([':type' => $type, ':itemID' => $itemID, ':ip' => $ip]);
+if($query6->rowCount() > 2){
+	exit("-1");
+}
+$query6 = $db->prepare("INSERT INTO actions (type, value, timestamp, value2) VALUES 
+											(:type,:itemID, :time, :ip)");
+$query6->execute([':type' => $type, ':itemID' => $itemID, ':time' => time(), ':ip' => $ip]);
+if($_POST["type"]=="1"){ //level
 	if($_POST["like"]==1){
-	$itemID = $ep->remove($_POST["itemID"]);
 	$query=$db->prepare("select * from levels where levelID = :itemID");
 	$query->execute([':itemID' => $itemID]);
 	$result2 = $query->fetchAll();
@@ -25,10 +41,9 @@ if($_POST["type"]=1){
 	echo "1";
 	}
 }
-if($_POST["type"]=2){
-	if($_POST["like"]==1){
-	$itemID = $ep->remove($_POST["itemID"]);
-	$query=$db->prepare("select * from comments where commentID = :itemID");
+if($_POST["type"]=="2"){ //comments
+	if($_POST["like"]=="1"){
+		$query=$db->prepare("select * from comments where commentID = :itemID");
 	$query->execute([':itemID' => $itemID]);
 	$result2 = $query->fetchAll();
 	$result = $result2[0];
@@ -48,10 +63,9 @@ if($_POST["type"]=2){
 	echo "1";
 	}
 }
-if($_POST["type"]=3){
-	if($_POST["like"]==1){
-	$itemID = $ep->remove($_POST["itemID"]);
-	$query=$db->prepare("select * from acccomments where commentID = :itemID");
+if($_POST["type"]=="3"){ //acc comments
+	if($_POST["like"]=="1"){
+		$query=$db->prepare("select * from acccomments where commentID = :itemID");
 	$query->execute([':itemID' => $itemID]);
 	$result2 = $query->fetchAll();
 	$result = $result2[0];
