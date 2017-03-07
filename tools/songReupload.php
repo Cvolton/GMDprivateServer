79f55712e99d131e3e349f1a9ff2aa379ca8718e
Cvolton <clubpenguinmisa@gmail.com>
2017-03-07 18:24:00 +0100

    spam prevention filter removal in songReupload.php, will improve later

--- tools/songReupload.php
+++ tools/songReupload.php
@@ -20,13 +20,13 @@ if($result != "-1"){
 	$resultfixed = str_replace("~", "", $result);
 	$resultarray = explode('|', $resultfixed);
 	$song = urldecode($resultarray[13]);
-	if (filter_var($song, FILTER_VALIDATE_URL) == TRUE AND preg_match('/^[a-zA-Z0-9]+$/', $resultarray[7]) AND preg_match('/^[a-zA-Z0-9]+$/', $resultarray[9]) AND strpos($resultarray[3], ';') == false) 
-	{
+	//if (filter_var($song, FILTER_VALIDATE_URL) == TRUE AND preg_match('/^[a-zA-Z0-9]+$/', $resultarray[7]) AND preg_match('/^[a-zA-Z0-9]+$/', $resultarray[9]) AND strpos($resultarray[3], ';') == false) 
+	//{
 		$songReup = new songReup();
 		echo $songReup->reup($result);
-	}else{
-		echo "This song has triggered our spam prevention filter.";
-	}
+	//}else{
+	//	echo "This song has triggered our spam prevention filter.";
+	//}
 }else{
 	echo "This song either doesnt exist, or the author isnt scouted -_-";
 }
