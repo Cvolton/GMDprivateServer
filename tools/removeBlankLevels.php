a6b6b10768dd6cee239971b49fac6fc8bc85f073
Cvolton <clubpenguinmisa@gmail.com>
2017-03-16 01:09:08 +0100

    bugfix: cron job automatically removing all levels

--- tools/removeBlankLevels.php
+++ tools/removeBlankLevels.php
@@ -1,7 +1,5 @@
 <?php
 include "../connection.php";
-$query = $db->prepare("DELETE FROM levels WHERE levelString = ''");
-$query->execute();
 $query = $db->prepare("DELETE FROM users WHERE extID = ''");
 $query->execute();
 $query = $db->prepare("DELETE FROM songs WHERE download = ''");
