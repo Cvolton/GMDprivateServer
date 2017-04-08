4f2c9f663ec901061bf82712d7c60081f251499e
Cvolton <clubpenguinmisa@gmail.com>
2017-04-08 23:36:51 +0200

    updated unlisted.php for the new password system
    forgot to commit this file

--- tools/stats/unlisted.php
+++ tools/stats/unlisted.php
@@ -5,19 +5,19 @@ require_once "../../incl/lib/exploitPatch.php";
 $ep = new exploitPatch();
 //here im getting all the data
 $userName = $ep->remove($_POST["userName"]);
-$password = md5($_POST["password"] . "epithewoihewh577667675765768rhtre67hre687cvolton5gw6547h6we7h6wh");
+$password = $ep->remove($_POST["password"]);
 if($userName != "" AND $password != ""){
 	$generatePass = new generatePass();
 	$pass = $generatePass->isValidUsrname($userName, $password);
 	if ($pass == 1) {
-		$query = $db->prepare("SELECT * FROM accounts WHERE userName=:userName");	
+		$query = $db->prepare("SELECT accountID FROM accounts WHERE userName=:userName");	
 		$query->execute([':userName' => $userName]);
 		if($query->rowCount()==0){
 			echo "Invalid password or nonexistant account. <a href='unlisted.php'>Try again</a>";
 		}else{
-			$result = $query->fetchAll();
+			$accountID = $query->fetchColumn();
 			$query = $db->prepare("SELECT levelID, levelName FROM levels WHERE extID=:extID AND unlisted=1");	
-			$query->execute([':extID' => $result[0]["accountID"]]);
+			$query->execute([':extID' => $accountID]);
 			$result = $query->fetchAll();
 			echo '<table border="1"><tr><th>ID</th><th>Name</th></tr>';
 			foreach($result as &$level){
