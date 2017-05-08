b1613d970af90578142cd35dd5a2508a5cd741f2
Cvolton <clubpenguinmisa@gmail.com>
2017-05-08 22:17:20 +0200

    added fallback to getGJLevels20.php
    based on Absolute Gamer's song bypass

--- incl/misc/getGJSongInfo.php
+++ incl/misc/getGJSongInfo.php
@@ -5,6 +5,9 @@ require_once "../lib/songReup.php";
 $songReup = new songReup();
 require_once "../lib/exploitPatch.php";
 $ep = new exploitPatch();
+if(empty($_POST["songID"])){
+	exit("-1");
+}
 $songid = $ep->remove($_POST["songID"]);
 $query3=$db->prepare("SELECT ID,name,authorID,authorName,size,isDisabled,download FROM songs WHERE ID = :songid LIMIT 1");
 $query3->execute([':songid' => $songid]);
@@ -21,19 +24,54 @@ if($query3->rowCount() == 0) {
 	$context  = stream_context_create($options);
 	$result = file_get_contents($url, false, $context);
 	if ($result == "-2" OR $result == "-1" OR $result == "") {
-		$ch = curl_init(); 
-        curl_setopt($ch, CURLOPT_URL, "http://www.newgrounds.com/audio/listen/".$songid); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        $songinfo = curl_exec($ch); 
-        curl_close($ch);
-		$songurl = explode('","', explode('"url":"', $songinfo)[1])[0];
-		$songauthor = explode('","', explode('artist":"', $songinfo)[1])[0];
-		$songurl = str_replace("\/", "/", $songurl);
-		$songname = explode("<title>", explode("</title>", $songinfo)[0])[1];
-		if($songurl == ""){
-			exit("-1");
+		$url = 'http://www.boomlings.com/database/getGJLevels21.php';
+		$data = array(
+			'gameVersion' => '21',
+			'binaryVersion' => '33',
+			'gdw' => '0',
+			'type' => '2',
+			'str' => '',
+			'diff' => '-',
+			'len' => '-',
+			'page' => '0',
+			'total' => '9999',
+			'uncompleted' => '0',
+			'onlyCompleted' => '0',
+			'featured' => '0',
+			'original' => '0',
+			'twoPlayer' => '0',
+			'coins' => '0',
+			'epic' => '0',
+			'song' => $songid,
+			'customSong' => '1',
+			'secret' => 'Wmfd2893gb7'
+		);
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		if(substr_count($result, "1~|~".$songid."~|~2") != 0){
+			$result = explode('#',$result)[2];
+		}else{
+			$ch = curl_init(); 
+			curl_setopt($ch, CURLOPT_URL, "http://www.newgrounds.com/audio/listen/".$songid); 
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+			$songinfo = curl_exec($ch); 
+			curl_close($ch);
+			if(empty(explode('"url":"', $songinfo)[1])){
+				exit("-1");
+			}
+			$songurl = explode('","', explode('"url":"', $songinfo)[1])[0];
+			$songauthor = explode('","', explode('artist":"', $songinfo)[1])[0];
+			$songurl = str_replace("\/", "/", $songurl);
+			$songname = explode("<title>", explode("</title>", $songinfo)[0])[1];
+			if($songurl == ""){
+				exit("-1");
+			}
+			$result = "1~|~".$songid."~|~2~|~".$songname."~|~3~|~1234~|~4~|~".$songauthor."~|~5~|~6.69~|~6~|~~|~10~|~".$songurl."~|~7~|~~|~8~|~1";
 		}
-		$result = "1~|~".$songid."~|~2~|~".$songname."~|~3~|~1234~|~4~|~".$songauthor."~|~5~|~6.69~|~6~|~~|~10~|~".$songurl."~|~7~|~~|~8~|~1";
 	}
 	echo $result;
 	$reup = $songReup->reup($result);
