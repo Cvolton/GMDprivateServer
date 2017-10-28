6af1754c77d7f67a9d9b800a0d7c6977c6d82db4
Cvolton <clubpenguinmisa@gmail.com>
2017-10-28 21:41:26 +0200

    required config entries

---
+++ config/discord.php
@@ -0,0 +1,5 @@
+<?php
+$discordEnabled = 0;
+$secret = "please change this if you intend to use a discord bot with the server";
+$bottoken = "please change this to a discord bot token if you use one";
+?>
---
+++ config/songAdd.php
@@ -0,0 +1,2 @@
+<?php
+$api_key = "dc467dd431fc48eb0244b0aead929ccd";
