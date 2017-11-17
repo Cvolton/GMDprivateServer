a5847d375406851d2624aff609f9b570f56c4b6f
Michael Brabec <clubpenguinmisa@gmail.com>
2017-11-17 16:37:58 +0100

    2.11 comments stuff

--- incl/comments/getGJComments.php
+++ incl/comments/getGJComments.php
@@ -4,6 +4,8 @@ chdir(dirname(__FILE__));
 include "../lib/connection.php";
 require_once "../lib/exploitPatch.php";
 $ep = new exploitPatch();
+require_once "../lib/mainLib.php";
+$gs = new mainLib();
 $binaryVersion = $ep->remove($_POST["binaryVersion"]);
 $gameVersion = $ep->remove($_POST["gameVersion"]);
 $commentstring = "";
@@ -22,10 +24,12 @@ if($mode==0){
 	$modeColumn = "likes";
 }
 if(!$_POST["levelID"]){
+	$displayLevelID = true;
 	$levelID = $ep->remove($_POST["userID"]);
 	$query = "SELECT levelID, commentID, timestamp, comment, userID, likes, isSpam, percent FROM comments WHERE userID = :levelID ORDER BY $modeColumn DESC LIMIT 10 OFFSET $commentpage";
 	$countquery = "SELECT count(*) FROM comments WHERE userID = :levelID";
 }else{
+	$displayLevelID = false;
 	$levelID = $ep->remove($_POST["levelID"]);
 	$query = "SELECT levelID, commentID, timestamp, comment, userID, likes, isSpam, percent FROM comments WHERE levelID = :levelID ORDER BY $modeColumn DESC LIMIT 10 OFFSET $commentpage";
 	$countquery = "SELECT count(*) FROM comments WHERE levelID = :levelID";
@@ -46,7 +50,10 @@ foreach($result as &$comment1) {
 		if($gameVersion < 20){
 			$actualcomment = base64_decode($actualcomment);
 		}
-		$commentstring .= "1~".$comment1["levelID"]."~2~".$actualcomment."~3~".$comment1["userID"]."~4~".$comment1["likes"]."~5~0~7~".$comment1["isSpam"]."~9~".$uploadDate."~6~".$comment1["commentID"]."~10~".$comment1["percent"]."";
+		if($displayLevelID){
+			$commentstring .= "1~".$comment1["levelID"]."~";
+		}
+		$commentstring .= "2~".$actualcomment."~3~".$comment1["userID"]."~4~".$comment1["likes"]."~5~0~7~".$comment1["isSpam"]."~9~".$uploadDate."~6~".$comment1["commentID"]."~10~".$comment1["percent"];
 		$query12 = $db->prepare("SELECT userID, userName, icon, color1, color2, iconType, special, extID FROM users WHERE userID = :userID");
 		$query12->execute([':userID' => $comment1["userID"]]);
 		if ($query12->rowCount() > 0) {
@@ -61,7 +68,7 @@ foreach($result as &$comment1) {
 				$userstring .=  $user["userID"] . ":" . $user["userName"] . ":" . $extID . "|";
 			}
 			if($binaryVersion > 31){
-				$commentstring .= ":1~".$user["userName"]."~7~1~9~".$user["icon"]."~10~".$user["color1"]."~11~".$user["color2"]."~14~".$user["iconType"]."~15~".$user["special"]."~16~".$user["extID"];
+				$commentstring .= "~11~".$gs->getMaxValuePermission($extID, "modBadgeLevel")."~12~".$gs->getAccountCommentColor($extID).":1~".$user["userName"]."~7~1~9~".$user["icon"]."~10~".$user["color1"]."~11~".$user["color2"]."~14~".$user["iconType"]."~15~".$user["special"]."~16~".$user["extID"];
 			}
 			$commentstring .= "|";
 		}
