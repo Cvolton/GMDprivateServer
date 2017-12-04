3892d629dfc52912f3e6cee1bbe78d1e6cf551cd
Michael Brabec <clubpenguinmisa@gmail.com>
2017-12-04 10:14:04 +0100

    commands not dependant on isAdmin anymore

--- incl/misc/commands.php
+++ incl/misc/commands.php
@@ -1,5 +1,16 @@
 <?php
 class Commands {
+	public function ownCommand($comment, $command, $accountID, $targetExtID){
+		require_once "../lib/mainLib.php";
+		$gs = new mainLib();
+		$commandInComment = strtolower("!".$command);
+		$commandInPerms = ucfirst(strtolower($command));
+		$commandlength = strlen($commandInComment);
+		if(substr($comment,0,$commandlength) == $commandInComment AND (($gs->checkPermission($accountID, "command".$commandInPerms."All") OR ($targetExtID == $accountID AND $gs->checkPermission($accountID, "command".$commandInPerms."Own"))))){
+			return true;
+		}
+		return false;
+	}
 	public function doCommands($accountID, $comment, $levelID) {
 		include dirname(__FILE__)."/../lib/connection.php";
 		require_once "../lib/exploitPatch.php";
@@ -8,14 +19,10 @@ class Commands {
 		$gs = new mainLib();
 		$commentarray = explode(' ', $comment);
 		$uploadDate = time();
-		//GETTING USERINFO
-		$query2 = $db->prepare("SELECT isAdmin FROM accounts WHERE accountID = :accID");
-		$query2->execute([':accID' => $accountID]);
-		$userinfo = $query2->fetchAll()[0];
 		//LEVELINFO
 		$query2 = $db->prepare("SELECT extID FROM levels WHERE levelID = :id");
 		$query2->execute([':id' => $levelID]);
-		$targetExtID = $query2->fetchAll()[0]["extID"];
+		$targetExtID = $query2->fetchColumn();
 		//ADMIN COMMANDS
 		if(substr($comment,0,5) == '!rate' AND $gs->checkPermission($accountID, "commandRate")){
 			$starStars = $commentarray[2];
@@ -142,79 +149,77 @@ class Commands {
 
 		
 	//NON-ADMIN COMMANDS
-		if ($userinfo["isAdmin"] == 1 OR $targetExtID == $accountID) {
-			if(substr($comment,0,7) == '!rename'){
-				$name = $ep->remove(str_replace("!rename ", "", $comment));
-				$query = $db->prepare("UPDATE levels SET levelName=:levelName WHERE levelID=:levelID");
-				$query->execute([':levelID' => $levelID, ':levelName' => $name]);
-				$query = $db->prepare("INSERT INTO modactions (type, value, timestamp, account, value3) VALUES ('8', :value, :timestamp, :id, :levelID)");
-				$query->execute([':value' => $name, ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
-				return true;
-			}
-			if(substr($comment,0,5) == '!pass'){
-				$pass = $ep->remove(str_replace("!pass ", "", $comment));
-				if(is_numeric($pass)){
-					$pass = sprintf("%06d", $pass);
-					if($pass == "000000"){
-						$pass = "";
-					}
-					$pass = "1".$pass;
-					$query = $db->prepare("UPDATE levels SET password=:password WHERE levelID=:levelID");
-					$query->execute([':levelID' => $levelID, ':password' => $pass]);
-					$query = $db->prepare("INSERT INTO modactions (type, value, timestamp, account, value3) VALUES ('9', :value, :timestamp, :id, :levelID)");
-					$query->execute([':value' => $pass, ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
-					return true;
+		if($this->ownCommand($comment, "rename", $accountID, $targetExtID)){
+			$name = $ep->remove(str_replace("!rename ", "", $comment));
+			$query = $db->prepare("UPDATE levels SET levelName=:levelName WHERE levelID=:levelID");
+			$query->execute([':levelID' => $levelID, ':levelName' => $name]);
+			$query = $db->prepare("INSERT INTO modactions (type, value, timestamp, account, value3) VALUES ('8', :value, :timestamp, :id, :levelID)");
+			$query->execute([':value' => $name, ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+			return true;
+		}
+		if($this->ownCommand($comment, "pass", $accountID, $targetExtID)){
+			$pass = $ep->remove(str_replace("!pass ", "", $comment));
+			if(is_numeric($pass)){
+				$pass = sprintf("%06d", $pass);
+				if($pass == "000000"){
+					$pass = "";
 				}
-			}
-			if(substr($comment,0,12) == '!description'){
-				$desc = base64_encode($ep->remove(str_replace("!description ", "", $comment)));
-				$query = $db->prepare("UPDATE levels SET levelDesc=:desc WHERE levelID=:levelID");
-				$query->execute([':levelID' => $levelID, ':desc' => $desc]);
-				$query = $db->prepare("INSERT INTO modactions (type, value, timestamp, account, value3) VALUES ('13', :value, :timestamp, :id, :levelID)");
-				$query->execute([':value' => $desc, ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+				$pass = "1".$pass;
+				$query = $db->prepare("UPDATE levels SET password=:password WHERE levelID=:levelID");
+				$query->execute([':levelID' => $levelID, ':password' => $pass]);
+				$query = $db->prepare("INSERT INTO modactions (type, value, timestamp, account, value3) VALUES ('9', :value, :timestamp, :id, :levelID)");
+				$query->execute([':value' => $pass, ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
 				return true;
 			}
-			if(substr($comment,0,7) == '!public'){
-				$query = $db->prepare("UPDATE levels SET unlisted='0' WHERE levelID=:levelID");
-				$query->execute([':levelID' => $levelID]);
-				$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('12', :value, :levelID, :timestamp, :id)");
-				$query->execute([':value' => "0", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
-				return true;
-			}
-			if(substr($comment,0,7) == '!unlist'){
-				$query = $db->prepare("UPDATE levels SET unlisted='1' WHERE levelID=:levelID");
-				$query->execute([':levelID' => $levelID]);
-				$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('12', :value, :levelID, :timestamp, :id)");
-				$query->execute([':value' => "1", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
-				return true;
-			}
-			if(substr($comment,0,8) == '!sharecp'){
-				$query = $db->prepare("SELECT userID FROM users WHERE userName = :userName ORDER BY isRegistered DESC LIMIT 1");
-				$query->execute([':userName' => $commentarray[1]]);
-				$targetAcc = $query->fetchColumn();
-				//var_dump($result);
-				$query = $db->prepare("INSERT INTO cpshares (levelID, userID) VALUES (:levelID, :userID)");
-				$query->execute([':userID' => $targetAcc, ':levelID' => $levelID]);
-				$query = $db->prepare("UPDATE levels SET isCPShared='1' WHERE levelID=:levelID");
-				$query->execute([':levelID' => $levelID]);
-				$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('11', :value, :levelID, :timestamp, :id)");
-				$query->execute([':value' => $commentarray[1], ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
-				return true;
-			}
-			if(substr($comment,0,4) == '!ldm'){
-				$query = $db->prepare("UPDATE levels SET isLDM='1' WHERE levelID=:levelID");
-				$query->execute([':levelID' => $levelID]);
-				$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('14', :value, :levelID, :timestamp, :id)");
-				$query->execute([':value' => "1", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
-				return true;
-			}
-			if(substr($comment,0,6) == '!unldm'){
-				$query = $db->prepare("UPDATE levels SET isLDM='0' WHERE levelID=:levelID");
-				$query->execute([':levelID' => $levelID]);
-				$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('14', :value, :levelID, :timestamp, :id)");
-				$query->execute([':value' => "0", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
-				return true;
-			}
+		}
+		if($this->ownCommand($comment, "description", $accountID, $targetExtID)){
+			$desc = base64_encode($ep->remove(str_replace("!description ", "", $comment)));
+			$query = $db->prepare("UPDATE levels SET levelDesc=:desc WHERE levelID=:levelID");
+			$query->execute([':levelID' => $levelID, ':desc' => $desc]);
+			$query = $db->prepare("INSERT INTO modactions (type, value, timestamp, account, value3) VALUES ('13', :value, :timestamp, :id, :levelID)");
+			$query->execute([':value' => $desc, ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+			return true;
+		}
+		if($this->ownCommand($comment, "public", $accountID, $targetExtID)){
+			$query = $db->prepare("UPDATE levels SET unlisted='0' WHERE levelID=:levelID");
+			$query->execute([':levelID' => $levelID]);
+			$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('12', :value, :levelID, :timestamp, :id)");
+			$query->execute([':value' => "0", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+			return true;
+		}
+		if($this->ownCommand($comment, "unlist", $accountID, $targetExtID)){
+			$query = $db->prepare("UPDATE levels SET unlisted='1' WHERE levelID=:levelID");
+			$query->execute([':levelID' => $levelID]);
+			$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('12', :value, :levelID, :timestamp, :id)");
+			$query->execute([':value' => "1", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+			return true;
+		}
+		if($this->ownCommand($comment, "sharecp", $accountID, $targetExtID)){
+			$query = $db->prepare("SELECT userID FROM users WHERE userName = :userName ORDER BY isRegistered DESC LIMIT 1");
+			$query->execute([':userName' => $commentarray[1]]);
+			$targetAcc = $query->fetchColumn();
+			//var_dump($result);
+			$query = $db->prepare("INSERT INTO cpshares (levelID, userID) VALUES (:levelID, :userID)");
+			$query->execute([':userID' => $targetAcc, ':levelID' => $levelID]);
+			$query = $db->prepare("UPDATE levels SET isCPShared='1' WHERE levelID=:levelID");
+			$query->execute([':levelID' => $levelID]);
+			$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('11', :value, :levelID, :timestamp, :id)");
+			$query->execute([':value' => $commentarray[1], ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+			return true;
+		}
+		if($this->ownCommand($comment, "ldm", $accountID, $targetExtID)){
+			$query = $db->prepare("UPDATE levels SET isLDM='1' WHERE levelID=:levelID");
+			$query->execute([':levelID' => $levelID]);
+			$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('14', :value, :levelID, :timestamp, :id)");
+			$query->execute([':value' => "1", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+			return true;
+		}
+		if($this->ownCommand($comment, "unldm", $accountID, $targetExtID)){
+			$query = $db->prepare("UPDATE levels SET isLDM='0' WHERE levelID=:levelID");
+			$query->execute([':levelID' => $levelID]);
+			$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('14', :value, :levelID, :timestamp, :id)");
+			$query->execute([':value' => "0", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+			return true;
 		}
 		return false;
 	}
