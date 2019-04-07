5eb3907b2a9e5fc6b672f017345a21983a9f5f1e
Javier <32813748+Aspoky@users.noreply.github.com>
2019-04-07 16:25:29 +0200

    multiple message deletion

--- incl/messages/deleteGJMessages.php
+++ incl/messages/deleteGJMessages.php
@@ -4,18 +4,36 @@ include "../lib/connection.php";
 require_once "../lib/GJPCheck.php";
 require_once "../lib/exploitPatch.php";
 $ep = new exploitPatch();
-$messageID = $ep->remove($_POST["messageID"]);
+if(isset($_POST['messageID'])){$messageID = $ep->remove($_POST["messageID"]);}
 $accountID = $ep->remove($_POST["accountID"]);
-$gjp = $ep->remove($_POST["gjp"]);
-$GJPCheck = new GJPCheck();
-$gjpresult = $GJPCheck->check($gjp,$accountID);
-if($gjpresult == 1){
-	$query = $db->prepare("DELETE FROM messages WHERE messageID=:messageID AND accID=:accountID LIMIT 1");
-	$query->execute([':messageID' => $messageID, ':accountID' => $accountID]);
-	$query = $db->prepare("DELETE FROM messages WHERE messageID=:messageID AND toAccountID=:accountID LIMIT 1");
-	$query->execute([':messageID' => $messageID, ':accountID' => $accountID]);
-	echo "1";
-}else{
-	echo "-1";
+if(isset($_POST['messages'])){
+	$messageID = "0";
+	$messages = $ep->remove($_POST["messages"]);
+	$messages = preg_replace('/[^0-9,]/', '', $messages);
+	$gjp = $ep->remove($_POST["gjp"]);
+	$GJPCheck = new GJPCheck();
+	$gjpresult = $GJPCheck->check($gjp,$accountID);
+	if($gjpresult == 1){
+		$query = $db->prepare("DELETE FROM messages WHERE messageID IN (".$messages.") AND accID=:accountID LIMIT 10");
+		$query->execute([':accountID' => $accountID]);
+		$query = $db->prepare("DELETE FROM messages WHERE messageID IN (".$messages.") AND toAccountID=:accountID LIMIT 10");
+		$query->execute([':accountID' => $accountID]);
+		echo "1";
+	}else{
+		echo "-1";
+	}
+} else {
+	$gjp = $ep->remove($_POST["gjp"]);
+	$GJPCheck = new GJPCheck();
+	$gjpresult = $GJPCheck->check($gjp,$accountID);
+	if($gjpresult == 1){
+		$query = $db->prepare("DELETE FROM messages WHERE messageID=:messageID AND accID=:accountID LIMIT 1");
+		$query->execute([':messageID' => $messageID, ':accountID' => $accountID]);
+		$query = $db->prepare("DELETE FROM messages WHERE messageID=:messageID AND toAccountID=:accountID LIMIT 1");
+		$query->execute([':messageID' => $messageID, ':accountID' => $accountID]);
+		echo "1";
+	}else{
+		echo "-1";
+	}
 }
 ?>
