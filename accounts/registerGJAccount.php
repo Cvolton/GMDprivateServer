80c7460e543f2eea9d8e9896471403be69e9b0a8
absoIute <ben.stafford.03@icloud.com>
2019-04-14 23:18:23 +0100

    register rate limiting

--- accounts/registerGJAccount.php
+++ accounts/registerGJAccount.php
@@ -1,7 +1,10 @@
 <?php
+
+chdir(dirname(__FILE__));
 include "../incl/lib/connection.php";
 require_once "../incl/lib/exploitPatch.php";
 $ep = new exploitPatch();
+$ip = $_SERVER['REMOTE_ADDR'];
 if($_POST["userName"] != ""){
 	//here im getting all the data
 	$userName = $ep->remove($_POST["userName"]);
@@ -12,13 +15,25 @@ if($_POST["userName"] != ""){
 	$query2 = $db->prepare("SELECT count(*) FROM accounts WHERE userName LIKE :userName");
 	$query2->execute([':userName' => $userName]);
 	$regusrs = $query2->fetchColumn();
-	if ($regusrs > 0) {
+	//rate limiting
+	$query3 = $db->prepare("SELECT count(*) FROM accounts WHERE registerDate > :time AND ip = :ip");
+	$query3->execute([':time' => time() - 300 /*5 minutes*/, ':ip' => $ip]);
+	$ratelimit1 = $query3->fetchColumn();
+	
+	if ($ratelimit1 > 0)
+	{
+		echo "-13";
+	}
+	else if ($regusrs > 0)
+	{
 		echo "-2";
-	}else{
+	}
+	else
+	{
 		$hashpass = password_hash($password, PASSWORD_DEFAULT);
-		$query = $db->prepare("INSERT INTO accounts (userName, password, email, secret, saveData, registerDate, saveKey)
-		VALUES (:userName, :password, :email, :secret, '', :time, '')");
-		$query->execute([':userName' => $userName, ':password' => $hashpass, ':email' => $email, ':secret' => $secret, ':time' => time()]);
+		$query = $db->prepare("INSERT INTO accounts (userName, password, email, secret, saveData, registerDate, saveKey, ip)
+		VALUES (:userName, :password, :email, :secret, '', :time, '', :ip)");
+		$query->execute([':userName' => $userName, ':password' => $hashpass, ':email' => $email, ':secret' => $secret, ':time' => time(), ':ip' => $ip]);
 		echo "1";
 	}
 }
