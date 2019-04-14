02dd293f20d726887a3cedc55bf2e4112c5bce00
absoIute <ben.stafford.03@icloud.com>
2019-04-14 23:43:16 +0100

    mod actions code cleanup + mod ranks sorting

--- tools/stats/modActions.php
+++ tools/stats/modActions.php
@@ -1,109 +1,130 @@
-<h1>Actions Count</h1>
-<table border="1">
-<tr><th>Moderator</th><th>Count</th><th>Levels rated</th><th>Last time online</th></tr>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Mod Actions</title>
+		<?php include "../../../../incl/style.php"; ?>
+	</head>
+	
+	<body>
+		<?php include "../../../../incl/navigation.php"; ?>
+		
+		<div class="smain nofooter">
 <?php
-//error_reporting(0);
+
 include "../../incl/lib/connection.php";
-$query = $db->prepare("SELECT accountID, userName FROM accounts WHERE isAdmin = 1");
-$query->execute();
-$result = $query->fetchAll();
-foreach($result as &$mod){
-	$query = $db->prepare("SELECT lastPlayed FROM users WHERE extID = :id");
-	$query->execute([':id' => $mod["accountID"]]);
-	$time = date("d/m/Y G:i:s", $query->fetchColumn());
-	$query = $db->prepare("SELECT count(*) FROM modactions WHERE account = :id");
-	$query->execute([':id' => $mod["accountID"]]);
-	$actionscount = $query->fetchColumn();
-	$query = $db->prepare("SELECT count(*) FROM modactions WHERE account = :id AND type = '1'");
-	$query->execute([':id' => $mod["accountID"]]);
-	$lvlcount = $query->fetchColumn();
-	echo "<tr><td>".$mod["userName"]."</td><td>".$actionscount."</td><td>".$lvlcount."</td><td>".$time."</td></tr>";
-}
-?>
-</table>
-<h1>Actions Log</h1>
-<table border="1"><tr><th>Moderator</th><th>Action</th><th>Value</th><th>Value2</th><th>LevelID</th><th>Time</th></tr>
-<?php
+
+$types_ = array('1' => "Rated a level",
+				'2' => "Featured change",
+				'3' => "Coins verification state",
+				'4' => "Epic change",
+				'5' => "Set as daily feature",
+				'6' => "Deleted a level",
+				'7' => "Creator change",
+				'8' => "Renamed a level",
+				'9' => "Changed level password",
+				'10' => "Changed demon difficulty",
+				'11' => "Shared CP",
+				'12' => "Changed level publicity",
+				'13' => "Changed level description",
+				'14' => "Changed level CP reward");
+
 $query = $db->prepare("SELECT * FROM modactions ORDER BY ID DESC");
 $query->execute();
 $result = $query->fetchAll();
-foreach($result as &$action){
-	//detecting mod
-	$account = $action["account"];
-	$query = $db->prepare("SELECT * FROM accounts WHERE accountID = :id");
-	$query->execute([':id'=>$account]);
-	$result2 = $query->fetchAll();
-	$account = $result2[0]["userName"];
-	//detecting action
-	$value = $action["value"];
-	$value2 = $action["value2"];
-	switch($action["type"]){
-		case 1:
-			$actionname = "Rated a level";
-			break;
-		case 2:
-			$actionname = "Featured change";
-			break;
-		case 3:
-			$actionname = "Coins verification state";
-			break;
-		case 4:
-			$actionname = "Epic change";
-			break;
-		case 5:
-			$actionname = "Set as daily feature";
-			if(is_numeric($value2)){
-				$value2 = date("d/m/Y G:i:s", $value2);
-			}
-			break;
-		case 6:
-			$actionname = "Deleted a level";
-			break;
-		case 7:
-			$actionname = "Creator change";
-			break;
-		case 8:
-			$actionname = "Renamed a level";
-			break;
-		case 9:
-			$actionname = "Changed level password";
-			break;
-		case 10:
-			$actionname = "Changed demon difficulty";
-			break;
-		case 11:
-			$actionname = "Shared CP";
-			break;
-		case 12:
-			$actionname = "Changed level publicity";
-			break;
-		case 13:
-			$actionname = "Changed level description";
-			break;
-		case 14:
-			$actionname = "Changed level CP reward";
-			break;
-		default:
-			$actionname = $action["type"];
-			break;
-		}
-	if($action["type"] == 2 OR $action["type"] == 3 OR $action["type"] == 4){
-		if($action["value"] == 1){
-			$value = "True";
-		}else{
-			$value = "False";
-		}
-	}
-	if($action["type"] == 5 OR $action["type"] == 6){
-		$value = "";
-	}
-	$time = date("d/m/Y G:i:s", $action["timestamp"]);
-	if($action["type"] == 5 AND $action["value2"] > time()){
-		echo "<tr><td>".$account."</td><td>".$actionname."</td><td>".$value."</td><td>".$value2."</td><td>future</td><td>".$time."</td></tr>";
-	}else{
-		echo "<tr><td>".$account."</td><td>".$actionname."</td><td>".$value."</td><td>".$value2."</td><td>".$action["value3"]."</td><td>".$time."</td></tr>";
-	}
+
+$exMods = array();
+$currentMods = array();
+
+foreach ($result as $action)
+{
+	$id = $action['account'];
 	
+	if (!array_key_exists($id, $exMods))
+	{
+		$queryName = $db->prepare("SELECT * FROM accounts WHERE accountID = :id");
+		$queryName->execute([':id' => $id]);
+		$exMods[$id] = $queryName->fetchAll()[0];
+		$exMods[$id]['rateCount'] = 0;
+		$exMods[$id]['actionCount'] = 0;
+	}
+	else
+	{
+		$exMods[$id]['actionCount']++;
+		
+		if ($action['type'] == 1)
+		{
+			$exMods[$id]['rateCount']++;
+		}
+	}
 }
-?>
-</table>
+
+$query = $db->prepare("SELECT * FROM accounts WHERE isAdmin = 1");
+$query->execute();
+$result2 = $query->fetchAll();
+
+foreach ($result2 as $account)
+{
+	$id = $account['accountID'];
+	
+	if (array_key_exists($id, $exMods))
+	{
+		$currentMods[$id] = $exMods[$id];
+		unset($exMods[$id]);
+	}
+	else
+	{
+		$queryName = $db->prepare("SELECT * FROM accounts WHERE accountID = :id");
+		$queryName->execute([':id' => $id]);
+		$currentMods[$id] = $queryName->fetchAll()[0];
+	}
+}
+
+echo '<h1>Admins</h1><table><tr><th>Username</th><th>AccountID</th><th>Actions</th><th>Rated Levels</th></tr>';
+
+foreach ($currentMods as $mod)
+{
+	if ($mod['isHeadAdmin'] == 1)
+	{
+		echo "<tr><td>".$mod['userName']."</td><td>".$mod['accountID']."</td><td>".$mod['actionCount']."</td><td>".$mod['rateCount']."</td></tr>";
+	}
+}
+
+echo '</table><h1>Moderators</h1><table><tr><th>Username</th><th>AccountID</th><th>Actions</th><th>Rated Levels</th></tr>';
+
+foreach ($currentMods as $mod)
+{
+	if ($mod['isHeadAdmin'] == 0)
+	{
+		echo "<tr><td>".$mod['userName']."</td><td>".$mod['accountID']."</td><td>".$mod['actionCount']."</td><td>".$mod['rateCount']."</td></tr>";
+	}
+}
+
+echo '</table><h1>Ex-Moderators</h1><table><tr><th>Username</th><th>AccountID</th><th>Actions</th><th>Rated Levels</th></tr>';
+
+foreach ($exMods as $mod)
+{
+	if ($mod['rateCount'] != 0 AND !empty($mod['userName']))
+	{
+		echo "<tr><td>".$mod['userName']."</td><td>".$mod['accountID']."</td><td>".$mod['actionCount']."</td><td>".$mod['rateCount']."</td></tr>";
+	}
+}
+
+echo '</table><h1>Actions</h1><table><tr><th>Username</th><th>Type</th><th>LevelID</th><th>Value</th><th>Value2</th><th>Time</th></tr>';
+
+foreach ($result as $action)
+{
+	$username = array_key_exists($action['account'], $exMods) ? $exMods[$action['account']]['userName'] : $currentMods[$action['account']]['userName'];
+	$type = $types_[$action['type']];
+	$levelid = $action["value3"];
+	$value1 = $action["value"];
+	$value2 = $action["value2"];
+	$time = date("d/m/Y G:i:s", $action["timestamp"]);
+	
+	echo "<tr><td>$username</td><td>$type</td><td>$levelid</td><td>$value1</td><td>$value2</td><td>$time</td></tr>";
+}
+
+?>			
+			</table>
+		</div>
+	</body>
+</html>
