2fb8eff65bafa7bcaa99953bb5d8cd21f7c10e76
YohanYoutube <57457105+YohanYoutube@users.noreply.github.com>
2019-11-06 19:32:48 +0100

    Update dailyChests.php

--- config/dailyChests.php
+++ config/dailyChests.php
@@ -7,23 +7,23 @@
 	REWARDS
 */
 //SMALL CHEST
-$chest1minOrbs = 200;
-$chest1maxOrbs = 400;
-$chest1minDiamonds = 2;
+$chest1minOrbs = 500;
+$chest1maxOrbs = 1000;
+$chest1minDiamonds = 5;
 $chest1maxDiamonds = 10;
 $chest1minShards = 1;
-$chest1maxShards = 6;
-$chest1minKeys = 1;
-$chest1maxKeys = 6;
+$chest1maxShards = 3;
+$chest1minKeys = 2;
+$chest1maxKeys = 5;
 //BIG CHEST
-$chest2minOrbs = 2000;
-$chest2maxOrbs = 4000;
-$chest2minDiamonds = 20;
-$chest2maxDiamonds = 100;
-$chest2minShards = 1;
-$chest2maxShards = 6; // THIS VARIABLE IS NAMED IMPROPERLY, A MORE ACCURATE NAME WOULD BE $chest2minItemID AND $chest2maxItemID, BUT I DON'T WANT TO RENAME THIS FOR COMPATIBILITY REASONS... IF YOU'RE GETTING A BLANK CUBE IN YOUR DAILY CHESTS, YOU SET THIS TOO HIGH
-$chest2minKeys = 1;
-$chest2maxKeys = 6;
+$chest2minOrbs = 5000;
+$chest2maxOrbs = 10000;
+$chest2minDiamonds = 50;
+$chest2maxDiamonds = 500;
+$chest2minShards = 10;
+$chest2maxShards = 20; // THIS VARIABLE IS NAMED IMPROPERLY, A MORE ACCURATE NAME WOULD BE $chest2minItemID AND $chest2maxItemID, BUT I DON'T WANT TO RENAME THIS FOR COMPATIBILITY REASONS... IF YOU'RE GETTING A BLANK CUBE IN YOUR DAILY CHESTS, YOU SET THIS TOO HIGH
+$chest2minKeys = 4;
+$chest2maxKeys = 10;
 //REWARD TIMES (in seconds)
 $chest1wait = 3600;
 $chest2wait = 14400;
