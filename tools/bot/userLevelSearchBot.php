26930c1fbbc5625e3405d01d3567f6caea504d77
lxrdzik <35407720+lxrdzik@users.noreply.github.com>
2019-12-19 17:01:02 +0100

    fix SHOWING RESULT FOR $str in userLevelSearchBot
    of course this code still doesn't work but I fixed SHOWING RESULT FOR $str bug, which wasn't view str value
    *sorry for my english* and Cvolton fix bug, which API doesn't show level even if you put correct nickname with more than 1 levels

--- tools/bot/userLevelSearchBot.php
+++ tools/bot/userLevelSearchBot.php
@@ -5,7 +5,7 @@ require_once "../../incl/lib/exploitPatch.php";
 $ep = new exploitPatch();
 require_once "../../incl/lib/mainLib.php";
 $gs = new mainLib();
-$str = $ep->remove($_POST["str"]);
+$str = $ep->remove($_GET["str"]);
 $difficulty = "";
 $original = "";
 //getting level data
