4f9a218bc918b0ca141248e361cc65c9abf12c7e
WOSHIZHAZHA120 <52521836+WOSHIZHAZHA120@users.noreply.github.com>
2020-01-30 13:42:13 +0800

    fixed: failed to fetch info in songid > 5000000

--- incl/misc/getGJSongInfo.php
+++ incl/misc/getGJSongInfo.php
@@ -12,6 +12,9 @@ $songid = $ep->remove($_POST["songID"]);
 $query3=$db->prepare("SELECT ID,name,authorID,authorName,size,isDisabled,download FROM songs WHERE ID = :songid LIMIT 1");
 $query3->execute([':songid' => $songid]);
 if($query3->rowCount() == 0) {
+        if ($songid > 5000000) {
+            exit ("-1");
+        }
 	$url = 'http://www.boomlings.com/database/getGJSongInfo.php';
 	$data = array('songID' => $songid, 'secret' => 'Wmfd2893gb7');
 	$options = array(
