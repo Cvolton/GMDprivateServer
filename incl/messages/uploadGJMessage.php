4a32fe87c04c647c926eec05cd0e3983a0abe5a7
Michael Brabec <clubpenguinmisa@gmail.com>
2020-02-16 16:49:07 +0100

    made uploadGJMessage safer

--- incl/messages/uploadGJMessage.php
+++ incl/messages/uploadGJMessage.php
@@ -12,9 +12,9 @@ $gameVersion =  $ep->remove($_POST["gameVersion"]);
 $binaryVersion =  $ep->remove($_POST["binaryVersion"]);
 $secret =  $ep->remove($_POST["secret"]);
 $subject =  $ep->remove($_POST["subject"]);
-$toAccountID =  $ep->remove($_POST["toAccountID"]);
+$toAccountID =  $ep->number($_POST["toAccountID"]);
 $body =  $ep->remove($_POST["body"]);
-$accID =  $ep->remove($_POST["accountID"]);
+$accID =  $ep->number($_POST["accountID"]);
 $query3 = "SELECT userName FROM users WHERE extID = :accID ORDER BY userName DESC";
 $query3 = $db->prepare($query3);
 $query3->execute([':accID' => $accID]);
@@ -25,7 +25,7 @@ $register = 1;
 $userID = $gs->getUserID($id);
 $uploadDate = time();
 
-$blocked = $db->query("SELECT ID FROM `blocks` WHERE person1 = $toAccountID AND person2 = $accID")->fetchAll(PDO::FETCH_COLUMN);
+$blocked = $db->prepare("SELECT ID FROM `blocks` WHERE person1 = $toAccountID AND person2 = $accID")->fetchAll(PDO::FETCH_COLUMN);
 $mSOnly = $db->query("SELECT mS FROM `accounts` WHERE accountID = $toAccountID AND mS > 0")->fetchAll(PDO::FETCH_COLUMN);
 $friend = $db->query("SELECT ID FROM `friendships` WHERE (person1 = $accID AND person2 = $toAccountID) || (person2 = $accID AND person1 = $toAccountID)")->fetchAll(PDO::FETCH_COLUMN);
 
--- incl/relationships/uploadFriendRequest.php
+++ incl/relationships/uploadFriendRequest.php
@@ -16,19 +16,22 @@ $comment = $ep->remove($_POST["comment"]);
 $uploadDate = time();
 $query = $db->prepare("SELECT count(*) FROM friendreqs WHERE (accountID=:accountID AND toAccountID=:toAccountID) OR (toAccountID=:accountID AND accountID=:toAccountID)");
 $query->execute([':accountID' => $accountID, ':toAccountID' => $toAccountID]);
-if($query->fetchColumn() == 0){
-	//GJPCheck
-	$GJPCheck = new GJPCheck();
-	$gjpresult = $GJPCheck->check($gjp,$accountID);
-	if($gjpresult == 1){
-		$query = $db->prepare("INSERT INTO friendreqs (accountID, toAccountID, comment, uploadDate)
+
+$blocked = $db->query("SELECT ID FROM `blocks` WHERE person1 = $toAccountID AND person2 = $accID")->fetchAll(PDO::FETCH_COLUMN);
+$mSOnly = $db->query("SELECT frS FROM `accounts` WHERE accountID = $toAccountID AND mS > 0")->fetchAll(PDO::FETCH_COLUMN);
+
+$GJPCheck = new GJPCheck();
+$gjpresult = $GJPCheck->check($gjp,$id);
+if (!empty($mSOnly[0]) and $mSOnly[0] == 2) {
+    echo -1;
+} else {
+    if ($gjpresult == 1 and empty($blocked[0]) and !empty($frSOnly[0])) {
+        $query = $db->prepare("INSERT INTO friendreqs (accountID, toAccountID, comment, uploadDate)
 		VALUES (:accountID, :toAccountID, :comment, :uploadDate)");
 		$query->execute([':accountID' => $accountID, ':toAccountID' => $toAccountID, ':comment' => $comment, ':uploadDate' => $uploadDate]);
 		echo 1;
-	}else{
-		echo -1;
-	}
-}else{
-	echo -1;
+    } else {
+        echo -1;
+    }
 }
 ?>
