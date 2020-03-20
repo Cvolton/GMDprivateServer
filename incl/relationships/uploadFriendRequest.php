435e4e8e4fe528544fd08d581a6ce0362a059618
Michael Brabec <clubpenguinmisa@gmail.com>
2020-03-20 16:24:44 +0100

    added a block check

--- incl/relationships/uploadFriendRequest.php
+++ incl/relationships/uploadFriendRequest.php
@@ -9,29 +9,28 @@ $ep = new exploitPatch();
 if(empty($_POST["accountID"]) OR empty($_POST["toAccountID"])){
 	exit("-1");
 }
-$accountID = $ep->remove($_POST["accountID"]);
+$accountID = $ep->number($_POST["accountID"]);
 $gjp = $ep->remove($_POST["gjp"]);
-$toAccountID = $ep->remove($_POST["toAccountID"]);
+$toAccountID = $ep->number($_POST["toAccountID"]);
 $comment = $ep->remove($_POST["comment"]);
 $uploadDate = time();
+$blocked = $db->query("SELECT ID FROM `blocks` WHERE person1 = $toAccountID AND person2 = $accountID")->fetchAll(PDO::FETCH_COLUMN);
+$frSOnly = $db->query("SELECT frS FROM `accounts` WHERE accountID = $toAccountID AND frS = 1")->fetchAll(PDO::FETCH_COLUMN);
 $query = $db->prepare("SELECT count(*) FROM friendreqs WHERE (accountID=:accountID AND toAccountID=:toAccountID) OR (toAccountID=:accountID AND accountID=:toAccountID)");
 $query->execute([':accountID' => $accountID, ':toAccountID' => $toAccountID]);
-
-$blocked = $db->query("SELECT ID FROM `blocks` WHERE person1 = $toAccountID AND person2 = $accID")->fetchAll(PDO::FETCH_COLUMN);
-$mSOnly = $db->query("SELECT frS FROM `accounts` WHERE accountID = $toAccountID AND mS > 0")->fetchAll(PDO::FETCH_COLUMN);
-
-$GJPCheck = new GJPCheck();
-$gjpresult = $GJPCheck->check($gjp,$id);
-if (!empty($mSOnly[0]) and $mSOnly[0] == 2) {
-    echo -1;
-} else {
-    if ($gjpresult == 1 and empty($blocked[0]) and !empty($frSOnly[0])) {
-        $query = $db->prepare("INSERT INTO friendreqs (accountID, toAccountID, comment, uploadDate)
+if($query->fetchColumn() == 0){
+	//GJPCheck
+	$GJPCheck = new GJPCheck();
+	$gjpresult = $GJPCheck->check($gjp,$accountID);
+	if($gjpresult == 1 and empty($blocked[0]) and empty($frSOnly[0])){
+		$query = $db->prepare("INSERT INTO friendreqs (accountID, toAccountID, comment, uploadDate)
 		VALUES (:accountID, :toAccountID, :comment, :uploadDate)");
 		$query->execute([':accountID' => $accountID, ':toAccountID' => $toAccountID, ':comment' => $comment, ':uploadDate' => $uploadDate]);
 		echo 1;
-    } else {
-        echo -1;
-    }
+	}else{
+		echo -1;
+	}
+}else{
+	echo -1;
 }
 ?>
