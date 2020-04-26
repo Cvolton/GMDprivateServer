c5be2c7e534304a8e395796d299a9e6bceb17d9c
Phytee <62219347+Phytee@users.noreply.github.com>
2020-04-26 21:09:00 +0300

    Create localeFI.php

---
+++ dashboard/incl/lang/localeFI.php
@@ -0,0 +1,93 @@
+<?php
+
+//	NAVBAR
+
+$string["homeNavbar"] = "Kotisivu";
+
+$string["accountManagement"] = "Käyttäjänhallinta";
+$string["changePassword"] = "Vaihda käyttäjänimi";
+$string["changeUsername"] = "Vaihda salasana";
+$string["unlistedLevels"] = "Unlisted tasot";
+
+$string["modTools"] = "Modityökalut";
+$string["leaderboardBan"] = "Bannaa käyttäjä";
+$string["packCreate"] = "Luo Map Pack";
+
+$string["reuploadSection"] = "Julkaisu";
+$string["levelReupload"] = "Kentän julkaisu";
+$string["songAdd"] = "Lisää kappale";
+
+$string["browse"] = "Selaa";
+$string["statsSection"] = "Tilastot";
+$string["dailyTable"] = "Päivittäiset tasot";
+$string["modActions"] = "Modien teot";
+$string["gauntletTable"] = "Lista Gauntleteista";
+$string["packTable"] = "Lista Map Packeista";
+$string["leaderboardTime"] = "Tulostaulun edistyminen";
+
+$string["language"] = "Kieli";
+
+$string["loginHeader"] = "Tervetuloa, %s";
+$string["logout"] = "Kirjaudu ulos";
+$string["login"] = "Kirjaudu sisään";
+
+/*
+	REUPLOAD
+*/
+$string["reuploadBTN"] = "Julkaise";
+$string["errorGeneric"] = "On tapahtunut virhe:";
+$string["tryAgainBTN"] = "Yritä uudellleen";
+//songAdd.php
+$string["songAddUrlFieldLabel"] = "Kappaleen linkki: (Vain suoria, Dropbox tai Soundcloud Linkkejä)";
+$string["songAddUrlFieldPlaceholder"] = "Kappaleen linkki";
+$string["songAddAnotherBTN"] = "Uusi kappale";
+///errors
+$string["songAddError-2"] = "Linkki on vääränlainen";
+$string["songAddError-3"] = "Tämä kappale on jo julkaistu";
+$string["songAddError-4"] = "Tätä kappaletta ei voi julkaista";
+
+/*
+	STATS
+*/
+$string["ID"] = "ID";
+$string["stars"] = "Tähdet";
+$string["coins"] = "Coinit";
+$string["accounts"] = "Käyttäjät";
+$string["levels"] = "Tasot";
+$string["songs"] = "Kappaleet";
+$string["author"] = "Tekijä";
+$string["name"] = "Nimi";
+$string["userCoins"] = "Usercoinit";
+$string["time"] = "Aika";
+$string["deletedLevel"] = "Poistettu taso";
+$string["mod"] = "Moderaattori";
+$string["count"] = "Määrä";
+$string["ratedLevels"] = "Arvostellut tasot";
+$string["lastSeen"] = "Viimeksi online-tilassa";
+$string["level"] = "Taso";
+$string["pageInfo"] = "Näytetään sivua %s sivusta %s";
+$string["first"] = "Ensimmäinen";
+$string["previous"] = "Edellinen";
+$string["next"] = "Seuraava";
+$string["last"] = "Viimeinen";
+//modActionsList
+$string["action"] = "Teko";
+$string["value"] = "Ensimmäinen arvo";
+$string["value2"] = "Toinen Value";
+$string["modAction1"] = "Arvosteli tason";
+$string["modAction2"] = "Un/featured tason";
+$string["modAction3"] = "Un/verified coinit";
+$string["modAction4"] = "Un/epiced tason";
+$string["modAction5"] = "Laittoi päivittäiseksi featureksi";
+$string["modAction6"] = "Poisti tason";
+$string["modAction7"] = "Tekijän vaihto";
+$string["modAction8"] = "Nimesi tason uudelleen";
+$string["modAction9"] = "Vaihtoi tason salasanaa";
+$string["modAction10"] = "Vaihtoi demon-vaikeustasoa";
+$string["modAction11"] = "Jakoi CP";
+$string["modAction12"] = "Un/published";
+$string["modAction13"] = "Vaihtoi tason descriptionia";
+$string["modAction14"] = "Enabled/disabled LDM";
+$string["modAction15"] = "Un/bannasi tulostaululta";
+//errors
+$string["errorNoAccWithPerm"] = "Error: No accounts with the '%s' permission have been found";
