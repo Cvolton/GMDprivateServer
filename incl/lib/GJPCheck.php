d53150b6f5aa07ab4203ac32a5a1b5c3e0d5307b
Intelligent Cat <mohammadaminkazmi@gmail.com>
2020-08-25 15:02:16 +0430

    New GJP check

--- incl/lib/GJPCheck.php
+++ incl/lib/GJPCheck.php
@@ -1,15 +1,20 @@
 <?php
 class GJPCheck {
 	public function check($gjp, $accountID) {
-		include dirname(__FILE__)."/connection.php";;
+		include dirname(__FILE__)."/connection.php";
 		include dirname(__FILE__)."/../../config/security.php";
-		/*if($sessionGrants){
-			$query = $db->prepare("SELECT count(*) FROM actions WHERE type = 10 AND value = :accountID AND timestamp > :timestamp");
-			$query->execute([':accountID' => $accountID, ':timestamp' => time() - 3600]);
-			if($query->fetchColumn() > 0){
+		if($sessionGrants){
+			if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+				$ip = $_SERVER['HTTP_CLIENT_IP'];
+			} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+				$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+			} else {
+				$ip = $_SERVER['REMOTE_ADDR'];
+			}
+			if ($_SESSION["ip"] == $ip) {
 				return 1;
 			}
-		}*/
+		}
 		require_once dirname(__FILE__)."/XORCipher.php";
 		require_once dirname(__FILE__)."/generatePass.php";
 		$xor = new XORCipher();
