0f07aded8f14099e440162419d9a325bf4d58105
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2020-08-25 18:11:48 +0100

    small fix

--- incl/mods/requestUserAccess.php
+++ incl/mods/requestUserAccess.php
@@ -13,10 +13,18 @@ if($id != "" AND $gjp != ""){
 	$GJPCheck = new GJPCheck();
 	$gjpresult = $GJPCheck->check($gjp,$id);
 	if($gjpresult == 1){
-		$permState = $gs->getMaxValuePermission($id, "actionRequestMod");
-		echo $permState;
+		if ($gs->getMaxValuePermission($id,"actionRequestMod") == 1) { // checks if they have mod
+		$permState = $gs->getMaxValuePermission($id,"modBadgeLevel"); // checks mod badge level so it knows what to show
+		if ($permState == 0) {
+			exit(-1); //fix infinite req loop
+		}
+		if ($permState != 1 || $permState != 0){ // if the mod badge level is higher than 2, it will still show elder mod badge
+			echo 2;
+			exit();
+		}
+		echo $permState; }
 	}else{
-		echo -1;
+		echo -1; 
 	}
 }else{
 	echo -1;
