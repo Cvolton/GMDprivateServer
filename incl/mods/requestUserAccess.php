914c6effe24924d21424a1003451b05dc9e87cb6
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2020-08-25 20:24:59 +0100

    made the infinite loop togglable
    credits to Intelligent Cat for helping with the cleanup

--- incl/mods/requestUserAccess.php
+++ incl/mods/requestUserAccess.php
@@ -4,21 +4,25 @@ chdir(dirname(__FILE__));
 require_once "../lib/GJPCheck.php";
 require_once "../lib/exploitPatch.php";
 $ep = new exploitPatch();
-require_once "../lib/mainLib.php"; //mainlib is also connection.php
+require_once "../lib/mainLib.php"; //this is connection.php too
 $gs = new mainLib();
 $gjp = $ep->remove($_POST["gjp"]);
-$id = $ep->remove($_POST["accountID"]);
-if($id != "" AND $gjp != ""){
+$accountID = $ep->remove($_POST["accountID"]);
+if($accountID != "" AND $gjp != ""){
 	$GJPCheck = new GJPCheck();
-	$gjpresult = $GJPCheck->check($gjp,$id);
+	$gjpresult = $GJPCheck->check($gjp,$accountID);
 	if($gjpresult == 1){
-		if ($gs->getMaxValuePermission($id,"actionRequestMod") == 1) { // checks if they have mod
-		$permState = $gs->getMaxValuePermission($id,"modBadgeLevel"); // checks mod badge level so it knows what to show
+		if ($gs->getMaxValuePermission($accountID,"actionRequestMod") == 1) { // checks if they have mod
+		$permState = $gs->getMaxValuePermission($accountID,"modBadgeLevel"); // checks mod badge level so it knows what to show
+		/*if ($permState == 0) { 
+			exit();	//this makes the req have an infinite loop, remove the comment to enable the infinite loop
+		}*/					   
 		if ($permState >= 2){ // if the mod badge level is higher than 2, it will still show elder mod message
 			exit("2");
 		}
-		echo $permState; }
-	}else{
+		echo $permState; 
+	}
+}else{
 		echo -1; 
 	}
 }else{
