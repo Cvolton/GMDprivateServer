ff1e34b9470d912431776fe943ae4c3793cc1546
Intelligent Cat <mohammadaminkazmi@gmail.com>
2020-08-26 17:18:01 +0430

    a bit changed

--- config/security.php
+++ config/security.php
@@ -1,4 +1,4 @@
 <?php
 $cloudSaveEncryption = 0; //0 = password string replacement, 1 = cloud save encryption (password dependant)
-$sessionGrants = 1; //0 = GJP check is done every time; 1 = GJP check is not applied to the same ips; drastically improves performance, slightly descreases security
+$sessionGrants = 1; //0 = GJP check is done every time; 1 = GJP check is not applied to the same ips for one hour; drastically improves performance, slightly descreases security
 ?>
