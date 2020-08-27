82881f4c0d8998c2a3d5c88aba30a8c44815d5b8
Intelligent Cat <mohammadaminkazmi@gmail.com>
2020-08-27 16:53:38 +0430

    Added level reupload limiting
    Co-Authored-By: Jecket <jecket22@users.noreply.github.com>

---
+++ config/reuploadLimits.php
@@ -0,0 +1,8 @@
+<?php
+$isLevelReuploadLimitDaily = 0; // 0 for false and 1 for true
+$level_reupload = 0; // any number for applying the limit, isLevelReuploadLimitDaily indicates that if this limit resets daily, 0 for unlimited and -1 to disable reuploading 
+$song_reupload = 0; // just like level reuploading limit, but for songs
+$song_reupload_soundcloud = 1; // 1 for yes and 0 for no
+$song_reupload_dropbox = 1; // same as soundcloud
+$song_reupload_directlinks = 1; //same as soundcloud
+?>
