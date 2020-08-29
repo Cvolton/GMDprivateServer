9eb223f843f1fdb6e1e796648a82002394a4bf20
Intelligent Cat <mohammadaminkazmi@gmail.com>
2020-08-29 16:23:21 +0430

    Update autoban.php

--- tools/cron/autoban.php
+++ tools/cron/autoban.php
@@ -1,6 +1,7 @@
 <hr>
 <?php
 include "../../incl/lib/connection.php";
+require_once "../../incl/lib/mainLib.php";
 echo "Initializing autoban";
 ob_flush();
 flush();
@@ -19,13 +20,10 @@ foreach($levelstuff as $level){
 		$demons++;
 	}
 }
+// mappack stars
 $query = $db->prepare("SELECT stars FROM mappacks");
 $query->execute();
 $result = $query->fetchAll();
-//counting stars
-echo "<h3>Stars based bans</h3>";
-ob_flush();
-flush();
 foreach($result as $pack){
 	$stars += $pack["stars"];
 }
@@ -34,6 +32,22 @@ $stars = $stars + 200 + $quarter;
 $query = $db->prepare("SELECT userID, userName FROM users WHERE stars > :stars");
 $query->execute([':stars' => $stars]);
 $result = $query->fetchAll();
+// gauntlet stars
+$query = $db->prepare("SELECT level1, level2, level3, level4, level5 FROM gauntlets");
+$query->execute();
+$result = $query->fetchAll();
+foreach($result as $gauntlet){
+	foreach($gauntlet as $level){
+		$query = $db->prepare("SELECT starStars FROM levels WHERE levelID = :levelid");
+		$query->execute([':levelid' => $level]);
+		$result = $query->fetchColumn();
+		$stars += $result["starStars"];
+	}
+}
+//counting stars
+echo "<h3>Stars based bans</h3>";
+ob_flush();
+flush();
 //banning ppl
 foreach($result as $user){
 	$query = $db->prepare("UPDATE users SET isBanned = '1' WHERE userID = :id");
