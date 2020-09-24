4c02f9c273948df82d980d04a0ac81c8ed5c9ab9
Absolute <ben.stafford.03@icloud.com>
2020-09-24 18:33:04 +0100

    Fix map-pack rewards duping bug after loading data

--- incl/levelpacks/getGJMapPacks.php
+++ incl/levelpacks/getGJMapPacks.php
@@ -5,6 +5,8 @@ include "../lib/connection.php";
 require_once "../lib/exploitPatch.php";
 $ep = new exploitPatch();
 
+$isVerify = $_POST['isVerify'];
+
 if (!is_numeric($_POST["page"]))
 {
 	exit("-1");
@@ -14,7 +16,12 @@ $page = $ep->remove($_POST["page"]);
 $packpage = $page*10;
 $mappackstring = "";
 $lvlsmultistring = "";
-$query = $db->prepare("SELECT colors2,rgbcolors,ID,name,levels,stars,coins,difficulty FROM `mappacks` ORDER BY `stars` ASC LIMIT 10 OFFSET $packpage");
+
+if ($isVerify)
+	$query = $db->prepare("SELECT ID,name,levels,stars,coins FROM `mappacks` ORDER BY `stars` ASC");
+else
+	$query = $db->prepare("SELECT colors2,rgbcolors,ID,name,levels,stars,coins,difficulty FROM `mappacks` ORDER BY `stars` ASC LIMIT 10 OFFSET $packpage");
+
 $query->execute();
 $result = $query->fetchAll();
 $packcount = $query->rowCount();
@@ -24,7 +31,11 @@ foreach($result as &$mappack) {
 	if($colors2 == "none" OR $colors2 == ""){
 		$colors2 = $mappack["rgbcolors"];
 	}
-	$mappackstring .= "1:".$mappack["ID"].":2:".$mappack["name"].":3:".$mappack["levels"].":4:".$mappack["stars"].":5:".$mappack["coins"].":6:".$mappack["difficulty"].":7:".$mappack["rgbcolors"].":8:".$colors2."|";
+	
+	if ($isVerify)
+		$mappackstring .= "1:".$mappack["ID"].":3:".$mappack["levels"].":4:".$mappack["stars"].":5:".$mappack["coins"]."|";
+	else
+		$mappackstring .= "1:".$mappack["ID"].":2:".$mappack["name"].":3:".$mappack["levels"].":4:".$mappack["stars"].":5:".$mappack["coins"].":6:".$mappack["difficulty"].":7:".$mappack["rgbcolors"].":8:".$colors2."|";
 }
 $query = $db->prepare("SELECT count(*) FROM mappacks");
 $query->execute();
@@ -32,7 +43,11 @@ $totalpackcount = $query->fetchColumn();
 $mappackstring = substr($mappackstring, 0, -1);
 $lvlsmultistring = substr($lvlsmultistring, 0, -1);
 echo $mappackstring;
-echo "#".$totalpackcount.":".$packpage.":10";
+
+if ($isVerify)
+	echo "#".$totalpackcount.":".$packpage.":".$totalpackcount;
+else
+	echo "#".$totalpackcount.":".$packpage.":10";
 echo "#";
 require "../lib/generateHash.php";
 $hash = new generateHash();
