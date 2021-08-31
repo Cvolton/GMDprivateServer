406a085338f327ebffb36c66ae0e910923845629
Michael Brabec <clubpenguinmisa@gmail.com>
2021-08-31 02:13:43 +0200

    Removed an out of place comment

--- incl/lib/songReup.php
+++ incl/lib/songReup.php
@@ -1,11 +1,9 @@
 <?php
-//credits to pavlukivan for decoding and to IAD for most of genSolo
 class songReup {
 	public function reup($result) {
 		include dirname(__FILE__)."/connection.php";
 		$resultfixed = str_replace("~", "", $result);
 		$resultarray = explode('|', $resultfixed);
-		//var_dump($resultarray);
 		$uploadDate = time();
 		$query = $db->prepare("INSERT INTO songs (ID, name, authorID, authorName, size, download)
 		VALUES (:id, :name, :authorID, :authorName, :size, :download)");
