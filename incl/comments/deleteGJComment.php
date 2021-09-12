4cc6197d599deb922970b027fa95c565f0492d8a
Cvolton <cvolton@cvolton.eu>
2021-09-12 15:24:48 +0200

    optimized deleteGJComment

--- incl/comments/deleteGJComment.php
+++ incl/comments/deleteGJComment.php
@@ -9,20 +9,12 @@ $commentID = ExploitPatch::remove($_POST["commentID"]);
 $accountID = GJPCheck::getAccountIDOrDie();
 
 //TODO: optimize these queries, there's surely a way
-$query = $db->prepare("SELECT userID FROM users WHERE extID = :accountID");
-$query->execute([':accountID' => $accountID]);
-$userID = $query->fetchColumn();
+$userID = $gs->getUserID($accountID);
 $query = $db->prepare("DELETE FROM comments WHERE commentID=:commentID AND userID=:userID LIMIT 1");
 $query->execute([':commentID' => $commentID, ':userID' => $userID]);
 if($query->rowCount() == 0){
-	$query = $db->prepare("SELECT levelID FROM comments WHERE commentID = :commentID");
+	$query = $db->prepare("SELECT users.extID FROM comments INNER JOIN levels ON levels.levelID = comments.levelID INNER JOIN users ON levels.userID = users.userID WHERE commentID = :commentID");
 	$query->execute([':commentID' => $commentID]);
-	$levelID = $query->fetchColumn();
-	$query = $db->prepare("SELECT userID FROM levels WHERE levelID = :levelID");
-	$query->execute([':levelID' => $levelID]);
-	$creatorID = $query->fetchColumn();
-	$query = $db->prepare("SELECT extID FROM users WHERE userID = :userID");
-	$query->execute([':userID' => $creatorID]);
 	$creatorAccID = $query->fetchColumn();
 	if($creatorAccID == $accountID || $gs->checkPermission($accountID, "actionDeleteComment") == 1){
 		$query = $db->prepare("DELETE FROM comments WHERE commentID=:commentID AND levelID=:levelID LIMIT 1");
