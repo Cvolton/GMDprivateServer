ff15695ee6b5bb347084c53ae1356e64aa7fc41c
Cvolton <cvolton@cvolton.eu>
2021-10-28 21:42:31 +0200

    Fix requestUserAccess

--- incl/mods/requestUserAccess.php
+++ incl/mods/requestUserAccess.php
@@ -8,7 +8,7 @@ $gs = new mainLib();
 
 $accountID = GJPCheck::getAccountIDOrDie();
 
-if ($gs->getMaxValuePermission($accountID,"actionRequestMod") == 1) { // checks if they have mod
+if ($gs->getMaxValuePermission($accountID,"actionRequestMod") >= 1) { // checks if they have mod
 	$permState = $gs->getMaxValuePermission($accountID,"modBadgeLevel"); // checks mod badge level so it knows what to show					   
 	if ($permState >= 2){ // if the mod badge level is higher than 2, it will still show elder mod message
 		exit("2");
