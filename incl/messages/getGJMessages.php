4e03f70a382bb659e34e4338f2a8988324acd855
Jecket <35532868+Jecket22@users.noreply.github.com>
2021-11-27 20:14:34 +0100

    Make use of makeTime()

--- incl/messages/getGJMessages.php
+++ incl/messages/getGJMessages.php
@@ -4,6 +4,8 @@ chdir(dirname(__FILE__));
 include "../lib/connection.php";
 require_once "../lib/GJPCheck.php";
 require_once "../lib/exploitPatch.php";
+require_once "../lib/mainLib.php";
+$gs = new mainLib();
 $msgstring = "";
 //code begins
 $toAccountID = GJPCheck::getAccountIDOrDie();
@@ -30,7 +32,7 @@ if($msgcount == 0){
 }
 foreach ($result as &$message1) {
 	if($message1["messageID"]!=""){
-		$uploadDate = date("d/m/Y G.i", $message1["timestamp"]);
+		$uploadDate = $gs-makeTime(time() - $message1["timestamp"]);
 		if($getSent == 1){
 			$accountID = $message1["toAccountID"];
 		}else{
