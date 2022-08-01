667845f8cfcb3e4facbad73c06b88fbecc5a4fbe
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2022-08-02 01:38:31 +0500

    Added support to unban player

---
+++ incl/lang/localeEN.php
@@ -0,0 +1,220 @@
+<?php
+/*
+	NAVBAR
+*/
+$string["homeNavbar"] = "Home";
+$string["welcome"] = "Welcome to GDPS!";
+$string["levelsWeek"] = "Levels uploaded in a week";
+$string["levels3Months"] = "Levels uploaded in 3 months";
+
+$string["accountManagement"] = "Account management";
+$string["changePassword"] = "Change password";
+$string["changeUsername"] = "Change username";
+$string["unlistedLevels"] = "Your unlisted levels";
+
+$string["manageSongs"] = "Manage songs";
+$string["gauntletManage"] = "Manage Gauntlets";
+$string["suggestLevels"] = "Suggested levels";
+
+$string["modTools"] = "Mod tools";
+$string["leaderboardBan"] = "Ban a user";
+$string["unlistedMod"] = "Unlisted levels";
+
+$string["reuploadSection"] = "Reupload";
+$string["levelReupload"] = "Level reupload";
+$string["songAdd"] = "Add a song";
+$string["songLink"] = "Add a song by link";
+$string["packManage"] = "Manage Map Packs";
+
+$string["browse"] = "Browse";
+$string["statsSection"] = "Stats";
+$string["dailyTable"] = "Daily levels";
+$string["modActionsList"] = "Mod actions";
+$string["modActions"] = "Server moderators";
+$string["gauntletTable"] = "List of Gauntlets";
+$string["packTable"] = "List of Map Packs";
+$string["leaderboardTime"] = "Leaderboards progress";
+
+$string["download"] = "Download";
+$string["forwindows"] = "For Windows";
+$string["forandroid"] = "For Android";
+$string["language"] = "Language";
+
+$string["loginHeader"] = "Welcome, %s!";
+$string["logout"] = "Logout";
+$string["login"] = "Login";
+$string["loginBox"] = "Login to account";
+$string["loginSuccess"] = "You successfully login to your account!";
+$string["loginAlready"] = "You already logged in!";
+$string["clickHere"] = "Dashboard";
+$string["enterUsername"] = "Enter nickname";
+$string["enterPassword"] = "Enter password";
+
+$string["changePassTitle"] = "Change password";
+$string["changedPass"] = "Password successfully changed! You need to login to you account again.";
+$string["wrongPass"] = "Wrong password!";
+$string["samePass"] = "Passwords you entered are the same!";
+$string["changePassDesc"] = "Here you can change your password!";
+$string["oldPassword"] = "Old password";
+$string["newPassword"] = "New password";
+$string["confirmNew"] = "Confirm password";
+
+$string["changeNickTitle"] = "Change nickname";
+$string["changedNick"] = "Nickname successfully changed! You need to login to you account again.";
+$string["wrongNick"] = "Wrong nickname!";
+$string["sameNick"] = "Nicknames you entered are the same!";
+$string["alreadyUsedNick"] = "Nickname you entered is already taken!";
+$string["changeNickDesc"] = "Here you can change your nickname!";
+$string["oldNick"] = "Old nickname";
+$string["newNick"] = " New nickname";
+$string["password"] = "Password";
+
+$string["packCreate"] = "Create a Map Pack";
+$string["packCreateTitle"] = "Create a Map Pack";
+$string["packCreateDesc"] = "Here you can create a Map Pack!";
+$string["packCreateSuccess"] = "You successfully created a Map Pack called";
+$string["packCreateOneMore"] = "One more Map Pack?";
+$string["packName"] = "Name of Map Pack";
+$string["color"] = "Color";
+$string["sameLevels"] = "You chose the same levels!";
+$string["show"] = "Show";
+
+$string["gauntletCreate"] = "Create Gauntlet";
+$string["gauntletCreateTitle"] = "Create Gauntlet";
+$string["gauntletCreateDesc"] = "Here you can create a Gauntlet!";
+$string["gauntletCreateSuccess"] = "You successfully created a Gauntlet!";
+$string["gauntletCreateOneMore"] = "One more Gauntlet?";
+$string["chooseLevels"] = "Choose levels!";
+$string["checkbox"] = "Confirm";
+$string["level1"] = "1 level";
+$string["level2"] = "2 level";
+$string["level3"] = "3 level";
+$string["level4"] = "4 level";
+$string["level5"] = "5 level";
+
+/*
+	REUPLOAD
+*/
+
+$string["reuploadBTN"] = "Upload";
+$string["errorGeneric"] = "Error appeared!";
+$string["tryAgainBTN"] = "Try again";
+//songAdd.php
+$string["songAddUrlFieldLabel"] = "Song URL: (Direct or Dropbox links only)";
+$string["songAddUrlFieldPlaceholder"] = "Song URL";
+$string["songAddNameFieldPlaceholder"] = "Name";
+$string["songAddAuthorFieldPlaceholder"] = "Author";
+$string["songAddButton"] = "Choose song";
+$string["songAddAnotherBTN"] = "Another Song?";
+$string["songAdded"] = "Song added!";
+$string["songID"] = "Song ID: ";
+$string["songIDw"] = "Song ID";
+$string["songAuthor"] = "Author";
+$string["size"] = "Size";
+$string["delete"] = "Delete";
+$string["change"] = "Change";
+///errors
+$string["songAddError-2"] = "Invalid URL";
+$string["songAddError-3"] = "This song has been reuploaded already";
+$string["songAddError-4"] = "This song isn't reuploadable";
+$string["songAddError-5"] = "Song size is higher than 12 megabytes";
+$string["songAddError-6"] = "Something went wrong while uploading a song! :с";
+$string["songAddError-7"] = "You can only upload audio!";
+
+$string["page"] = "Page";
+
+/*
+	STATS
+*/
+$string["ID"] = "ID";
+$string["stars"] = "Stars";
+$string["coins"] = "Coins";
+$string["accounts"] = "Accounts";
+$string["levels"] = "Levels";
+$string["songs"] = "Songs";
+$string["author"] = "Creator";
+$string["name"] = "Name";
+$string["date"] = "Date";
+$string["username"] = "Nickname";
+$string["accountID"] = "Account ID";
+$string["registerDate"] = "Register date";
+$string["levelAuthor"] = "Level author";
+$string["isAdmin"] = "Role on server";
+$string["isAdminYes"] = "Yes";
+$string["isAdminNo"] = "No";
+$string["userCoins"] = "User Coins";
+$string["time"] = "Time";
+$string["deletedLevel"] = "Deleted Level";
+$string["mod"] = "Moderator";
+$string["count"] = "Amount of actions";
+$string["ratedLevels"] = "Rated Levels";
+$string["lastSeen"] = "Last Time Online";
+$string["level"] = "Level";
+$string["pageInfo"] = "Showing page %s of %s";
+$string["first"] = "First";
+$string["previous"] = "Previous";
+$string["next"] = "Next";
+$string["never"] = "Never";
+$string["last"] = "Last";
+$string["go"] = "Go";
+$string["levelid"] = "Level ID";
+$string["levelname"] = "Level name";
+$string["leveldesc"] = "Level description";
+$string["noDesc"] = "No description";
+$string["levelpass"] = "Password";
+$string["nopass"] = "No password";
+$string["unrated"] = "Unrated";
+$string["rate"] = "Rate";
+$string["suggestFeatured"] = "Featured?";
+//modActionsList
+$string["banDesc"] = "Here you can ban a player from leaderboard!";
+
+$string["admin"] = "Administrator";
+$string["elder"] = "Elder moderator";
+$string["moder"] = "Moderator";
+$string["player"] = "Player";
+
+$string["starsLevel2"] = "stars";
+$string["starsLevel1"] = "stars";
+$string["starsLevel0"] = "star";
+$string["coins1"] = "coins";
+$string["coins0"] = "coin";
+
+$string["accid"] = "with account ID";
+$string["banned"] = "was successfully banned!";
+$string["unbanned"] = "was successfully unbanned!";
+$string["ban"] = "Ban";
+$string["nothingFound"] = "This player doesn't exist!";
+$string["banUserID"] = "Nickname or account ID";
+$string["banUserPlace"] = "Ban a user";
+$string["banYourself"] = "You can't ban yourself!"; 
+$string["banYourSelfBtn!"] = "Ban someone else";
+$string["action"] = "Action";
+$string["value"] = "1st Value";
+$string["value2"] = "2nd Value";
+$string["modAction1"] = "Rated a level";
+$string["modAction2"] = "Un/featured a level";
+$string["modAction3"] = "Un/verified coins";
+$string["modAction4"] = "Un/epiced a level";
+$string["modAction5"] = "Set as daily feature";
+$string["modAction6"] = "Deleted a level";
+$string["modAction7"] = "Creator change";
+$string["modAction8"] = "Renamed a level";
+$string["modAction9"] = "Changed level password";
+$string["modAction10"] = "Changed demon difficulty";
+$string["modAction11"] = "Shared CP";
+$string["modAction12"] = "Un/published";
+$string["modAction13"] = "Changed level description";
+$string["modAction14"] = "Enabled/disabled LDM";
+$string["modAction15"] = "Leaderboard un/banned";
+$string["modAction16"] = "Song ID change";
+$string["modAction17"] = "Created a Map Pack";
+$string["modAction18"] = "Created a Gauntlet";
+$string["modAction19"] = "Changed song";
+$string["Kish!"] = "Go away!";
+$string["noPermission"] = "You don't have permission!";
+$string["noLogin?"] = "You are not logged into your account!";
+$string["LoginBtn"] = "Login into account";
+$string["dashboard"] = "Dashboard";
+//errors
+$string["errorNoAccWithPerm"] = "Error: No accounts with the '%s' permission have been found";
---
+++ incl/lang/localeRU.php
@@ -0,0 +1,232 @@
+<?php
+/*
+	NAVBAR
+*/
+$string["homeNavbar"] = "Домой";
+$string["welcome"] = "Добро пожаловать на GDPS!";
+$string["levelsWeek"] = "Выложено уровней за неделю";
+$string["levels3Months"] = "Выложено уровней за 3 месяца";
+
+$string["accountManagement"] = "Управление аккаунтом";
+$string["changePassword"] = "Сменить пароль";
+$string["changeUsername"] = "Сменить никнейм";
+$string["unlistedLevels"] = "Ваши скрытые уровни";
+
+$string["manageSongs"] = "Управление песнями";
+$string["gauntletManage"] = "Управление Гаунтлетами";
+$string["suggestLevels"] = "Предложенные уровни";
+
+$string["modTools"] = "Инструменты модератора";
+$string["leaderboardBan"] = "Забанить пользователя";
+$string["unlistedMod"] = "Cкрытые уровни";
+
+$string["reuploadSection"] = "Загрузка на сервер";
+$string["levelReupload"] = "Перенести уровень";
+$string["songAdd"] = "Добавить песню";
+$string["songLink"] = "Добавить песню по ссылке";
+$string["packManage"] = "Управление Мап-Паками";
+
+$string["browse"] = "Просмотр";
+$string["statsSection"] = "Статистика";
+$string["dailyTable"] = "Ежедневные уровни";
+$string["modActionsList"] = "Действия модераторов";
+$string["modActions"] = "Список модераторов";
+$string["gauntletTable"] = "Гаунтлеты";
+$string["packTable"] = "Мап-Паки";
+$string["leaderboardTime"] = "Таблица лидеров за 24 часа";
+
+$string["download"] = "Скачать";
+$string["forwindows"] = "Для Windows";
+$string["forandroid"] = "Для Android";
+$string["language"] = "Язык";
+
+$string["loginHeader"] = "Приветствуем, %s!";
+$string["logout"] = "Выйти";
+$string["login"] = "Войти";
+$string["loginBox"] = "Вход в аккаунт";
+$string["loginSuccess"] = "Вы успешно вошли в аккаунт!";
+$string["loginAlready"] = "Вы уже вошли в аккаунт!";
+$string["clickHere"] = "Панель сервера";
+$string["enterUsername"] = "Введите ваш никнейм";
+$string["enterPassword"] = "Введите ваш пароль";
+
+$string["changePassTitle"] = "Смена пароля";
+$string["changedPass"] = "Пароль успешно сменён! Вам нужно заново войти в ваш аккаунт.";
+$string["wrongPass"] = "Неверный пароль!";
+$string["samePass"] = "Введённые вами пароли одинаковые!";
+$string["changePassDesc"] = "Здесь вы можете сменить свой пароль!";
+$string["oldPassword"] = "Старый пароль";
+$string["newPassword"] = "Новый пароль";
+$string["confirmNew"] = "Подтверждение пароля";
+
+$string["changeNickTitle"] = "Смена никнейма";
+$string["changedNick"] = "Никнейм успешно сменён! Вам нужно заново войти в ваш аккаунт.";
+$string["wrongNick"] = "Неверный никнейм!";
+$string["sameNick"] = "Введённые вами никнеймы одинаковые!";
+$string["alreadyUsedNick"] = "Введённый вами никнейм уже занят!";
+$string["changeNickDesc"] = "Здесь вы можете сменить свой никнейм!";
+$string["oldNick"] = "Старый никнейм";
+$string["newNick"] = "Новый никнейм";
+$string["password"] = "Пароль";
+
+$string["packCreate"] = "Создать Мап-Пак";
+$string["packCreateTitle"] = "Создание Мап-Пака";
+$string["packCreateDesc"] = "Здесь вы можете создать Мап-Пак!";
+$string["packCreateSuccess"] = "Вы успешно создали Мап-Пак под названием";
+$string["packCreateOneMore"] = "Ещё один Мап-Пак?";
+$string["packName"] = "Название Мап-Пака";
+$string["color"] = "Цвет";
+$string["sameLevels"] = "Вы выбрали одинаковые уровни!";
+$string["show"] = "Показать";
+
+$string["gauntletCreate"] = "Создать Гаунтлет";
+$string["gauntletCreateTitle"] = "Создание Гаунтлета";
+$string["gauntletCreateDesc"] = "Здесь вы можете создать Гаунтлет!";
+$string["gauntletCreateSuccess"] = "Вы успешно создали Гаунтлет!";
+$string["gauntletCreateOneMore"] = "Ещё один Гаунтлет?";
+$string["chooseLevels"] = "Выберите уровни!";
+$string["checkbox"] = "Подтвердить";
+$string["level1"] = "1 уровень";
+$string["level2"] = "2 уровень";
+$string["level3"] = "3 уровень";
+$string["level4"] = "4 уровень";
+$string["level5"] = "5 уровень";
+
+$string["addQuest"] = "Добавить квест";
+$string["addQuestDesc"] = "Здесь вы можете создать квест!";
+$string["questName"] = "Название квеста";
+$string["questAmount"] = "Необходимое кол-во";
+$string["questReward"] = "Награда";
+$string["questCreate"] = "Создать квест";
+$string["invalidPost"] = "Неверные данные!";
+$string["fewMoreQuests"] = "Советуем вам создать ещё несколько квестов.";
+$string["oneMoreQuest?"] = "Ещё один квест?";
+
+/*
+	REUPLOAD
+*/
+
+$string["reuploadBTN"] = "Загрузить";
+$string["errorGeneric"] = "Произошла ошибка!";
+$string["tryAgainBTN"] = "Попробуйте ещё раз";
+//songAdd.php
+$string["songAddDesc"] = "Здесь вы можете добавить свою песню!";
+$string["songAddUrlFieldLabel"] = "Ссылка на песню. (Прямая, Dropbox. YouTube не работает)";
+$string["songAddUrlFieldPlaceholder"] = "Ссылка на песню";
+$string["songAddNameFieldPlaceholder"] = "Название песни";
+$string["songAddAuthorFieldPlaceholder"] = "Автор песни";
+$string["songAddButton"] = "Выбрать песню";
+$string["songAddAnotherBTN"] = "Ещё одну песню?";
+$string["songAdded"] = "Песня загружена!";
+$string["songID"] = "ID вашей песни: ";
+$string["songIDw"] = "ID песни";
+$string["songAuthor"] = "Автор";
+$string["size"] = "Размер";
+$string["delete"] = "Удалить";
+$string["change"] = "Изменить";
+///errors
+$string["songAddError-2"] = "Неправильный URL (возможно, ссылка не прямая).";
+$string["songAddError-3"] = "Песня уже загружена!";
+$string["songAddError-4"] = "Эту песню нельзя загрузить :с";
+$string["songAddError-5"] = "Песня весит больше 12 мегабайт!";
+$string["songAddError-6"] = "Что-то случилось при загрузке песни! :с";
+$string["songAddError-7"] = "Загружать можно только аудиофайлы!";
+
+$string["page"] = "Страница";
+
+/*
+	STATS
+*/
+$string["ID"] = "ID";
+$string["orbs"] = "Орбы";
+$string["stars"] = "Звёзды";
+$string["coins"] = "Монеты";
+$string["accounts"] = "Аккаунты";
+$string["levels"] = "Уровни";
+$string["songs"] = "Песни";
+$string["author"] = "Автор";
+$string["name"] = "Название";
+$string["date"] = "Дата";
+$string["username"] = "Никнейм";
+$string["accountID"] = "ID аккаунта";
+$string["registerDate"] = "Дата регистрации";
+$string["levelAuthor"] = "Автор уровня";
+$string["isAdmin"] = "Роль на сервере";
+$string["isAdminYes"] = "Да";
+$string["isAdminNo"] = "Нет";
+$string["userCoins"] = "Пользовательские монеты";
+$string["time"] = "Время";
+$string["deletedLevel"] = "Удалённый уровень";
+$string["mod"] = "Модератор";
+$string["count"] = "Количество действий";
+$string["ratedLevels"] = "Оценённые уровни";
+$string["lastSeen"] = "Последний раз был онлайн";
+$string["level"] = "Уровень";
+$string["pageInfo"] = "Страница %s из %s";
+$string["first"] = "Первая страница";
+$string["previous"] = "Назад";
+$string["next"] = "Вперёд";
+$string["never"] = "Никогда";
+$string["last"] = "Последняя страница";
+$string["go"] = "Вперёд";
+$string["levelid"] = "ID уровня";
+$string["levelname"] = "Название уровня";
+$string["leveldesc"] = "Описание уровня";
+$string["noDesc"] = "Без описания";
+$string["levelpass"] = "Пароль";
+$string["nopass"] = "Без пароля";
+$string["unrated"] = "Не оценён";
+$string["rate"] = "Оценка";
+$string["suggestFeatured"] = "Featured?";
+//modActionsList
+$string["banDesc"] = "Здесь вы можете забанить игрока из лидерборда!";
+
+$string["admin"] = "Администратор";
+$string["elder"] = "Старший модератор";
+$string["moder"] = "Модератор";
+$string["player"] = "Игрок";
+
+$string["starsLevel2"] = "звёзд";
+$string["starsLevel1"] = "звезды";
+$string["starsLevel0"] = "звезда";
+$string["coins1"] = "монет";
+$string["coins0"] = "монета";
+
+$string["accid"] = "с ID аккаунта";
+$string["banned"] = "был успешно забанен!";
+$string["unbanned"] = "был успешно разбанен!";
+$string["nothingFound"] = "Такой пользователь не найден!";
+$string["ban"] = "Забанить";
+$string["banUserID"] = "Никнейм или ID игрока";
+$string["banUserPlace"] = "Забанить пользователя";
+$string["banYourself"] = "Вы не можете забанить себя!"; 
+$string["banYourSelfBtn!"] = "Забанить кого-то другого";
+$string["action"] = "Действие";
+$string["value"] = "1 значение";
+$string["value2"] = "2 значение";
+$string["modAction1"] = "Оценил(а) уровень";
+$string["modAction2"] = "Снял(а)/добавил(а) Featured у уровня";
+$string["modAction3"] = "Снял(а)/добавил(а) монеты уровню";
+$string["modAction4"] = "Снял(а)/добавил(а) Epic уровню";
+$string["modAction5"] = "Указал(а) уровень, как ежедневный";
+$string["modAction6"] = "Удалил(а) уровень";
+$string["modAction7"] = "Сменил(а) автора";
+$string["modAction8"] = "Переименовал(а) уровень";
+$string["modAction9"] = "Сменил(а) пароль уровню";
+$string["modAction10"] = "Сменил(а) демон-сложность уровню";
+$string["modAction11"] = "Поделился(лась) CP";
+$string["modAction12"] = "Скрыл(а)/показал(а) уровень";
+$string["modAction13"] = "Сменил(а) описание уровню";
+$string["modAction14"] = "Включил(а)/выключил(а) LDM уровню";
+$string["modAction15"] = "Забанил(а)/разбанил(а) в лидербордах";
+$string["modAction16"] = "Сменил(а) песню уровню";
+$string["modAction17"] = "Создал(а) Мап-Пак";
+$string["modAction18"] = "Создал(а) Гаунтлет";
+$string["modAction19"] = "Изменил(а) песню";
+$string["Kish!"] = "Кыш!";
+$string["noPermission"] = "У вас нет прав!";
+$string["noLogin?"] = "Вы не вошли в аккаунт!";
+$string["LoginBtn"] = "Войти в аккаунт";
+$string["dashboard"] = "Панель сервера";
+//errors
+$string["errorNoAccWithPerm"] = "Ошибка: аккаунтов с правом '%s' не было найдено";
