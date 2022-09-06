a35f6af21dd59acd0535f91d6aad92ba374114b1
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2022-09-06 18:38:05 +0300

    Now errors appearing automatically

--- dashboard/errors/500/index.php
+++ dashboard/errors/500/index.php
@@ -1,9 +1,33 @@
+<style>
+* {
+	background: #36393e;
+}
+  .error {
+	display: flex;
+    margin: 15vw 0px 0px 0px;
+    padding: 0;
+    font-size: 200;
+    color: #c0c0c0;
+    align-items: center;
+    font-weight: 700;
+    text-shadow: 0px 0px 10px black;
+    justify-content: center;
+    cursor: pointer;
+}
+.errtext {
+	display: flex;
+    margin: 0;
+    font-size: 35px;
+    justify-content: center;
+	color: #c0c0c0;
+    text-shadow: 0px 0px 5px black;
+}</style>
 <?php
-session_start();
-require "../../incl/dashboardLib.php";
-$dl = new dashboardLib();
-$dl->title(500);
-$dl->printFooter('../../');
-$dl->error('<a href="../"><p class="error">500</p></a><br>
-<p class="errtext">'.$dl->getLocalizedString("500").'</p>');
+$path = str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['SCRIPT_FILENAME']);
+$path =  str_replace('/errors/500/index.php', '', $path);
+include $_SERVER['DOCUMENT_ROOT'].''.$path.'/../config/dashboard.php';
+if($_COOKIE["lang"] == 'RU') $text = 'Внутренняя ошибка сервера!'; else $text = 'Internal server error!';
+echo '<title>500 | '.$gdps.'</title>
+<a href="'.$path.'"><p class="error">500</p></a>
+<p class="errtext">'.$text.'</p>';
 ?>
