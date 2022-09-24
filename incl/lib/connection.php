20c56a7c632e848c0ba129b649e101db9f1bbb95
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2022-09-24 18:02:37 +0500

    Update connection.php

--- incl/lib/connection.php
+++ incl/lib/connection.php
@@ -2,7 +2,7 @@
 //error_reporting(0);
 include dirname(__FILE__)."/../../config/connection.php";
 require_once dirname(__FILE__)."/ipCheck.php";
-$gs = new ipCheck();
+$ic = new ipCheck();
 date_default_timezone_set('Europe/Moscow');
 @header('Content-Type: text/html; charset=utf-8');
 if(!isset($port))
@@ -10,7 +10,7 @@ if(!isset($port))
 // banip check
     $banipcheck = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password, array(PDO::ATTR_PERSISTENT => true));
     $banip = $banipcheck->prepare("SELECT IP FROM bannedips WHERE IP=:ip");
-  	$banip->execute([':ip' => $gs->getYourIP()]);
+  	$banip->execute([':ip' => $ic->getYourIP()]);
   	$banip2 = $banip->fetch();
   	if($banip2 != 0) exit(-1);
 try {
