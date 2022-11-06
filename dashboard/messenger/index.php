b2859399fbe1707e17e7be7dd63c397cce7cbd18
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2022-11-06 12:07:12 +0500

    Crying smile when you don't have friend

--- dashboard/messenger/index.php
+++ dashboard/messenger/index.php
@@ -122,6 +122,7 @@ $(document).change(function(){
 			$options .= '<div class="messenger"><text class="receiver">'.$receiver.''.$notify.'</text><br>
 			<button type="submit" class="btn-rendel" name="accountID" value='.$recid.' style="margin-top:5px">'.$dl->getLocalizedString("write").'</button></div>';
 		}
+		if(empty($options)) $options = '<div class="icon" style="height: 70px;width: 70px;margin-left: 0px;background:#36393e"><text class="receiver" style="font-size:50px"><i class="fa-regular fa-face-sad-cry"></i></text></div>';
 		$dl->printSong('<div class="form">
 			<h1>'.$dl->getLocalizedString("messenger").'</h1>
 			<form class="form__inner" method="post" action="messenger/">
