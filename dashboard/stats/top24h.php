31c896850a7e552a2241ef462a09cbf086600b6d
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2022-11-18 22:33:26 +0500

    Update top 24

--- dashboard/stats/top24h.php
+++ dashboard/stats/top24h.php
@@ -15,9 +15,9 @@ if(isset($_GET["page"]) AND is_numeric($_GET["page"]) AND $_GET["page"] > 0){
 	$page = 0;
 	$actualpage = 1;
 }
-$table = '<table class="table table-inverse"><tr><th>#</th><th>'.$dl->getLocalizedString("accountID").'</th><th>'.$dl->getLocalizedString("username").'</th><th>'.$dl->getLocalizedString("stars").'</th></tr>';
+$table = '<table class="table table-inverse"><tr><th>#</th><th>'.$dl->getLocalizedString("accountID").'</th><th>'.$dl->getLocalizedString("username").'</th><th>'.$dl->getLocalizedString("stars").'</th><th>'.$dl->getLocalizedString("coins").'</th></tr>';
 $time = time() - 86400;
-$query = $db->prepare("SELECT users.extID, SUM(actions.value) AS stars, users.userName FROM actions INNER JOIN users ON actions.account = users.userID WHERE type = '9' AND timestamp > :time AND users.isBanned = 0 GROUP BY (stars) DESC ORDER BY stars DESC LIMIT 10");
+$query = $db->prepare("SELECT users.extID, SUM(actions.value) AS stars, users.userName FROM actions INNER JOIN users ON actions.account = users.userID WHERE type = '9' AND timestamp > :time AND users.isBanned = 0 GROUP BY (stars) DESC ORDER BY stars DESC LIMIT 10 OFFSET $page");
 $query->execute([':time' => $time]);
 $result = $query->fetchAll();
 $x = $page + 1;
@@ -35,11 +35,20 @@ foreach($result as &$action){
 	$userid = $action["extID"];
 	$username =  '<form style="margin:0" method="post" action="profile/"><button style="margin:0" class="accbtn" name="accountID" value="'.$action["extID"].'">'.$action["userName"].'</button></form>';
 	$stars = $action["stars"];
+	$strs = end(str_split($stars));
+	if($strs == 1) $star = 0; elseif($strs < 5 AND $strs != 0 AND $stars > 20 OR $stars < 10) $star = 1; else $star = 2;
+    $stars = $stars.' '.$dl->getLocalizedString("starsLevel$star");
 	if($stars < 1) break;
-	$table .= "<tr><th scope='row'>".$x."</th><td>".$userid."</td><td>".$username."</td><td>".$stars."</td></tr>";
+	$coin = $db->prepare("SELECT coins FROM levelscores WHERE accountID = :id");
+	$coin->execute([':id' => $userid]);
+	$coins = $coin->fetchColumn();
+	if(empty($coins)) $coins = 0;
+	if($coins > 1) $coin = 1; elseif($coins == 0) $coin = 2; else $coin = 0;
+	$coins = $coins.' '.$dl->getLocalizedString("coins$coin");
+ 	$table .= "<tr><th scope='row'>".$x."</th><td>".$userid."</td><td>".$username."</td><td>".$stars."</td><td>".$coins."</td></tr>";
 	$x++;
 }
-if(empty(str_replace( '<table class="table table-inverse"><tr><th>#</th><th>'.$dl->getLocalizedString("accountID").'</th><th>'.$dl->getLocalizedString("username").'</th><th>'.$dl->getLocalizedString("stars").'</th></tr>', '', $table))) {
+if(empty(str_replace( '<table class="table table-inverse"><tr><th>#</th><th>'.$dl->getLocalizedString("accountID").'</th><th>'.$dl->getLocalizedString("username").'</th><th>'.$dl->getLocalizedString("stars").'</th><th>'.$dl->getLocalizedString("coins").'</th></tr>', '', $table))) {
 	$dl->printSong('<div class="form">
     <h1>'.$dl->getLocalizedString("errorGeneric").'</h1>
     <form class="form__inner" method="post" action=".">
@@ -54,9 +63,10 @@ $table .= "</table>";
 	bottom row
 */
 //getting count
-$packcount = count($result);
+$query = $db->prepare("SELECT users.extID, SUM(actions.value) AS stars, users.userName FROM actions INNER JOIN users ON actions.account = users.userID WHERE type = '9' AND timestamp > :time AND users.isBanned = 0 GROUP BY (stars) DESC ORDER BY stars DESC");
+$query->execute([':time' => $time]);
+$packcount = count($query->fetchColumn());
 $pagecount = ceil($packcount / 10);
 $bottomrow = $dl->generateBottomRow($pagecount, $actualpage);
 $dl->printPage($table . $bottomrow, true, "stats");
-
 ?>
