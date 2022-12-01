9beda41e12e91ee35422a38ee29ab17c4bf3fbaa
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2022-12-01 18:27:40 +0500

    Unused

--- tools/index.php
+++ tools/index.php
@@ -1,21 +1,16 @@
-<a href="../dashboard">Check out the dashboard beta here</a>
-<?php
-function listdir($dir){
-	$dirstring = "";
-	$files = scandir($dir);
-	foreach($files as $file) {
-		if(pathinfo($file, PATHINFO_EXTENSION) == "php" AND $file != "index.php"){
-			$dirstring .= "<li><a href='$dir/$file'>$file</a></li>";
+<html>
+	<head>
+		<title>Use Dashboard!</title>
+	</head>
+	<body>
+		<h1 style="font-size:75px;color:white;display:flex;align-items: center;margin: 0;height: 100%;text-shadow: 0 0 10px black;justify-content: center;">Use Dashboard!</h1>
+	<style>* {
+			background-color: #36393e;
 		}
-	}
-	return $dirstring;
-}
-echo '<h1>Account management tools:</h1><ul>';
-echo listdir("account");
-echo'</ul><h1>Upload related tools:</h1><ul>';
-echo listdir(".");
-echo "</ul><h1>The cron job (fixing CPs, autoban, etc.)</h1><ul>";
-echo "<li><a href='cron/cron.php'>cron.php</a></li>";
-echo "</ul><h1>Stats related tools</h1><ul>";
-echo listdir("stats");
-?>
+		body {
+			height: 100%;
+			margin: 0;
+		}
+	</style>
+	</body>
+</html>
