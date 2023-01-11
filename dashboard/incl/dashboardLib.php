c3b837c9740d5c58922436fe8653335be400dafb
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2023-01-11 19:10:11 +0500

    😇

--- dashboard/incl/dashboardLib.php
+++ dashboard/incl/dashboardLib.php
@@ -142,7 +142,7 @@ class dashboardLib {
 				break;
 		}
 		echo '<nav id="navbarepta" class="navbar navbar-expand-lg navbar-dark menubar">
-			<a class="navbar-brand" href="" style="margin-right:0.5rem"><img style="width:39px" src="icon.png"></a>
+			<a class="navbar-brand" href="" style="margin-right:0.5rem"><img style="width:30px" src="icon.png"></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
