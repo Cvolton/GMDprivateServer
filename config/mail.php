975225aef1110a56f1e5d7868fda86d295a6174e
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2023-02-01 20:41:32 +0500

    More accurate (worked for me)

--- config/mail.php
+++ config/mail.php
@@ -1,8 +1,8 @@
 <?php
 $mailEnabled = 0; // 0 - off, 1 - on
 
-$mailbox = 'mailbox';
-$mailuser = 'user';
+$mailbox = '{mail.example.com:143/novalidate-cert}INBOX';
+$mailuser = 'user@example.com';
 $mailpass = 'pass';
 $flags = 0;
 $retries = 0;
