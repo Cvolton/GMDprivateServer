72cc32667b6c69456b3f01feff1b33fe3189af14
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2023-04-06 15:55:46 +0500

    Fixed

--- dashboard/clans/index.php
+++ dashboard/clans/index.php
@@ -24,15 +24,15 @@ $style = $closed = $create = "";
 foreach($clans as &$clan) {
 	$name = base64_decode($clan["clan"]);
 	$desc = base64_decode($clan["desc"]);
-	$members = $db->prepare("SELECT clan FROM users WHERE clan = :id");
+	$members = $db->prepare("SELECT count(clan) FROM users WHERE clan = :id");
 	$members->execute([':id' => $clan["ID"]]);
-	$members = count($members->fetchAll()) - 1;
+	$members = $members->fetchColumn() - 1;
 	$dontmind = mb_substr($members, -1);
     if($dontmind == 1) $dm = 0; elseif($dontmind < 5 AND $dontmind > 0) $dm = 1; else $dm = 2;
     if($members > 9 AND $members < 20) $dm = 2;
 	if($clan["isClosed"] == 1) $closed = ' <i style="font-size:15px;color:#36393e" class="fa-solid fa-lock"></i>';
 	$options .= '<div class="profile" style="display: inherit;border-radius: 30px;margin-top: 15px;flex-wrap: nowrap;padding: 0 5 15 20;min-width: 100%;justify-content: space-between;height: max-content;margin-bottom: 0px;align-items: center;"><div style="width: 100%;display: flex;height: 100%;flex-wrap: wrap;flex-direction: column;justify-content: space-between;"><div style="margin-right: 10px;">
-		<div style="display: flex;justify-content: space-between;align-items: center;"><h1 style="width:max-content;text-align:left">'.sprintf($dl->getLocalizedString('demonlistLevel'), '<span style="color:#'.$clan["color"].';grid-gap: 3px;display: inline-flex;">'.$name.$closed.'</span>', $clan["clanOwner"], $gs->getAccountName($clan["clanOwner"])).'</h1><div style="display: flex;grid-gap: 5px;align-items: center;"><h3 style="margin: 0;font-size: 20px;">'.sprintf($dl->getLocalizedString("members".$dm), $membercount).'</h3><button style="width:max-content;padding:15px;height:max-content" type="button" onclick="a(\'clan/'.$name.'\', true, true)" class="btn-rendel"><i class="fa-solid fa-magnifying-glass"></i></button></div></div>
+		<div style="display: flex;justify-content: space-between;align-items: center;"><h1 style="width:max-content;text-align:left">'.sprintf($dl->getLocalizedString('demonlistLevel'), '<span style="color:#'.$clan["color"].';grid-gap: 3px;display: inline-flex;">'.$name.$closed.'</span>', $clan["clanOwner"], $gs->getAccountName($clan["clanOwner"])).'</h1><div style="display: flex;grid-gap: 5px;align-items: center;"><h3 style="margin: 0;font-size: 20px;">'.sprintf($dl->getLocalizedString("members".$dm), $members).'</h3><button style="width:max-content;padding:15px;height:max-content" type="button" onclick="a(\'clan/'.$name.'\', true, true)" class="btn-rendel"><i class="fa-solid fa-magnifying-glass"></i></button></div></div>
 		<p style="margin-bottom: 10px;width:100%;text-align:left">'.$desc.'</p></div></div>
 	</div>';
 }
