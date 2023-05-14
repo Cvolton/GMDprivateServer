c24e2fee6afcc7131f090bee5649cf6a858eac24
masckmaster2007 <63162857+masckmaster2007@users.noreply.github.com>
2023-05-14 17:13:15 +0200

    trad fr :)

--- dashboard/incl/lang/localeFR.php
+++ dashboard/incl/lang/localeFR.php
@@ -13,22 +13,22 @@ $string["tipsAfterInstalling"] = "Bienvenue au tableau de bord! Nous te donnons 
 1. Il y a de nouvelles permissions dans la base de données SQL dans la table 'roles'! Vous devriez y jeter un coup d'oeil...<br>
 2. Si vous placez 'icon.png' dans le dossier 'dashboard', alors le logo de votre GDPS va apparaître en haut à gauche!<br>
 3. Vous devriez configurer config/dashboard.php!";
-$string["wwygdt"] = "What are you gonna do today?";
-$string["game"] = "Game";
-$string["guest"] = "guest";
-$string["account"] = "Account";
-$string["levelsOptDesc"] = "View levels list";
-$string["songsOptDesc"] = "View songs list";
-$string["yourClanOptDesc"] = "View clan \"%s\"";
-$string["clanOptDesc"] = "View clans list";
-$string["yourProfile"] = "Your profile";
-$string["profileOptDesc"] = "View your profile";
-$string["messengerOptDesc"] ="Open messenger";
-$string["addSongOptDesc"] = "Add song to server";
-$string["loginOptDesc"] = "Login to account";
-$string["createAcc"] = "Create an account";
-$string["registerOptDesc"] = "Register on %s";
-$string["downloadOptDesc"] = "Download %s";
+$string["wwygdt"] = "Qu'allez vous faire aujourd'hui?";
+$string["game"] = "Jeu";
+$string["guest"] = "invité";
+$string["account"] = "Compte";
+$string["levelsOptDesc"] = "Voir la liste des nixeaux";
+$string["songsOptDesc"] = "Voir la liste des chansons";
+$string["yourClanOptDesc"] = "Voir la liste du clan \"%s\"";
+$string["clanOptDesc"] = "Voir la liste des clans";
+$string["yourProfile"] = "Votre profile";
+$string["profileOptDesc"] = "Voir votre profile";
+$string["messengerOptDesc"] ="Ouvrir la messagerie";
+$string["addSongOptDesc"] = "Ajouter une chanson dans votre serveur";
+$string["loginOptDesc"] = "Se connecter au compte";
+$string["createAcc"] = "Créer un compte";
+$string["registerOptDesc"] = "Créer dans %s";
+$string["downloadOptDesc"] = "Télécharger %s";
 
 $string["tryCron"] = "Exécuter Cron";
 $string["cronSuccess"] = "Succès!";
@@ -101,7 +101,7 @@ $string["loginAlready"] = "Vous êtes déjà connecté!";
 $string["clickHere"] = "Tableau de bord";
 $string["enterUsername"] = "Entrez le nom d'utilisateur";
 $string["enterPassword"] = "Entrez le mot de passe";
-$string["loginDesc"] = "Here you can login to your account!";
+$string["loginDesc"] = "Tu peux to connecter dans ton compte ici!";
 
 $string["register"] = "S'inscrire";
 $string["registerAcc"] = "Inscription du compte";
