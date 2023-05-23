6da7261f7f6908fc3298ced8c9a58efae50c21b8
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2023-05-24 02:49:18 +0500

    Fixed captchas part 2

--- incl/lib/Captcha.php
+++ incl/lib/Captcha.php
@@ -9,13 +9,13 @@ class Captcha {
 				$very = 'very';
 			}
 			if ($captchaType == 1) {
-				echo "<script src='https://js.hCaptcha.com/1/api.js' async defer></script>";
+				echo "<script src='https://js.hCaptcha.com/1/api.js' id='captchascript' async defer></script>";
 				echo "<div class=\"h-captcha\" $vis id=\"" . $very . "coolcaptcha\" data-sitekey=\"${CaptchaKey}\" data-theme='dark' style='border-width: 0px !important;border-radius: 20px !important;'></div>";
 			} elseif ($captchaType == 2) {
-				echo "<script src='https://www.google.com/recaptcha/api.js' async defer></script>";
+				echo "<script src='https://www.google.com/recaptcha/api.js' id='captchascript' async defer></script>";
 				echo "<div class=\"g-recaptcha\" $vis id=\"" . $very . "coolcaptcha\" data-sitekey=\"${CaptchaKey}\"></div>";
 			} elseif ($captchaType == 3) {
-				echo "<script src='https://challenges.cloudflare.com/turnstile/v0/api.js' async defer></script>";
+				echo "<script src='https://challenges.cloudflare.com/turnstile/v0/api.js' id='captchascript' async defer></script>";
 				echo "<div class=\"cf-turnstile\" $vis id=\"" . $very . "coolcaptcha\" data-sitekey=\"${CaptchaKey}\" data-callback=\"javascriptCallback\"></div>";
 			}
 		}
