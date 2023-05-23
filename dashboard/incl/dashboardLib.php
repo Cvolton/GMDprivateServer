e3441c41958db06252a974ef4cf0e4f8f9789e78
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2023-05-24 04:14:55 +0500

    Yeah

--- dashboard/incl/dashboardLib.php
+++ dashboard/incl/dashboardLib.php
@@ -17,7 +17,7 @@ class dashboardLib {
 						<meta charset="utf-8">
 						<meta name="color-scheme" content="dark">
 						<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit="no">';
-          	if($isSubdirectory) echo '<base href="../">';
+          	if($isSubdirectory) echo '<base href="../">'; else echo '<base href=".">';
 				echo '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
                           <script src="https://kit.fontawesome.com/10e18026cb.js" crossorigin="anonymous"></script>
                           <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
