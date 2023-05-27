8a81867292bd008f02f976ad62e2d37fc264aa0f
0x1DEA <seebeyondedm@gmail.com>
2023-05-27 08:54:45 -0500

    Fix NUMEROUS egregious errors from comments on PR #1002

--- incl/lib/LevelParser.php
+++ incl/lib/LevelParser.php
@@ -1,12 +1,12 @@
 <?php
 
+/**
+ * In the future use this class for level validation. Data or other parameters.
+ */
 class LevelParser {
     public $data = [];
-
-    public function __construct($levelString)
-    {
-        $this->data = self::map($levelString, ':');
-    }
+    const MAX_GROUPS_21 = 1099;
+    const MAX_GROUPS_22 = 9999;
 
     /**
      * Convert GD's weird "keyed" array strings to dictionary. Works with out-of-order keys.
@@ -31,7 +31,7 @@ class LevelParser {
      * @param $separator string
      * @return string
      */
-    public static function demap($dict, $separator) {
+    public static function unmap($dict, $separator) {
         $string = '';
         foreach ($dict as $key => $value) {
             $string[] .= "${separator}${key}${separator}${value}";
@@ -55,23 +55,53 @@ class LevelParser {
         return base64_decode(strtr($string, '-_', '+/'), true);
     }
 
+    public static function decodeAllB64($string) {
+        // https://www.debugpointer.com/regex/regex-for-base64
+        // In case of emergency break glass. This may work as an alternative due to lenient php function or very large level string
+        // if (preg_match('/^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}={2})$/'. $string)) {
+        if (base64_encode(base64_decode($string, true)) === $string) {
+
+        }
+
+        if (strpos('-') === false || strpos('_') === false) {
+
+        }
+    }
+
     /**
      * Validates level data for various things (currently only ACE exploit)
      *
      * @return bool
      */
-    public function validate() {
-        // Ocular Miracle ain't happening
-        $max_level_size = 50 * 1024 * 1024; // 50 MB Max uncompressed size
-        $objs = explode(';', zlib_decode(self::base64_urldecode($this->data[4]), $max_level_size));
-        // Skip level header
-        for ($i = 1; $i < 2; $i++) {
-            $obj = self::map($objs[$i], ',');
-            // Check for pickup trigger exploit
-            if ($obj[1] == 1817) {
-                $id = intval($obj[80]);
-                if ($id > 1099 || $id < 0) return false;
+    public static function validate($levelString, $version) {
+        include '../../config/security.php';
+        try {
+            $data = $levelString;
+            // Decode (strict mode) if falsy then
+            $decoded = self::base64_urldecode($levelString);
+            if ($decoded !== false) $data = $decoded;
+
+            // Check for zlib magic
+            if(substr($data, 0, 3) === "\x1b\x8b\x08") {
+                $data = zlib_decode($data, $maxUncompressedLevelSize);
+                if (!$data) {
+                    return false;
+                }
             }
+            // Check if result invalid (any character outside ascii range). Better heuristic for detecting junk?
+            if (preg_match('/[^\x20-\x7e]/', $data)) return false;
+            $objs = explode(';', $data);
+            // Skip level header
+            for ($i = 1; $i < 2; $i++) {
+                $obj = self::map($objs[$i], ',');
+                // Clamp groups based on version
+                if (array_key_exists(80, $obj)) {
+                    $id = intval($obj[80]);
+                    if ($id > ($version > 21 ? self::MAX_GROUPS_22 : self::MAX_GROUPS_21) || $id < 0) return false;
+                }
+            }
+        } catch (Exception $e) {
+            return false;
         }
 
         return true;
