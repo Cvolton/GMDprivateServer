4036655240472168922b6b37585a2ad98b4e4a07
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2023-05-30 11:13:45 +0500

    Trying to fix mail part 1

--- config/mail.php
+++ config/mail.php
@@ -5,5 +5,6 @@ $mailbox = 'mail.example.com'; // Your SMTP mail host
 $mailport = '587'; // Your SMTP mail port
 $mailuser = 'noreply@example.com'; // Your mail login
 $mailpass = 'password'; // Your mail password
+$mailtype = false; // Mail security type (ssl or tls), set to false to disable security
 $yourmail = 'noreply@example.com'; // Mail, that you will use (its same to $mailuser ig)
 ?>
