7308d698c6b6352a2d8d7cffb7a5bc7cdfb83803
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2023-11-09 02:59:29 +0500

    Untested, should work

--- incl/lib/connection.php
+++ incl/lib/connection.php
@@ -5,22 +5,21 @@ include_once dirname(__FILE__)."../../config/security.php";
 @header('Content-Type: text/html; charset=utf-8');
 if(!isset($port))
 	$port = 3306;
-// banip check
-if($activeBanIP) {
-    require_once dirname(__FILE__)."/ipCheck.php";
-    $ic = new ipCheck();
-    $banipcheck = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password, array(PDO::ATTR_PERSISTENT => true));
-    $banip = $banipcheck->prepare("SELECT IP FROM bannedips WHERE IP=:ip");
-  	$banip->execute([':ip' => $ic->getYourIP()]);
-  	$banip2 = $banip->fetch();
-  	if($banip2 != 0) exit(-1);
-}
 try {
     $db = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password, array(
     PDO::ATTR_PERSISTENT => true
 ));
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// banip check
+	if($activeBanIP) {
+	    require_once dirname(__FILE__)."/ipCheck.php";
+	    $ic = new ipCheck();
+	    $banip = $db->prepare("SELECT IP FROM bannedips WHERE IP=:ip");
+	  	$banip->execute([':ip' => $ic->getYourIP()]);
+	  	$banip = $banip->fetch();
+	  	if($banip != 0) exit(-1);
+	}
     }
 catch(PDOException $e)
     {
