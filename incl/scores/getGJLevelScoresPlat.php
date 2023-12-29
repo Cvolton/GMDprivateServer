695d76011a3d27a7f75d4817dbb3151617486f05
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2023-12-29 20:15:19 +0500

    Fix 5 (aahhhh)

--- incl/scores/getGJLevelScoresPlat.php
+++ incl/scores/getGJLevelScoresPlat.php
@@ -19,12 +19,12 @@ $query2 = $db->prepare("SELECT {$mode} FROM platscores WHERE accountID = :accoun
 $query2->execute([':accountID' => $accountID, ':levelID' => $levelID]);
 $oldPercent = $query2->fetchColumn();
 if($query2->rowCount() == 0) {
-	if(($mode == "time" AND $scores['time'] > 0) OR ($mode == "points" AND ($scores['time'] > 0 AND $scores['points'] > 0))) {
-		$query = $db->prepare("INSERT INTO platscores (accountID, levelID, {$mode}, timestamp) VALUES (:accountID, :levelID, :{$mode}, :timestamp)");
-		$query->execute([':accountID' => $accountID, ':levelID' => $levelID, ":{$mode}" => $scores[$mode], ':timestamp' => $uploadDate]);
+	if($scores['time'] > 0) {
+		$query = $db->prepare("INSERT INTO platscores (accountID, levelID, time, timestamp) VALUES (:accountID, :levelID, :time, :timestamp)");
+		$query->execute([':accountID' => $accountID, ':levelID' => $levelID, ":time" => $scores['time'], ':timestamp' => $uploadDate]);
 	}
 } else {
-	if(($mode == "time" AND $oldPercent > $scores['time'] AND $scores['time'] > 0) OR ($mode == "points" AND $oldPercent < $scores['points'] AND ($scores['time'] > 0 AND $scores['points'] > 0))) {
+	if((($mode == "time" AND $oldPercent > $scores['time']) OR ($mode == "points" AND $oldPercent < $scores['points'])) AND $scores['time'] > 0) {
 		$query = $db->prepare("UPDATE platscores SET {$mode}=:{$mode}, timestamp=:timestamp WHERE accountID=:accountID AND levelID=:levelID");
 	} else {
 		$query = $db->prepare("SELECT count(*) FROM platscores WHERE {$mode}=:{$mode} AND timestamp=:timestamp AND accountID=:accountID AND levelID=:levelID");
@@ -42,15 +42,15 @@ switch($type){
 		$friends = $gs->getFriends($accountID);
 		$friends[] = $accountID;
 		$friends = implode(",",$friends);
-		$query2 = $db->prepare("SELECT * FROM platscores WHERE levelID = :levelID AND accountID IN ($friends) AND (time > 0 OR points > 0) ORDER BY {$mode} {$order}");
+		$query2 = $db->prepare("SELECT * FROM platscores WHERE levelID = :levelID AND accountID IN ($friends) AND time > 0 ORDER BY {$mode} {$order}");
 		$query2args = [':levelID' => $levelID];
 		break;
 	case 1:
-		$query2 = $db->prepare("SELECT * FROM platscores WHERE levelID = :levelID AND (time > 0 OR points > 0) ORDER BY {$mode} {$order}");
+		$query2 = $db->prepare("SELECT * FROM platscores WHERE levelID = :levelID AND time > 0 ORDER BY {$mode} {$order}");
 		$query2args = [':levelID' => $levelID];
 		break;
 	case 2:
-		$query2 = $db->prepare("SELECT * FROM platscores WHERE levelID = :levelID AND timestamp > :time AND (time > 0 OR points > 0) ORDER BY {$mode} {$order}");
+		$query2 = $db->prepare("SELECT * FROM platscores WHERE levelID = :levelID AND timestamp > :time AND time > 0 ORDER BY {$mode} {$order}");
 		$query2args = [':levelID' => $levelID, ':time' => $uploadDate - 604800];
 		break;
 	default:
