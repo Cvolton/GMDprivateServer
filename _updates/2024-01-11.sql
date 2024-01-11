89c183b577af574a6518324f166ecfa18a3bcaf0
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2024-01-11 11:18:59 +0500

    INT

--- _updates/2024-01-11.sql
+++ _updates/2024-01-11.sql
@@ -1,2 +1,1 @@
-ALTER TABLE `users` ADD `dinfo` VARCHAR(2048) NULL DEFAULT '' AFTER `accJetpack`, ADD `dinfow` VARCHAR(2048) NULL DEFAULT '' AFTER `dinfo`, ADD `dinfog` VARCHAR(2048) NULL DEFAULT '' AFTER `dinfow`;
-/* IDK if dinfow and dinfog needs varchar instead of int */
+ALTER TABLE `users` ADD `dinfo` VARCHAR(2048) NULL DEFAULT '' AFTER `accJetpack`, ADD `dinfow` INT NULL DEFAULT 0 AFTER `dinfo`, ADD `dinfog` INT NULL DEFAULT 0 AFTER `dinfow`;
