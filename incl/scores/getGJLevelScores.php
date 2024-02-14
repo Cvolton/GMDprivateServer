127328d90522ce749cafae232957853edc68f28a
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2024-02-14 18:45:44 +0500

    False level score updating fix + lil cleanup

--- incl/scores/getGJLevelScores.php
+++ incl/scores/getGJLevelScores.php
@@ -30,32 +30,26 @@ if($query2->rowCount() == 0) {
 	$query = $db->prepare("INSERT INTO levelscores (accountID, levelID, percent, uploadDate, coins, attempts, clicks, time, progresses, dailyID)
 	VALUES (:accountID, :levelID, :percent, :uploadDate, :coins, :attempts, :clicks, :time, :progresses, :dailyID)");
 } else {
-	if($oldPercent <= $percent){
+	if($oldPercent < $percent) {
 		$query = $db->prepare("UPDATE levelscores SET percent=:percent, uploadDate=:uploadDate, coins=:coins, attempts=:attempts, clicks=:clicks, time=:time, progresses=:progresses, dailyID=:dailyID WHERE accountID=:accountID AND levelID=:levelID AND dailyID $condition 0");
-	}else{
+	} else {
 		$query = $db->prepare("SELECT count(*) FROM levelscores WHERE percent=:percent AND uploadDate=:uploadDate AND accountID=:accountID AND levelID=:levelID AND coins = :coins AND attempts = :attempts AND clicks = :clicks AND time = :time AND progresses = :progresses AND dailyID = :dailyID");
 	}
 }
 
 $query->execute([':accountID' => $accountID, ':levelID' => $levelID, ':percent' => $percent, ':uploadDate' => $uploadDate, ':coins' => $coins, ':attempts' => $attempts, ':clicks' => $clicks, ':time' => $time, ':progresses' => $progresses, ':dailyID' => $dailyID]);
-if($percent > 100){
+if($percent > 100 || $percent < 0) {
 	$query = $db->prepare("UPDATE users SET isBanned=1 WHERE extID = :accountID");
 	$query->execute([':accountID' => $accountID]);
 }
 
-
-
 //GETTING SCORES
-if(!isset($_POST["type"])){
-	$type = 1;
-}else{
-	$type = $_POST["type"];
-}
-switch($type){
+$type = $_POST['type'] ?? 1;
+switch($type) {
 	case 0:
 		$friends = $gs->getFriends($accountID);
 		$friends[] = $accountID;
-		$friends = implode(",",$friends);
+		$friends = implode(",", $friends);
 		$query2 = $db->prepare("SELECT accountID, uploadDate, percent, coins FROM levelscores WHERE dailyID $condition 0 AND levelID = :levelID AND accountID IN ($friends) ORDER BY percent DESC");
 		$query2args = [':levelID' => $levelID];
 		break;
@@ -80,17 +74,12 @@ foreach ($result as &$score) {
 	$extID = $score["accountID"];
 	$query2 = $db->prepare("SELECT userName, userID, icon, color1, color2, color3, iconType, special, extID, isBanned FROM users WHERE extID = :extID");
 	$query2->execute([':extID' => $extID]);
-	$user = $query2->fetchAll();
-	$user = $user[0];
+	$user = $query2->fetch();
 	$time = date("d/m/Y G.i", $score["uploadDate"]);
-	if($user["isBanned"]==0){
-		if($score["percent"] == 100){
-			$place = 1;
-		}else if($score["percent"] > 75){
-			$place = 2;
-		}else{
-			$place = 3;
-		}
+	if($user["isBanned"] == 0){
+		if($score["percent"] == 100) $place = 1;
+		else if($score["percent"] > 75) $place = 2;
+		else $place = 3;
 		echo "1:".$user["userName"].":2:".$user["userID"].":9:".$user["icon"].":10:".$user["color1"].":11:".$user["color2"].":51:".$user["color3"].":14:".$user["iconType"].":15:".$user["special"].":16:".$user["extID"].":3:".$score["percent"].":6:".$place.":13:".$score["coins"].":42:".$time."|";
 	}
 }
