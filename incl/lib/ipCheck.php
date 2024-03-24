dd8eaf8b886cca2e4c70966d864eee54ac40802c
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2024-03-25 01:31:27 +0500

    Doesn't work without that

--- incl/lib/ipCheck.php
+++ incl/lib/ipCheck.php
@@ -81,6 +81,7 @@ class ipCheck {
 	}
 	public function checkProxy() {
 		include_once __DIR__."/../../config/security.php";
+		global $blockFreeProxies;
 		if(!$blockFreeProxies) return;
 		$fileExists = file_exists(__DIR__ .'/../../config/proxies.txt');
 		$lastUpdate = $fileExists ? filemtime(__DIR__ .'/../../config/proxies.txt') : 0;
