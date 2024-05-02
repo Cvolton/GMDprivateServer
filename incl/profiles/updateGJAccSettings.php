3f057af82211d3e35a2a7a92d91d50477511e273
Xytriza <158840904+Xytriza@users.noreply.github.com>
2024-05-02 12:21:38 -0700

    filter the socials in game

--- incl/profiles/updateGJAccSettings.php
+++ incl/profiles/updateGJAccSettings.php
@@ -17,6 +17,10 @@ if(substr($youtubeurl, 0, 4) == "../@"){
     $youtubeurl = "@" . substr($youtubeurl, 4);
 }
 
+$youtubeurl = preg_replace("/(?!^@)[^a-zA-Z0-9_]/", "", $youtubeurl]);
+$twitter = preg_replace("/[^a-zA-Z0-9_]/", "", $twitter);
+$twitch = preg_replace("/[^a-zA-Z0-9_]/", "", $twitch);
+
 $query = $db->prepare("UPDATE accounts SET mS=:mS, frS=:frS, cS=:cS, youtubeurl=:youtubeurl, twitter=:twitter, twitch=:twitch WHERE accountID=:accountID");
 $query->execute([':mS' => $mS, ':frS' => $frS, ':cS' => $cS, ':youtubeurl' => $youtubeurl, ':accountID' => $accountID, ':twitch' => $twitch, ':twitter' => $twitter]);
 echo 1;
