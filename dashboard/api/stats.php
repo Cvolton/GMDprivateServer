4b7b48e720003bc46a5d121b90414ceefb6f7420
M336 <75208577+M336G@users.noreply.github.com>
2024-06-23 21:52:43 +0200

    Optimization & Bugfixes

--- dashboard/api/stats.php
+++ dashboard/api/stats.php
@@ -7,134 +7,94 @@ require_once "../incl/dashboardLib.php";
 include "../".$dbPath."incl/lib/connection.php";
 
 
-$query = $db->prepare("SELECT COUNT(*) FROM users");
-$query->execute();
-$users = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM users WHERE lastPlayed > :time - 2592000"); // 2592000 seconds = 30d
+// 2592000 seconds = 30d
+$query = $db->prepare("SELECT
+    (SELECT COUNT(*) FROM users) AS users,
+    (SELECT COUNT(*) FROM users WHERE lastPlayed > :time - 2592000) AS activeUsers, 
+    (SELECT COUNT(*) FROM levels) AS levels,
+    (SELECT COUNT(*) FROM levels WHERE starStars >= 1) AS ratedLevels,
+    (SELECT COUNT(*) FROM levels WHERE starStars >= 1 AND starFeatured >= 1 AND starEpic = 0) AS featuredLevels,
+    (SELECT COUNT(*) FROM levels WHERE starStars >= 1 AND starFeatured >= 1 AND starEpic = 1) AS epicLevels,
+    (SELECT COUNT(*) FROM levels WHERE starStars >= 1 AND starFeatured >= 1 AND starEpic = 2) AS legendaryLevels,
+    (SELECT COUNT(*) FROM levels WHERE starStars >= 1 AND starFeatured >= 1 AND starEpic = 3) AS mythicLevels,
+    (SELECT COUNT(*) FROM dailyfeatures WHERE type = 0) AS dailies,
+    (SELECT COUNT(*) FROM dailyfeatures WHERE type = 1) AS weeklies,
+    (SELECT COUNT(*) FROM gauntlets) AS gauntlets,
+    (SELECT COUNT(*) FROM mappacks) AS mapPacks,
+    (SELECT SUM(downloads) FROM levels) AS downloads,
+    (SELECT SUM(objects) FROM levels) AS objects,
+    (SELECT SUM(likes) FROM levels) AS likes,
+    (SELECT (SELECT COUNT(*) FROM comments) + (SELECT COUNT(*) FROM acccomments)) AS totalComments,
+    (SELECT COUNT(*) FROM comments) AS comments,
+    (SELECT COUNT(*) FROM acccomments) AS posts,
+    (SELECT COUNT(*) FROM replies) AS postReplies,
+    (SELECT SUM(stars) FROM users) AS stars,
+    (SELECT SUM(creatorPoints) FROM users) AS creatorPoints,
+    (SELECT COUNT(*) FROM users WHERE isBanned = 1 OR isCommentBanned = 1 OR isCreatorBanned = 1 OR isUploadBanned = 1) AS bannedPlayers,
+    (SELECT COUNT(*) FROM users WHERE isBanned = 1) AS leaderboardBanned,
+    (SELECT COUNT(*) FROM users WHERE isCommentBanned = 1) AS commentBanned,
+    (SELECT COUNT(*) FROM users WHERE isCreatorBanned = 1) AS creatorBanned,
+    (SELECT COUNT(*) FROM users WHERE isUploadBanned = 1) AS uploadBanned,
+    (SELECT COUNT(*) FROM bannedips) AS ipBanned
+");
 $query->execute([':time' => time()]);
-$activeUsers = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM levels");
-$query->execute();
-$levels = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM levels WHERE starStars >= 1");
-$query->execute();
-$ratedLevels = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM levels WHERE starStars >= 1 AND starFeatured >= 1 AND starEpic = 0");
-$query->execute();
-$featuredLevels = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM levels WHERE starStars >= 1 AND starFeatured >= 1 AND starEpic = 1");
-$query->execute();
-$epicLevels = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM levels WHERE starStars >= 1 AND starFeatured >= 1 AND starEpic = 2");
-$query->execute();
-$legendaryLevels = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM levels WHERE starStars >= 1 AND starFeatured >= 1 AND starEpic = 3");
-$query->execute();
-$mythicLevels = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM dailyfeatures WHERE type = 0");
-$query->execute();
-$dailies = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM dailyfeatures WHERE type = 1");
-$query->execute();
-$weeklies = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM gauntlets");
-$query->execute();
-$gauntlets = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM mappacks");
-$query->execute();
-$mapPacks = $query->fetchColumn();
-
-$query = $db->prepare("SELECT SUM(downloads) FROM levels");
-$query->execute();
-$downloads = $query->fetchColumn();
-
-$query = $db->prepare("SELECT SUM(objects) FROM levels");
-$query->execute();
-$objects = $query->fetchColumn();
-
-$query = $db->prepare("SELECT SUM(likes) FROM levels");
-$query->execute();
-$likes = $query->fetchColumn();
-
-$query = $db->prepare("SELECT (SELECT COUNT(*) FROM comments) + (SELECT COUNT(*) FROM acccomments)");
-$query->execute();
-$comments = $query->fetchColumn();
-
-$query = $db->prepare("SELECT SUM(stars) FROM users");
-$query->execute();
-$stars = $query->fetchColumn();
-
-$query = $db->prepare("SELECT SUM(creatorPoints) FROM users");
-$query->execute();
-$creatorPoints = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM users WHERE isBanned = 1 OR isCommentBanned = 1 OR isCreatorBanned = 1 OR isUploadBanned = 1");
-$query->execute();
-$bannedPlayers = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM users WHERE isBanned = 1");
-$query->execute();
-$leaderboardBanned = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM users WHERE isCommentBanned = 1");
-$query->execute();
-$commentBanned = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM users WHERE isCreatorBanned = 1");
-$query->execute();
-$creatorBanned = $query->fetchColumn();
-
-$query = $db->prepare("SELECT COUNT(*) FROM users WHERE isUploadBanned = 1");
-$query->execute();
-$uploadBanned = $query->fetchColumn();
+$stats = $query->fetch(PDO::FETCH_ASSOC);
 
 $stats = [
-    'users' => (int)$users,
-    'active_users' => (int)$activeUsers,
+    'users' => [
+        'total' => (int)$stats['users'],
+        'active' => (int)$stats['activeUsers']
+    ],
+    'levels' => [
+        'total' => (int)$stats['levels'],
+        'rated' => (int)$stats['ratedLevels'],
+        'featured' => (int)$stats['featuredLevels'],
+        'epic' => (int)$stats['epicLevels'],
+        'legendary' => (int)$stats['legendaryLevels'],
+        'mythic' => (int)$stats['mythicLevels']
+    ],
+    'special' => [
+        'dailies' => (int)$stats['dailies'],
+        'weeklies' => (int)$stats['weeklies'],
+        'gauntlets' => (int)$stats['gauntlets'],
+        'map_packs' => (int)$stats['mapPacks']
+    
+    ],
+    'downloads' => [
+        'total' => (int)$stats['downloads'],
+        'average' => (double)($stats['downloads'] / $stats['levels'])
+    ],
 
-    'levels' => (int)$levels,
-    'rated_levels' => (int)$ratedLevels,
-    'featured_levels' => (int)$featuredLevels,
-    'epic_levels' => (int)$epicLevels,
-    'legendary_levels' => (int)$legendaryLevels,
-    'mythic_levels' => (int)$mythicLevels,
-
-    'dailies' => (int)$dailies,
-    'weeklies' => (int)$weeklies,
-    'gauntlets' => (int)$gauntlets,
-    'map_packs' => (int)$mapPacks,
-
-    'downloads' => (int)$downloads,
-    'avg_downloads' => (double)$downloads / $levels,
-
-    'objects' => (int)$objects,
-    'avg_objects' => $objects / $levels,
-
-    'likes' => (int)$likes,
-    'avg_likes' => (double)$likes / $levels,
-
-    'comments' => (int)$comments,
-
-    'stars' => (int)$stars,
-    'creator_points' => (int)$creatorPoints,
-    'avg_creator_points' => (double)$creatorPoints / $users,
-
-    'banned_players' => (int)$bannedPlayers,
-    'leaderboard_banned' => (int)$leaderboardBanned,
-    'comment_banned' => (int)$commentBanned,
-    'creator_banned' => (int)$creatorBanned,
-    'upload_banned' => (int)$uploadBanned
+    'objects' => [
+        'total' => (int)$stats['objects'],
+        'average' => (double)($stats['objects'] / $stats['levels'])
+    ],
+    'likes' => [
+        'total' => (int)$stats['likes'],
+        'average' => (double)($stats['likes'] / $stats['levels'])
+    ],
+    'comments' => [
+        'total' => (int)$stats['totalComments'],
+        'comments' => (int)$stats['comments'],
+        'posts' => (int)$stats['posts'],
+        'post_replies' => (int)$stats['postReplies']
+    ],
+    'gained_stars' => [
+        'total' => (int)$stats['stars'],
+        'average' => (double)($stats['stars'] / $stats['users'])
+    ],
+    'creator_points' => [
+        'total' => (float)$stats['creatorPoints'],
+        'average' => (double)($stats['creatorPoints'] / $stats['users'])
+    ],
+    'bans' => [
+        'total' => (int)$stats['bannedPlayers'],
+        'leaderboard' => (int)$stats['leaderboardBanned'],
+        'comments' => (int)$stats['commentBanned'],
+        'creators' => (int)$stats['creatorBanned'],
+        'upload' => (int)$stats['uploadBanned'],
+        'ip' => (int)$stats['ipBanned']
+    ]
 ];
 
 exit(json_encode(['dashboard' => true, 'success' => true, 'stats' => $stats]));
