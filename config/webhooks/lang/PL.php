34e77a996174c8589306d2495ccc10b5dfe1a6b2
ExtremeSpe <136844328+ExtremeSpe@users.noreply.github.com>
2024-08-03 14:59:22 +0200

    pl webhook

---
+++ config/webhooks/lang/PL.php
@@ -0,0 +1,118 @@
+<?php
+/*
+	Welcome to webhooks translation file!
+	You're currently at Polish (Polski) language
+	Credits: extremespegd
+	
+	If you see array instead of simple string, that means you can add as many variations of translation as you want and they will be picked randomly
+*/
+$webhookLang['rateSuccessTitle'] = ['Nowy poziom został oceniony!', 'Nowy oceniony poziom!', 'Ktoś ocenił poziom!']; // This one is array
+$webhookLang['rateSuccessTitleDM'] = ['Twój poziom został oceniony!', 'Ktoś ocenił twój poziom!'];
+$webhookLang['rateSuccessDesc'] = '%1$s ocenił(a) poziom!'; // And this one is string
+$webhookLang['rateSuccessDescDM'] = '%1$s ocenił(a) twój poziom! %2$s';
+$webhookLang['rateFailTitle'] = ['Poziom został od-oceniony!', 'Ktoś od-ocenił poziom!'];
+$webhookLang['rateFailTitleDM'] = ['Twój poziom został od-oceniony!', 'Ktoś od-ocenił twój poziom!'];
+$webhookLang['rateFailDesc'] = '%1$s od-ocenił(a) poziom!';
+$webhookLang['rateFailDescDM'] = '%1$s od-ocenił(a) twój poziom! %2$s';
+
+$webhookLang['levelTitle'] = 'Poziom';
+$webhookLang['levelDesc'] = '%1$s przez %2$s'; // Name by Creator
+$webhookLang['levelIDTitle'] = 'ID Poziomu';
+$webhookLang['difficultyTitle'] = 'Trudność';
+$webhookLang['difficultyDesc0'] = '%1$s, %2$s gwiazdka'; // Auto, 1 star
+$webhookLang['difficultyDesc1'] = '%1$s, %2$s gwiazdki'; // Easy, 2 stars
+$webhookLang['difficultyDesc2'] = '%1$s, %2$s gwiazdek'; // Hard, 5 stars
+$webhookLang['difficultyDescMoon0'] = '%1$s, %2$s księżyc'; // Auto, 1 moon (Platformer)
+$webhookLang['difficultyDescMoon1'] = '%1$s, %2$s księżyce'; // Easy, 2 moons (Platformer)
+$webhookLang['difficultyDescMoon2'] = '%1$s, %2$s księżyców'; // Hard, 5 moons (Platformer)
+$webhookLang['statsTitle'] = 'Statystyki';
+$webhookLang['requestedTitle'] = 'Twórca prosi o';
+$webhookLang['requestedDesc0'] = '%1$s gwiazdkę'; // 1 star
+$webhookLang['requestedDesc1'] = '%1$s gwiazdki'; // 2 stars
+$webhookLang['requestedDesc2'] = '%1$s gwiazdek'; // 5 stars
+$webhookLang['requestedDescMoon0'] = '%1$s księżyc'; // 1 moon (Platformer)
+$webhookLang['requestedDescMoon1'] = '%1$s księżyce'; // 2 moons (Platformer)
+$webhookLang['requestedDescMoon2'] = '%1$s księżyców'; // 5 moons (Platformer)
+$webhookLang['descTitle'] = 'Opis';
+$webhookLang['descDesc'] = '*Brak opisu*';
+$webhookLang['footer'] = '%1$s, dziękuję za grę!';
+
+$webhookLang['suggestTitle'] = ['Sparwdź ten poziom!', 'Poziom został zasugerowany!', 'Ktoś zasugerował poziom!'];
+$webhookLang['suggestDesc'] = '%1$s zasugerował ocenienie poziomu!';
+$webhookLang['footerSuggest'] = '%1$s, dziękuję za moderację!';
+
+$webhookLang['demonlistTitle'] = ['Nowy rekord!', 'Ktoś opublikował nowy rekord!'];
+$webhookLang['demonlistDesc'] = '%1$s opublikował(a) przejście %2$s! Link do zatwierdzenia: ||%3$s||';
+$webhookLang['recordAuthorTitle'] = 'Autor rekordu';
+$webhookLang['recordAttemptsTitle'] = 'Próby';
+$webhookLang['recordAttemptsDesc0'] = 'Próba %1$s'; // 1 attempt
+$webhookLang['recordAttemptsDesc1'] = 'Próby %1$s'; // 2 attempts
+$webhookLang['recordAttemptsDesc2'] = 'Próby %1$s'; // 5 attempts
+$webhookLang['recordProofTitle'] = 'Dowód';
+$webhookLang['demonlistApproveTitle'] = ['Rekord został zatwierdzony!', 'Ktoś zatwierdził rekord!'];
+$webhookLang['demonlistApproveTitleDM'] = ['Twój rekord został zatwierdzony!', 'Ktoś zatwierdził twój rekord!'];
+$webhookLang['demonlistApproveDesc'] = '%1$s zatwierdził rekord %2$s poziomu %3$s!';
+$webhookLang['demonlistApproveDescDM'] = '%1$s zatwierdził twój rekord poziomu %2$s!';
+$webhookLang['demonlistDenyTitle'] = ['Rekord został odrzucony!', 'Ktoś odrzucił rekord!'];
+$webhookLang['demonlistDenyTitleDM'] = ['Twój rekord został odrzucony!', 'Ktoś odrzucił twój rekord!'];
+$webhookLang['demonlistDenyDesc'] = '%1$s odrzucił rekord %2$s poziomu %3$s!';
+$webhookLang['demonlistDenyDescDM'] = '%1$s odrzucił twój rekord poziomu %2$s!';
+
+$webhookLang['accountLinkTitle'] = ['Łączenie konta!', 'Ktoś chce połączyć konto!'];
+$webhookLang['accountLinkDesc'] = 'Wygląda na to, że %1$s chce podłączyć swoje konto w grze do twojego konta Discord. Napisz posta **!discord accept *code*** na twoim profilu w grze, aby to zrobić. Jeśli to nie ty - **ignoruj** tą wiadomość!';
+$webhookLang['accountCodeFirst'] = 'Pierwszy numer';
+$webhookLang['accountCodeSecond'] = 'Drugi numer';
+$webhookLang['accountCodeThird'] = 'Trzeci numer';
+$webhookLang['accountCodeFourth'] = 'Czwarty numer';
+$webhookLang['accountUnlinkTitle'] = ['Odłączanie konta!', 'Odłączyłeś swoje konto!'];
+$webhookLang['accountUnlinkDesc'] = 'Pomyślnie odłączyłeś %1$s od swojego konta Discord!';
+$webhookLang['accountAcceptTitle'] = ['Łączenie konta!', 'Połączyłeś swoje konto!'];
+$webhookLang['accountAcceptDesc'] = 'Pomyślnie połączyłeś %1$s ze swoim kontem Discord!';
+
+$webhookLang['playerBanTitle'] = ['Gracz został zbanowany!', 'Ktoś kogoś zbanował!', 'Ban!'];
+$webhookLang['playerBanTitleDM'] = ['Zostałeś zbanowany!', 'Ktoś cię zbanował!', 'Ban!'];
+$webhookLang['playerUnbanTitle'] = ['Gracz został odbanowany!', 'Ktoś kogoś odbanował!', 'Odbanowanie!'];
+$webhookLang['playerUnbanTitleDM'] = ['Zostałeś odbanowany!', 'Ktoś cię odbanował!', 'Odbanowanie!'];
+$webhookLang['playerBanTopDesc'] = '%1$s zbanował(a) %2$s z top graczy!';
+$webhookLang['playerBanTopDescDM'] = '%1$s zbanował(a) cię z top graczy.';
+$webhookLang['playerUnbanTopDesc'] = '%1$s odbanował(a) %2$s z top graczy!';
+$webhookLang['playerUnbanTopDescDM'] = '%1$s odbanował(a) cię z top graczy!';
+$webhookLang['playerBanCreatorDesc'] = '%1$s zbanował(a) %2$s z top twórców!';
+$webhookLang['playerBanCreatorDescDM'] = '%1$s zbanował(a) cię z top twórców.';
+$webhookLang['playerUnbanCreatorDesc'] = '%1$s odbanował(a) %2$s z top twórców!';
+$webhookLang['playerUnbanCreatorDescDM'] = '%1$s odbanował(a) cię z top twórców!';
+$webhookLang['playerBanUploadDesc'] = '%1$s zbanował(a) %2$s na wysyłanie poziomów!';
+$webhookLang['playerBanUploadDescDM'] = '%1$s zbanował(a) cię na wysyłanie poziomów.';
+$webhookLang['playerUnbanUploadDesc'] = '%1$s odbanował(a) %2$s na wysyłanie poziomów!';
+$webhookLang['playerUnbanUploadDescDM'] = '%1$s odbanował(a) cię na wysyłanie poziomów!';
+$webhookLang['playerModTitle'] = 'Moderator';
+$webhookLang['playerReasonTitle'] = 'Powód';
+$webhookLang['playerBanReason'] = '*Brak powodu*';
+$webhookLang['footerBan'] = '%1$s.';
+$webhookLang['playerBanCommentDesc'] = '%1$s zablokował(a) %2$s możliwość komentowania!';
+$webhookLang['playerBanCommentDescDM'] = '%1$s zablokował(a) tobie możliwość komentowania.';
+$webhookLang['playerUnbanCommentDesc'] = '%1$s odblokował(a) %2$s\'s ability to comment!';
+$webhookLang['playerUnbanCommentDescDM'] = '%1$s odblokował(a) tobie możliwość komentowania!';
+$webhookLang['playerBanAccountDesc'] = '%1$s zbanował(a) konto %2$s!';
+$webhookLang['playerBanAccountDescDM'] = '%1$s zbanował(a) twoje konto.';
+$webhookLang['playerUnbanAccountDesc'] = '%1$s odbanował(a) konto %2$s!';
+$webhookLang['playerUnbanAccountDescDM'] = '%1$s odbanował(a) twoje konto!';
+$webhookLang['playerExpiresTitle'] = 'Expires';
+$webhookLang['playerTypeTitle'] = 'Person type';
+$webhookLang['playerTypeName0'] = 'ID Konta';
+$webhookLang['playerTypeName1'] = 'ID Użytkownika';
+$webhookLang['playerTypeName2'] = 'Adres IP';
+
+$webhookLang['dailyTitle'] = 'Nowy dzienny poziom!';
+$webhookLang['dailyTitleDM'] = 'Twój poziom jest dzienny!';
+$webhookLang['dailyDesc'] = 'Ten poziom jest teraz dzienny!';
+$webhookLang['dailyDescDM'] = 'Twój poziom jest dzienny! %1$s';
+$webhookLang['weeklyTitle'] = 'Nowy tygodniowy poziom!';
+$webhookLang['weeklyTitleDM'] = 'Twój poziom jest tygodniowy!';
+$webhookLang['weeklyDesc'] = 'Ten poziom jest teraz tygodniowy!';
+$webhookLang['weeklyDescDM'] = 'Twój poziom jest tygodniowy! %1$s';
+$webhookLang['eventTitle'] = 'Nowy poziom wydarzeniowy!';
+$webhookLang['eventTitleDM'] = 'Twój poziom jest poziomem wydarzeniowym!';
+$webhookLang['eventDesc'] = 'Ten poziom jest teraz poziomem wydarzeniowym!';
+$webhookLang['eventDescDM'] = 'Twój poziom został użyty w wydarzeniu! %1$s';
+?>
