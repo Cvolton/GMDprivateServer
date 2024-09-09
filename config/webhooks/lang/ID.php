bd34e29d0f23d18ff451cb9a48251910b57b4308
IHNGEYMING <164995233+IHNGEYMING@users.noreply.github.com>
2024-09-09 20:44:37 +0700

    Lang Webhooks

---
+++ config/webhooks/lang/ID.php
@@ -0,0 +1,118 @@
+<?php
+/*
+	Welcome to webhooks translation file!
+	You're currently at English (English) language
+	Credits: *your username*
+	
+	If you see array instead of simple string, that means you can add as many variations of translation as you want and they will be picked randomly
+*/
+$webhookLang['rateSuccessTitle'] = ['Level baru telah diberi peringkat!', 'Level yang diberi peringkat baru!', 'Seseorang memberi peringkat pada suatu level!']; // This one is array
+$webhookLang['rateSuccessTitleDM'] = ['Level Anda telah dinilai!', 'Seseorang menilai level Anda!'];
+$webhookLang['rateSuccessDesc'] = '%1$s memberi nilai pada sebuah level!'; // And this one is string
+$webhookLang['rateSuccessDescDM'] = '%1$s menilai level Anda! %2$s';
+$webhookLang['rateFailTitle'] = ['Level belum diberi peringkat!', 'Seseorang tidak diberi peringkat pada level!'];
+$webhookLang['rateFailTitleDM'] = ['Level Anda belum diberi peringkat!', 'Seseorang tidak memberi peringkat pada level Anda!'];
+$webhookLang['rateFailDesc'] = '%1$s tidak memberi peringkat pada satu level!';
+$webhookLang['rateFailDescDM'] = '%1$s menghapus nilai level Anda! %2$s';
+
+$webhookLang['levelTitle'] = 'Level';
+$webhookLang['levelDesc'] = '%1$s oleh %2$s'; // Name by Creator
+$webhookLang['levelIDTitle'] = 'ID Level';
+$webhookLang['difficultyTitle'] = 'Kesulitan';
+$webhookLang['difficultyDesc0'] = '%1$s, %2$s bintang'; // Auto, 1 star
+$webhookLang['difficultyDesc1'] = '%1$s, %2$s bintang'; // Easy, 2 stars
+$webhookLang['difficultyDesc2'] = '%1$s, %2$s bintang'; // Hard, 5 stars
+$webhookLang['difficultyDescMoon0'] = '%1$s, %2$s bulan'; // Auto, 1 moon (Platformer)
+$webhookLang['difficultyDescMoon1'] = '%1$s, %2$s bulan'; // Easy, 2 moons (Platformer)
+$webhookLang['difficultyDescMoon2'] = '%1$s, %2$s bulan'; // Hard, 5 moons (Platformer)
+$webhookLang['statsTitle'] = 'Statistik';
+$webhookLang['requestedTitle'] = 'Pencipta meminta';
+$webhookLang['requestedDesc0'] = '%1$s bintang'; // 1 star
+$webhookLang['requestedDesc1'] = '%1$s bintang'; // 2 stars
+$webhookLang['requestedDesc2'] = '%1$s bintang'; // 5 stars
+$webhookLang['requestedDescMoon0'] = '%1$s bulan'; // 1 moon (Platformer)
+$webhookLang['requestedDescMoon1'] = '%1$s bulan'; // 2 moons (Platformer)
+$webhookLang['requestedDescMoon2'] = '%1$s bulan'; // 5 moons (Platformer)
+$webhookLang['descTitle'] = 'Deskripsi';
+$webhookLang['descDesc'] = '*Tidak ada deskripsi*';
+$webhookLang['footer'] = '%1$s, terima kasih sudah bermain!';
+
+$webhookLang['suggestTitle'] = ['Periksa level ini!', 'Level disarankan!', 'Seseorang menyarankan level!'];
+$webhookLang['suggestDesc'] = '%1$s menyarankan level untuk dinilai!';
+$webhookLang['footerSuggest'] = '%1$s, terima kasih telah memoderasi!';
+
+$webhookLang['demonlistTitle'] = ['Rekor baru!', 'Seseorang memposting rekor baru!'];
+$webhookLang['demonlistDesc'] = '%1$s memposting %2$s penyelesaiannya! Tautan untuk menyetujui: ||%3$s||';
+$webhookLang['recordAuthorTitle'] = 'Penulis rekaman';
+$webhookLang['recordAttemptsTitle'] = 'Percobaan';
+$webhookLang['recordAttemptsDesc0'] = '%1$s percobaan'; // 1 attempt
+$webhookLang['recordAttemptsDesc1'] = '%1$s percobaan'; // 2 attempts
+$webhookLang['recordAttemptsDesc2'] = '%1$s percobaan'; // 5 attempts
+$webhookLang['recordProofTitle'] = 'Bukti';
+$webhookLang['demonlistApproveTitle'] = ['Rekaman telah disetujui!', 'Seseorang menyetujui rekaman!'];
+$webhookLang['demonlistApproveTitleDM'] = ['Catatan Anda telah disetujui!', 'Seseorang menyetujui catatan Anda!'];
+$webhookLang['demonlistApproveDesc'] = '%1$s menyetujui rekor level %3$s %2$s!';
+$webhookLang['demonlistApproveDescDM'] = '%1$s menyetujui rekor level Anda %2$s!';
+$webhookLang['demonlistDenyTitle'] = ['Rekor ditolak!', 'Seseorang menyangkal rekor!'];
+$webhookLang['demonlistDenyTitleDM'] = ['Rekor Anda ditolak!', 'Seseorang menyangkal rekor Anda!'];
+$webhookLang['demonlistDenyDesc'] = '%1$s menolak rekor %2$s di level %3$s!';
+$webhookLang['demonlistDenyDescDM'] = '%1$s menolak rekor level Anda %2$s!';
+
+$webhookLang['accountLinkTitle'] = ['Penautan akun!', 'Seseorang ingin menautkan akun!'];
+$webhookLang['accountLinkDesc'] = 'Sepertinya %1$s ingin menautkan akun dalam game mereka ke akun Discord Anda. Posting **!discord, terima *kode*** di profil dalam game Anda untuk melakukannya. Jika itu bukan Anda - **abaikan** pesan ini!';
+$webhookLang['accountCodeFirst'] = 'Nomor pertama';
+$webhookLang['accountCodeSecond'] = 'Nomor kedua';
+$webhookLang['accountCodeThird'] = 'Nomor ketiga';
+$webhookLang['accountCodeFourth'] = 'Nomor keempat';
+$webhookLang['accountUnlinkTitle'] = ['Pembatalan tautan akun!', 'Anda membatalkan tautan akun Anda!'];
+$webhookLang['accountUnlinkDesc'] = 'Anda berhasil memutuskan tautan %1$s dari akun Discord Anda!';
+$webhookLang['accountAcceptTitle'] = ['Penautan akun!', 'Anda menautkan akun Anda!'];
+$webhookLang['accountAcceptDesc'] = 'Anda berhasil menautkan %1$s ke akun Discord Anda!';
+
+$webhookLang['playerBanTitle'] = ['Pemain dilarang!', 'Seseorang melarang seseorang!', 'Larangan!'];
+$webhookLang['playerBanTitleDM'] = ['Anda dilarang!', 'Seseorang melarang Anda!', 'Larangan!'];
+$webhookLang['playerUnbanTitle'] = ['Pemain telah dibatalkan pemblokirannya!', 'Seseorang membatalkan pemblokiran seseorang!', 'Batalkan pemblokiran!'];
+$webhookLang['playerUnbanTitleDM'] = ['Anda tidak diblokir!', 'Seseorang membatalkan pemblokiran Anda!', 'Batalkan pemblokiran!'];
+$webhookLang['playerBanTopDesc'] = '%1$s melarang %2$s dari pemain top!';
+$webhookLang['playerBanTopDescDM'] = '%1$s mencekal Anda dari pemain top.';
+$webhookLang['playerUnbanTopDesc'] = '%1$s membatalkan pemblokiran %2$s dari pemain top!';
+$webhookLang['playerUnbanTopDescDM'] = '%1$s membatalkan pemblokiran Anda dari pemain top!';
+$webhookLang['playerBanCreatorDesc'] = '%1$s melarang %2$s dari pembuat konten top!';
+$webhookLang['playerBanCreatorDescDM'] = '%1$s mencekal Anda dari kreator top.';
+$webhookLang['playerUnbanCreatorDesc'] = '%1$s membatalkan pemblokiran %2$s dari pembuat konten teratas!';
+$webhookLang['playerUnbanCreatorDescDM'] = '%1$s membatalkan pemblokiran Anda dari pembuat konten teratas!';
+$webhookLang['playerBanUploadDesc'] = '%1$s melarang %2$s mengunggah level!';
+$webhookLang['playerBanUploadDescDM'] = '%1$s melarang level pengunggahan untuk Anda.';
+$webhookLang['playerUnbanUploadDesc'] = '%1$s membatalkan pemblokiran %2$s dari level pengunggahan!';
+$webhookLang['playerUnbanUploadDescDM'] = '%1$s tingkat pengunggahan yang tidak diblokir untuk Anda!';
+$webhookLang['playerModTitle'] = 'Moderator';
+$webhookLang['playerReasonTitle'] = 'Alasan';
+$webhookLang['playerBanReason'] = '*Tidak ada alasan*';
+$webhookLang['footerBan'] = '%1$s.';
+$webhookLang['playerBanCommentDesc'] = '%1$s melarang kemampuan %2$s untuk berkomentar!';
+$webhookLang['playerBanCommentDescDM'] = '%1$s dilarang memberikan komentar kepada Anda.';
+$webhookLang['playerUnbanCommentDesc'] = '%1$s membatalkan pemblokiran kemampuan %2$s untuk berkomentar!';
+$webhookLang['playerUnbanCommentDescDM'] = '%1$s kemampuan yang tidak diblokir untuk mengomentari Anda!';
+$webhookLang['playerBanAccountDesc'] = '%1$s memblokir akun %2$s!';
+$webhookLang['playerBanAccountDescDM'] = '%1$s memblokir akun Anda.';
+$webhookLang['playerUnbanAccountDesc'] = '%1$s membatalkan pemblokiran akun %2$s!';
+$webhookLang['playerUnbanAccountDescDM'] = '%1$s membatalkan pemblokiran akun Anda!';
+$webhookLang['playerExpiresTitle'] = 'Kedaluwarsa';
+$webhookLang['playerTypeTitle'] = 'Tipe orang';
+$webhookLang['playerTypeName0'] = 'ID Akun';
+$webhookLang['playerTypeName1'] = 'ID Pengguna';
+$webhookLang['playerTypeName2'] = 'alamat IP';
+
+$webhookLang['dailyTitle'] = 'Level harian baru!';
+$webhookLang['dailyTitleDM'] = 'Level Anda setiap hari!';
+$webhookLang['dailyDesc'] = 'Level ini berlaku setiap hari sekarang!';
+$webhookLang['dailyDescDM'] = 'Level Anda menjadi setiap hari! %1$s';
+$webhookLang['weeklyTitle'] = 'Level mingguan baru!';
+$webhookLang['weeklyTitleDM'] = 'Level Anda mingguan!';
+$webhookLang['weeklyDesc'] = 'Level ini mingguan sekarang!';
+$webhookLang['weeklyDescDM'] = 'Level Anda menjadi mingguan! %1$s';
+$webhookLang['eventTitle'] = 'Tingkat acara baru!';
+$webhookLang['eventTitleDM'] = 'Level Anda adalah level acara!';
+$webhookLang['eventDesc'] = 'Level ini adalah level acara sekarang!';
+$webhookLang['eventDescDM'] = 'Level Anda digunakan dalam acara! %1$s';
+?>
