c9f70cd440e5d31d407ce2f5dd4c213970b4362b
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2025-01-11 01:59:10 +0300

    Small core fix

--- dashboard/api/login.php
+++ dashboard/api/login.php
@@ -6,6 +6,7 @@ require "../incl/dashboardLib.php";
 require "../".$dbPath."incl/lib/connection.php";
 require "../".$dbPath."config/security.php";
 require "../".$dbPath."config/mail.php";
+require "../".$dbPath."config/dashboard.php";
 require_once "../".$dbPath."incl/lib/exploitPatch.php";
 require_once "../".$dbPath."incl/lib/generatePass.php";
 require_once "../".$dbPath."incl/lib/mainLib.php";
@@ -20,18 +21,33 @@ if(isset($_POST["userName"]) AND isset($_POST["password"])){
 	}
 	$accountID = $gs->getAccountIDFromName($userName);
 	if($accountID == 0) exit(json_encode(['success' => false, 'error' => '-3']));
-  	$query = $db->prepare("SELECT auth FROM accounts WHERE accountID = :id");
-  	$query->execute([':id' => $accountID]);
-  	$auth = $query->fetch();
-    if($auth["auth"] == 'none') {
+  	$query = $db->prepare("SELECT auth FROM accounts WHERE accountID = :accountID");
+  	$query->execute([':accountID' => $accountID]);
+  	$auth = $query->fetchColumn();
+    if($auth == 'none') {
           $auth = $gs->randomString(8);
-          $query = $db->prepare("UPDATE accounts SET auth = :auth WHERE accountID = :id");
-          $query->execute([':auth' => $auth, ':id' => $accountID]);
-		  $auth["auth"] = $auth;
+          $query = $db->prepare("UPDATE accounts SET auth = :auth WHERE accountID = :accountID");
+          $query->execute([':auth' => $auth, ':accountID' => $accountID]);
     }
 	$color = $gs->getAccountCommentColor($accountID);
 	$gs->logAction($accountID, 2);
-	exit(json_encode(["success" => true, "user" => $userName, "accountID" => $accountID, "auth" => $auth["auth"], "color" => $color]));
+	$userID = $gs->getUserID($accountID, $userName);
+	$action = $db->prepare("SELECT * FROM users WHERE userID = :userID");
+	$action->execute([':userID' => $userID]);
+	$action = $action->fetch();
+	$iconType = ($action['iconType'] > 8) ? 0 : $action['iconType'];
+    $iconTypeMap = [0 => ['type' => 'cube', 'value' => $action['accIcon']], 1 => ['type' => 'ship', 'value' => $action['accShip']], 2 => ['type' => 'ball', 'value' => $action['accBall']], 3 => ['type' => 'ufo', 'value' => $action['accBird']], 4 => ['type' => 'wave', 'value' => $action['accDart']], 5 => ['type' => 'robot', 'value' => $action['accRobot']], 6 => ['type' => 'spider', 'value' => $action['accSpider']], 7 => ['type' => 'swing', 'value' => $action['accSwing']], 8 => ['type' => 'jetpack', 'value' => $action['accJetpack']]];
+    $iconValue = (isset($iconTypeMap[$iconType]) && $iconTypeMap[$iconType]['value'] > 0) ? $iconTypeMap[$iconType]['value'] : 1;
+    $avatarImg = $iconsRendererServer.'/icon.png?type=' . $iconTypeMap[$iconType]['type'] . '&value=' . $iconValue . '&color1=' . $action['color1'] . '&color2=' . $action['color2'] . ($action['accGlow'] != 0 ? '&glow=' . $action['accGlow'] . '&color3=' . $action['color3'] : '');
+	$clanID = $gs->isPlayerInClan($accountID);
+	if($clanID) {
+		$clan = $gs->getClanInfo($clanID);
+		$clanArray = [
+			'name' => $clan['clan'],
+			'color' => $clan['color']
+		];
+	} else $clanArray = [];
+	exit(json_encode(["success" => true, "user" => $userName, "accountID" => $accountID, "auth" => $auth, "color" => $color, "mainIcon" => $avatarImg, 'clan' => $clanArray]));
 } elseif(isset($_GET["auth"])) {
 	$auth = ExploitPatch::charclean($_GET["auth"]);
 	if(empty($auth)) exit(json_encode(['success' => false, 'error' => '-3']));
@@ -39,7 +55,22 @@ if(isset($_POST["userName"]) AND isset($_POST["password"])){
 	if(!is_array($check)) exit(json_encode(['success' => false, 'error' => $check]));
 	else {
 		$gs->logAction($check['accountID'], 2);
-		exit(json_encode(['success' => true, 'accountID' => $check['accountID'], 'userID' => $check['userID'], 'user' => $check["userName"], 'color' => $check['color']]));
+		$action = $db->prepare("SELECT * FROM users WHERE userID = :userID");
+		$action->execute([':userID' => $check['userID']]);
+		$action = $action->fetch();
+		$iconType = ($action['iconType'] > 8) ? 0 : $action['iconType'];
+		$iconTypeMap = [0 => ['type' => 'cube', 'value' => $action['accIcon']], 1 => ['type' => 'ship', 'value' => $action['accShip']], 2 => ['type' => 'ball', 'value' => $action['accBall']], 3 => ['type' => 'ufo', 'value' => $action['accBird']], 4 => ['type' => 'wave', 'value' => $action['accDart']], 5 => ['type' => 'robot', 'value' => $action['accRobot']], 6 => ['type' => 'spider', 'value' => $action['accSpider']], 7 => ['type' => 'swing', 'value' => $action['accSwing']], 8 => ['type' => 'jetpack', 'value' => $action['accJetpack']]];
+		$iconValue = (isset($iconTypeMap[$iconType]) && $iconTypeMap[$iconType]['value'] > 0) ? $iconTypeMap[$iconType]['value'] : 1;
+		$avatarImg = $iconsRendererServer.'/icon.png?type=' . $iconTypeMap[$iconType]['type'] . '&value=' . $iconValue . '&color1=' . $action['color1'] . '&color2=' . $action['color2'] . ($action['accGlow'] != 0 ? '&glow=' . $action['accGlow'] . '&color3=' . $action['color3'] : '');
+		$clanID = $gs->isPlayerInClan($check['accountID']);
+		if($clanID) {
+			$clan = $gs->getClanInfo($clanID);
+			$clanArray = [
+				'name' => $clan['clan'],
+				'color' => $clan['color']
+			];
+		} else $clanArray = [];
+		exit(json_encode(['success' => true, 'accountID' => $check['accountID'], 'userID' => $check['userID'], 'user' => $check["userName"], 'color' => $check['color'], "mainIcon" => $avatarImg, 'clan' => $clanArray]));
 	}
 } else exit(json_encode(['success' => false, 'error' => '0']));
 ?>
